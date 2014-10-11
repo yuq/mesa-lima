a2fd55cfb65d3933c27ed6c2259966a98acc55eb
Vinson Lee <vlee@freedesktop.org>
2014-10-10 22:40:21 -0700

    auxilary/os: Add DragonFly BSD support in os_get_total_physical_memory.
    This patch fixes this build error on DragonFly BSD.
    
      CC       os/os_misc.lo
    os/os_misc.c: In function 'os_get_total_physical_memory':
    os/os_misc.c:132:2: error: #error Unsupported *BSD
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/auxiliary/os/os_misc.c
+++ src/gallium/auxiliary/os/os_misc.c
@@ -128,6 +128,8 @@ os_get_total_physical_memory(uint64_t *size)
    mib[1] = HW_PHYSMEM64;
 #elif defined(PIPE_OS_FREEBSD)
    mib[1] = HW_REALMEM;
+#elif defined(PIPE_OS_DRAGONFLY)
+   mib[1] = HW_PHYSMEM;
 #else
 #error Unsupported *BSD
 #endif
