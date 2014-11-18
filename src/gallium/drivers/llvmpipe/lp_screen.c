763fc526c73bb7e3c7e2733106ad259f7dbd45f4
Roland Scheidegger <sroland@vmware.com>
2014-11-18 23:04:36 +0100

    llvmpipe: enable PIPE_CAP_TGSI_VS_LAYER_VIEWPORT
    No changes required in the driver itself, all handled by draw.
    
    piglit results in a quick run:
    skip->pass 7
    skip->fail 2
    (The new failures in the ARB_fragment_layer_viewport group are expected,
    we fail the same if gs doesn't write these outputs regardless of the vs.)
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -245,6 +245,7 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_ENDIANNESS:
       return PIPE_ENDIAN_NATIVE;
    case PIPE_CAP_TGSI_VS_LAYER_VIEWPORT:
+      return 1;
    case PIPE_CAP_MAX_TEXTURE_GATHER_COMPONENTS:
    case PIPE_CAP_TEXTURE_GATHER_SM5:
    case PIPE_CAP_BUFFER_MAP_PERSISTENT_COHERENT:
