bbac03ecca51fdbedf3c569c68322043c8e93fae
José Fonseca <jfonseca@vmware.com>
2014-11-25 22:27:04 +0000

    scons: Disable MSVC signed/unsigned mismatch warnings.
    By default gcc ignores the issue, and as result code that mixes
    signed/unsigned is so widespread through the code base that it ends up
    being little more than noise, potentially obscuring more pertinent
    warnings.
    
    Maybe one day we enable the corresponding gcc warnings and cleanup, but
    until then, this change disables them.
    
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>

--- scons/gallium.py
+++ scons/gallium.py
@@ -494,6 +494,7 @@ def generate(env):
             ]
         ccflags += [
             '/W3', # warning level
+            '/wd4018', # signed/unsigned mismatch
             '/wd4244', # conversion from 'type1' to 'type2', possible loss of data
             '/wd4305', # truncation from 'type1' to 'type2'
             '/wd4800', # forcing value to bool 'true' or 'false' (performance warning)
