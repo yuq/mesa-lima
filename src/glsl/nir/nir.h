f521a3c543ab1775603e1ad85c72e77812e039b5
Jason Ekstrand <jason.ekstrand@intel.com>
2014-11-26 15:08:19 -0800

    SQUASH: Use the enum for the variable mode

--- src/glsl/nir/nir.h
+++ src/glsl/nir/nir.h
@@ -149,7 +149,7 @@ typedef struct {
        *
        * \sa nir_variable_mode
        */
-      unsigned mode:4;
+      nir_variable_mode mode:4;
 
       /**
        * Interpolation mode for shader inputs / outputs
