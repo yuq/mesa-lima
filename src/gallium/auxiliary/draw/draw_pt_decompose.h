db3dfcfe90a3d27e6020e0d3642f8ab0330e57be
Roland Scheidegger <sroland@vmware.com>
2014-12-10 19:39:19 +0100

    draw: fix another decompose bug affecting constant interpolated attributes
    Previously the first provoking vertex convention would only be used if
    flatshading were enabled. No matter how I look at it that cannot be possibly
    correct. Maybe the code getting used was somewhat simpler that way at a time
    where there weren't constant interpolated attributes, only flatshading...
    (Note that all other places including the decomposition macros already do
    the same.)
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/gallium/auxiliary/draw/draw_pt_decompose.h
+++ src/gallium/auxiliary/draw/draw_pt_decompose.h
@@ -3,7 +3,6 @@
    const boolean quads_flatshade_last =      \
       draw->quads_always_flatshade_last;     \
    const boolean last_vertex_last =          \
-      !(draw->rasterizer->flatshade &&       \
-        draw->rasterizer->flatshade_first);
+      !draw->rasterizer->flatshade_first;
 
 #include "draw_decompose_tmp.h"
