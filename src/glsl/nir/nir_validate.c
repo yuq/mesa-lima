572d1f6e41907096f21743181fd96eda82f51a79
Jason Ekstrand <jason.ekstrand@intel.com>
2015-02-03 10:10:59 -0800

    nir/validate: Ensure that phi sources are SSA-only
    Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

--- src/glsl/nir/nir_validate.c
+++ src/glsl/nir/nir_validate.c
@@ -496,16 +496,9 @@ validate_phi_src(nir_phi_instr *instr, nir_block *pred, validate_state *state)
    exec_list_validate(&instr->srcs);
    nir_foreach_phi_src(instr, src) {
       if (src->pred == pred) {
-         unsigned num_components;
-         if (src->src.is_ssa)
-            num_components = src->src.ssa->num_components;
-         else {
-            if (src->src.reg.reg->is_packed)
-               num_components = 4; /* can't check anything */
-            else
-               num_components = src->src.reg.reg->num_components;
-         }
-         assert(num_components == instr->dest.ssa.num_components);
+         assert(src->src.is_ssa);
+         assert(src->src.ssa->num_components ==
+                instr->dest.ssa.num_components);
 
          validate_src(&src->src, state);
          return;
