06ed81044fc6b1dee90e67de314f81150746e104
Brian Paul <brianp@vmware.com>
2015-02-26 12:31:45 -0700

    c99_math.h: add defines for M_PI, M_E, M_LOG2E
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=89342
    Signed-off-by: Brian Paul <brianp@vmware.com>

--- include/c99_math.h
+++ include/c99_math.h
@@ -144,4 +144,17 @@ llrintf(float f)
 #endif
 
 
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+#ifndef M_E
+#define M_E (2.7182818284590452354)
+#endif
+
+#ifndef M_LOG2E
+#define M_LOG2E (1.4426950408889634074)
+#endif
+
+
 #endif /* #define _C99_MATH_H_ */
