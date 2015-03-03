4ab713423ffbdb885910f0c0efcabb594e55d227
Brian Paul <brianp@vmware.com>
2015-03-03 09:11:35 -0700

    mapi: use util/macros.h instead of locally defined macros
    The next step is to get rid of u_compiler.h completely.
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/mapi/u_compiler.h
+++ src/mapi/u_compiler.h
@@ -2,26 +2,6 @@
 #define _U_COMPILER_H_
 
 #include "c99_compat.h" /* inline, __func__, etc. */
-
-/* Function visibility */
-#ifndef PUBLIC
-#  if defined(__GNUC__) || (defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590))
-#    define PUBLIC __attribute__((visibility("default")))
-#  elif defined(_MSC_VER)
-#    define PUBLIC __declspec(dllexport)
-#  else
-#    define PUBLIC
-#  endif
-#endif
-
-#ifndef likely
-#  if defined(__GNUC__)
-#    define likely(x)   __builtin_expect(!!(x), 1)
-#    define unlikely(x) __builtin_expect(!!(x), 0)
-#  else
-#    define likely(x)   (x)
-#    define unlikely(x) (x)
-#  endif
-#endif
+#include "util/macros.h"
 
 #endif /* _U_COMPILER_H_ */
