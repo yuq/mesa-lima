f330ab93835c98e778dc22a50f8a018eb01d2ec3
Brian Paul <brianp@vmware.com>
2015-03-04 19:17:57 -0700

    dri/common: include stdio.h where needed
    Acked-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: José Fonseca <jfonseca@vmware.com>

--- src/mesa/drivers/dri/common/utils.c
+++ src/mesa/drivers/dri/common/utils.c
@@ -30,6 +30,7 @@
  */
 
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
--- src/mesa/drivers/dri/common/xmlconfig.c
+++ src/mesa/drivers/dri/common/xmlconfig.c
@@ -28,6 +28,7 @@
  */
 
 #include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <expat.h>
