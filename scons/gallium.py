1d30fd85dd2a240d4ccafc9a9eca55a129306cf5
Jose Fonseca <jfonseca@vmware.com>
2015-03-18 14:18:28 +0000

    scons: Silence MSVC warnings about overflows in constant arithmetic.
    These get triggered even when using the standard C99 INFINITY/NAN
    constants.
    
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- scons/gallium.py
+++ scons/gallium.py
@@ -495,8 +495,10 @@ def generate(env):
         ccflags += [
             '/W3', # warning level
             '/wd4018', # signed/unsigned mismatch
+            '/wd4056', # overflow in floating-point constant arithmetic
             '/wd4244', # conversion from 'type1' to 'type2', possible loss of data
             '/wd4305', # truncation from 'type1' to 'type2'
+            '/wd4756', # overflow in constant arithmetic
             '/wd4800', # forcing value to bool 'true' or 'false' (performance warning)
             '/wd4996', # disable deprecated POSIX name warnings
         ]
