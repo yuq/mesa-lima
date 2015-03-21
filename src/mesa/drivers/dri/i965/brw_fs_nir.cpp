a8c8b3b8720bb7ce8ac1cb94815ed36d8c881f66
Jason Ekstrand <jason.ekstrand@intel.com>
2015-03-21 12:21:21 -0700

    nir: Add a dedicated ffma peephole optimization
    i965/nir: Use the dedicated ffma peephole
    
    total instructions in shared programs: 4418748 -> 4394618 (-0.55%)
    instructions in affected programs:     1292790 -> 1268660 (-1.87%)
    helped:                                5999
    HURT:                                  457
    GAINED:                                4
    LOST:                                  9
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- src/mesa/drivers/dri/i965/brw_fs_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_nir.cpp
@@ -52,6 +52,8 @@ nir_optimize(nir_shader *nir)
       nir_validate_shader(nir);
       progress |= nir_opt_algebraic(nir);
       nir_validate_shader(nir);
+      progress |= nir_opt_peephole_ffma(nir);
+      nir_validate_shader(nir);
       progress |= nir_opt_constant_folding(nir);
       nir_validate_shader(nir);
       progress |= nir_opt_remove_phis(nir);
