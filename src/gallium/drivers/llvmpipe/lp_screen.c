489866938f8bf85625f0f452006a3422a2585950
Roland Scheidegger <sroland@vmware.com>
2015-03-31 17:23:17 +0200

    llvmpipe: enable ARB_texture_gather
    Just announce support for 4 components.
    While here also increase the max/min texel offsets (the limit is completely
    artificial, was chosen because that's what other hardware did, however there's
    other drivers using larger limits).
    Over a thousand little piglits skip->pass.
    
    v2: update docs/GL3.txt
    
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -180,10 +180,10 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    /* this is a lie could support arbitrary large offsets */
    case PIPE_CAP_MIN_TEXTURE_GATHER_OFFSET:
    case PIPE_CAP_MIN_TEXEL_OFFSET:
-      return -8;
+      return -32;
    case PIPE_CAP_MAX_TEXTURE_GATHER_OFFSET:
    case PIPE_CAP_MAX_TEXEL_OFFSET:
-      return 7;
+      return 31;
    case PIPE_CAP_CONDITIONAL_RENDER:
       return 1;
    case PIPE_CAP_TEXTURE_BARRIER:
@@ -249,6 +249,7 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_BUFFER_MAP_PERSISTENT_COHERENT:
       return 1;
    case PIPE_CAP_MAX_TEXTURE_GATHER_COMPONENTS:
+      return 4;
    case PIPE_CAP_TEXTURE_GATHER_SM5:
    case PIPE_CAP_TEXTURE_QUERY_LOD:
    case PIPE_CAP_SAMPLE_SHADING:
