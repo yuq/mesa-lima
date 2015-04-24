dfb274af4c6e0991fa20af1606e45bea6f947fed
Ilia Mirkin <imirkin@alum.mit.edu>
2015-04-24 19:33:05 -0400

    mesa: the function name appears to have a gl prefix already
    Currently we're producing errors like
    
    User error: GL_INVALID_OPERATION in glglDeleteProgramsARB(invalid call)
    
    And noop_warn appears to be called with the full function name. Don't
    prepend a gl prefix.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/mesa/main/context.c
+++ src/mesa/main/context.c
@@ -894,14 +894,14 @@ update_default_objects(struct gl_context *ctx)
  * If there's no current OpenGL context for the calling thread, we can
  * print a message to stderr.
  *
- * \param name  the name of the OpenGL function, without the "gl" prefix
+ * \param name  the name of the OpenGL function
  */
 static void
 nop_handler(const char *name)
 {
    GET_CURRENT_CONTEXT(ctx);
    if (ctx) {
-      _mesa_error(ctx, GL_INVALID_OPERATION, "gl%s(invalid call)", name);
+      _mesa_error(ctx, GL_INVALID_OPERATION, "%s(invalid call)", name);
    }
 #if defined(DEBUG)
    else if (getenv("MESA_DEBUG") || getenv("LIBGL_DEBUG")) {
