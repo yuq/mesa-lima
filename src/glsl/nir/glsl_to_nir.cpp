a45d55f17c7895c55214d2babeb0f0b03442c8a8
Ian Romanick <ian.d.romanick@intel.com>
2015-05-06 16:57:22 -0700

    nir: Don't produce nir_op_fexp from GLSL IR
    All paths that produce GLSL IR for NIR lower ir_unop_exp.  All paths
    that consume NIR will explode if they geta nir_op_fexp.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Acked-by: Jason Ekstrand <jason.ekstrand@intel.com>

--- src/glsl/nir/glsl_to_nir.cpp
+++ src/glsl/nir/glsl_to_nir.cpp
@@ -1038,7 +1038,7 @@ nir_visitor::visit(ir_expression *ir)
    case ir_unop_rcp:  emit(nir_op_frcp, dest_size, srcs);  break;
    case ir_unop_rsq:  emit(nir_op_frsq, dest_size, srcs);  break;
    case ir_unop_sqrt: emit(nir_op_fsqrt, dest_size, srcs); break;
-   case ir_unop_exp:  emit(nir_op_fexp, dest_size, srcs);  break;
+   case ir_unop_exp:  unreachable("ir_unop_exp should have been lowered");
    case ir_unop_log:  emit(nir_op_flog, dest_size, srcs);  break;
    case ir_unop_exp2: emit(nir_op_fexp2, dest_size, srcs); break;
    case ir_unop_log2: emit(nir_op_flog2, dest_size, srcs); break;
