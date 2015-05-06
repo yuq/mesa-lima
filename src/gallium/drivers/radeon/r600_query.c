yuq/mesa-lima

commit 96f164f6f047833091eb98a73aa80c31dc94f962

gallium: make pipe_context::begin_query return a boolean
GL_AMD_performance_monitor must return an error when a monitoring
session cannot be started.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Martin Peres <martin.peres@free.fr>

src/gallium/drivers/radeon/r600_query.c
@@ -425,7 +425,8 @@ static void r600_destroy_query(struct pipe_context *ctx, struct pipe_query *quer
 	FREE(query);
 }
 
-static void r600_begin_query(struct pipe_context *ctx, struct pipe_query *query)
+static boolean r600_begin_query(struct pipe_context *ctx,
+                                struct pipe_query *query)
 {
 	struct r600_common_context *rctx = (struct r600_common_context *)ctx;
 	struct r600_query *rquery = (struct r600_query *)query;
@@ -433,7 +434,7 @@ static void r600_begin_query(struct pipe_context *ctx, struct pipe_query *query)
 
 	if (!r600_query_needs_begin(rquery->type)) {
 		assert(0);
-		return;
+		return false;
 	}
 
 	/* Non-GPU queries. */
@@ -442,7 +443,7 @@ static void r600_begin_query(struct pipe_context *ctx, struct pipe_query *query)
 		return;
 	case R600_QUERY_DRAW_CALLS:
 		rquery->begin_result = rctx->num_draw_calls;
-		return;
+		return true;
 	case R600_QUERY_REQUESTED_VRAM:
 	case R600_QUERY_REQUESTED_GTT:
 	case R600_QUERY_VRAM_USAGE:
@@ -451,19 +452,19 @@ static void r600_begin_query(struct pipe_context *ctx, struct pipe_query *query)
 	case R600_QUERY_CURRENT_GPU_SCLK:
 	case R600_QUERY_CURRENT_GPU_MCLK:
 		rquery->begin_result = 0;
-		return;
+		return true;
 	case R600_QUERY_BUFFER_WAIT_TIME:
 		rquery->begin_result = rctx->ws->query_value(rctx->ws, RADEON_BUFFER_WAIT_TIME_NS);
-		return;
+		return true;
 	case R600_QUERY_NUM_CS_FLUSHES:
 		rquery->begin_result = rctx->ws->query_value(rctx->ws, RADEON_NUM_CS_FLUSHES);
-		return;
+		return true;
 	case R600_QUERY_NUM_BYTES_MOVED:
 		rquery->begin_result = rctx->ws->query_value(rctx->ws, RADEON_NUM_BYTES_MOVED);
-		return;
+		return true;
 	case R600_QUERY_GPU_LOAD:
 		rquery->begin_result = r600_gpu_load_begin(rctx->screen);
-		return;
+		return true;
 	}
 
 	/* Discard the old query buffers. */
@@ -489,6 +490,7 @@ static void r600_begin_query(struct pipe_context *ctx, struct pipe_query *query)
 	if (!r600_is_timer_query(rquery->type)) {
 		LIST_ADDTAIL(&rquery->list, &rctx->active_nontimer_queries);
 	}
+   return true;
 }
 
 static void r600_end_query(struct pipe_context *ctx, struct pipe_query *query)

src/gallium/drivers/freedreno/freedreno_query.c
@@ -59,11 +59,11 @@ fd_destroy_query(struct pipe_context *pctx, struct pipe_query *pq)
 	q->funcs->destroy_query(fd_context(pctx), q);
 }
 
-static void
+static boolean
 fd_begin_query(struct pipe_context *pctx, struct pipe_query *pq)
 {
 	struct fd_query *q = fd_query(pq);
-	q->funcs->begin_query(fd_context(pctx), q);
+	return q->funcs->begin_query(fd_context(pctx), q);
 }
 
 static void

src/gallium/drivers/freedreno/freedreno_query.h
@@ -37,7 +37,7 @@ struct fd_query;
 struct fd_query_funcs {
 	void (*destroy_query)(struct fd_context *ctx,
 			struct fd_query *q);
-	void (*begin_query)(struct fd_context *ctx, struct fd_query *q);
+	boolean (*begin_query)(struct fd_context *ctx, struct fd_query *q);
 	void (*end_query)(struct fd_context *ctx, struct fd_query *q);
 	boolean (*get_query_result)(struct fd_context *ctx,
 			struct fd_query *q, boolean wait,

src/gallium/drivers/freedreno/freedreno_query_hw.c
@@ -131,12 +131,12 @@ fd_hw_destroy_query(struct fd_context *ctx, struct fd_query *q)
 	free(hq);
 }
 
-static void
+static boolean
 fd_hw_begin_query(struct fd_context *ctx, struct fd_query *q)
 {
 	struct fd_hw_query *hq = fd_hw_query(q);
 	if (q->active)
-		return;
+		return false;
 
 	/* begin_query() should clear previous results: */
 	destroy_periods(ctx, &hq->periods);
@@ -149,6 +149,7 @@ fd_hw_begin_query(struct fd_context *ctx, struct fd_query *q)
 	/* add to active list: */
 	list_del(&hq->list);
 	list_addtail(&hq->list, &ctx->active_queries);
+   return true;
 }
 
 static void

src/gallium/drivers/freedreno/freedreno_query_sw.c
@@ -85,7 +85,7 @@ is_rate_query(struct fd_query *q)
 	}
 }
 
-static void
+static boolean
 fd_sw_begin_query(struct fd_context *ctx, struct fd_query *q)
 {
 	struct fd_sw_query *sq = fd_sw_query(q);
@@ -93,6 +93,7 @@ fd_sw_begin_query(struct fd_context *ctx, struct fd_query *q)
 	sq->begin_value = read_counter(ctx, q->type);
 	if (is_rate_query(q))
 		sq->begin_time = os_time_get();
+   return true;
 }
 
 static void

src/gallium/drivers/i915/i915_query.c
@@ -54,9 +54,10 @@ static void i915_destroy_query(struct pipe_context *ctx,
    FREE(query);
 }
 
-static void i915_begin_query(struct pipe_context *ctx,
+static boolean i915_begin_query(struct pipe_context *ctx,
                              struct pipe_query *query)
 {
+   return true;
 }
 
 static void i915_end_query(struct pipe_context *ctx, struct pipe_query *query)

src/gallium/drivers/ilo/ilo_query.c
@@ -111,7 +111,7 @@ ilo_destroy_query(struct pipe_context *pipe, struct pipe_query *query)
    FREE(q);
 }
 
-static void
+static boolean
 ilo_begin_query(struct pipe_context *pipe, struct pipe_query *query)
 {
    struct ilo_query *q = ilo_query(query);
@@ -124,6 +124,7 @@ ilo_begin_query(struct pipe_context *pipe, struct pipe_query *query)
    q->active = true;
 
    ilo_query_table[q->type].begin(ilo_context(pipe), q);
+   return true;
 }
 
 static void

src/gallium/drivers/llvmpipe/lp_query.c
@@ -184,7 +184,7 @@ llvmpipe_get_query_result(struct pipe_context *pipe,
 }
 
 
-static void
+static boolean
 llvmpipe_begin_query(struct pipe_context *pipe, struct pipe_query *q)
 {
    struct llvmpipe_context *llvmpipe = llvmpipe_context( pipe );
@@ -235,6 +235,7 @@ llvmpipe_begin_query(struct pipe_context *pipe, struct pipe_query *q)
    default:
       break;
    }
+   return true;
 }
 
 

commit 382b1a36e33c2d1f985996ecea54bdda547587d7

r600g: Fix Clang return-type build error.
Fix Clang return-type error introduced with commit
96f164f6f047833091eb98a73aa80c31dc94f962 "gallium: make
pipe_context::begin_query return a boolean".

  CC       r600_query.lo
r600_query.c:443:3: error: non-void function 'r600_begin_query' should return a value [-Wreturn-type]
                return;
                ^

Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Tom Stellard <thomas.stellard@amd.com>

src/gallium/drivers/radeon/r600_query.c
@@ -440,7 +440,7 @@ static boolean r600_begin_query(struct pipe_context *ctx,
 	/* Non-GPU queries. */
 	switch (rquery->type) {
 	case PIPE_QUERY_TIMESTAMP_DISJOINT:
-		return;
+		return true;
 	case R600_QUERY_DRAW_CALLS:
 		rquery->begin_result = rctx->num_draw_calls;
 		return true;