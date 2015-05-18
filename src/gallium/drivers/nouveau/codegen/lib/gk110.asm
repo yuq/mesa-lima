ae405d429ff62e279cb4bb84d29581d4f7467b52
Ilia Mirkin <imirkin@alum.mit.edu>
2015-05-18 12:57:31 -0400

    gk110/ir: switch to gk104-style sched codes rather than all-in-one
    Matches change to envydis/envyas tools.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/nouveau/codegen/lib/gk110.asm
+++ src/gallium/drivers/nouveau/codegen/lib/gk110.asm
@@ -11,7 +11,7 @@
 // SIZE:    22 / 14 * 8 bytes
 //
 gk110_div_u32:
-   sched 0x28282804280428
+   sched 0x28 0x04 0x28 0x04 0x28 0x28 0x28
    bfind u32 $r2 $r1
    xor b32 $r2 $r2 0x1f
    mov b32 $r3 0x1
@@ -19,7 +19,7 @@ gk110_div_u32:
    cvt u32 $r1 neg u32 $r1
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
-   sched 0x28282828282828
+   sched 0x28 0x28 0x28 0x28 0x28 0x28 0x28
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mul $r3 u32 $r1 u32 $r2
@@ -27,7 +27,7 @@ gk110_div_u32:
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mul $r3 u32 $r1 u32 $r2
-   sched 0x042c2828042804
+   sched 0x04 0x28 0x04 0x28 0x28 0x2c 0x04
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mov b32 $r3 $r0
    mul high $r0 u32 $r0 u32 $r2
@@ -35,7 +35,7 @@ gk110_div_u32:
    add $r1 (mul u32 $r1 u32 $r0) $r3
    set $p0 0x1 ge u32 $r1 $r2
    $p0 sub b32 $r1 $r1 $r2
-   sched 0x20282e20042c28
+   sched 0x28 0x2c 0x04 0x20 0x2e 0x28 0x20
    $p0 add b32 $r0 $r0 0x1
    $p0 set $p0 0x1 ge u32 $r1 $r2
    $p0 sub b32 $r1 $r1 $r2
@@ -51,7 +51,7 @@ gk110_div_u32:
 gk110_div_s32:
    set $p2 0x1 lt s32 $r0 0x0
    set $p3 0x1 lt s32 $r1 0x0 xor $p2
-   sched 0x28042804282820
+   sched 0x20 0x28 0x28 0x04 0x28 0x04 0x28
    cvt s32 $r0 abs s32 $r0
    cvt s32 $r1 abs s32 $r1
    bfind u32 $r2 $r1
@@ -59,7 +59,7 @@ gk110_div_s32:
    mov b32 $r3 0x1
    shl b32 $r2 $r3 clamp $r2
    cvt u32 $r1 neg u32 $r1
-   sched 0x28282828282828
+   sched 0x28 0x28 0x28 0x28 0x28 0x28 0x28
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mul $r3 u32 $r1 u32 $r2
@@ -67,7 +67,7 @@ gk110_div_s32:
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mul $r3 u32 $r1 u32 $r2
-   sched 0x28280428042828
+   sched 0x28 0x28 0x04 0x28 0x04 0x28 0x28
    add $r2 (mul high u32 $r2 u32 $r3) $r2
    mul $r3 u32 $r1 u32 $r2
    add $r2 (mul high u32 $r2 u32 $r3) $r2
@@ -75,7 +75,7 @@ gk110_div_s32:
    mul high $r0 u32 $r0 u32 $r2
    cvt u32 $r2 neg u32 $r1
    add $r1 (mul u32 $r1 u32 $r0) $r3
-   sched 0x2028042c28042c
+   sched 0x2c 0x04 0x28 0x2c 0x04 0x28 0x20
    set $p0 0x1 ge u32 $r1 $r2
    $p0 sub b32 $r1 $r1 $r2
    $p0 add b32 $r0 $r0 0x1
@@ -83,7 +83,7 @@ gk110_div_s32:
    $p0 sub b32 $r1 $r1 $r2
    $p0 add b32 $r0 $r0 0x1
    $p3 cvt s32 $r0 neg s32 $r0
-   sched 0x2c200428042e04
+   sched 0x04 0x2e 0x04 0x28 0x04 0x20 0x2c
    $p2 cvt s32 $r1 neg s32 $r1
    ret
 
