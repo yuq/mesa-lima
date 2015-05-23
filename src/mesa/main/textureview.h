8369675a55ab300a84b3a82632042a33883ca255
Brian Paul <brianp@vmware.com>
2015-05-22 17:20:57 -0700

    mesa: code clean-ups in textureview.[ch]
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

--- src/mesa/main/textureview.h
+++ src/mesa/main/textureview.h
@@ -41,7 +41,8 @@ _mesa_TextureView(GLuint texture, GLenum target, GLuint origtexture,
                   GLuint minlayer, GLuint numlayers);
 
 extern void
-_mesa_set_texture_view_state(struct gl_context *ctx, struct gl_texture_object *texObj,
-                       GLenum target, GLuint levels);
+_mesa_set_texture_view_state(struct gl_context *ctx,
+                             struct gl_texture_object *texObj,
+                             GLenum target, GLuint levels);
 
 #endif /* TEXTUREVIEW_H */
