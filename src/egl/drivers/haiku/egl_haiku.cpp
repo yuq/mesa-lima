0b652fedb5e097bcdea79e3b922e946d143148f6
Emil Velikov <emil.l.velikov@gmail.com>
2015-06-11 11:31:40 +0100

    egl/haiku: remove commented out code
    It serves little to no purpose. As the driver gets updated, one can
    look at the existing implementation (dri2) for reference rather than
    letting the commented functions bitrot.
    
    Cc: Alexander von Gluck IV <kallisti5@unixzen.com>
    Acked-by: Brian Paul <brianp@vmware.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/egl/drivers/haiku/egl_haiku.cpp
+++ src/egl/drivers/haiku/egl_haiku.cpp
@@ -74,48 +74,6 @@ struct haiku_egl_surface
 };
 
 
-/*
-static void
-swrastCreateDrawable(struct dri2_egl_display * dri2_dpy,
-	struct dri2_egl_surface * dri2_surf, int depth)
-{
-
-}
-
-
-static void
-swrastDestroyDrawable(struct dri2_egl_display * dri2_dpy,
-	struct dri2_egl_surface * dri2_surf)
-{
-
-}
-
-
-static void
-swrastGetDrawableInfo(__DRIdrawable * draw, int *x, int *y,
-	int *w, int *h, void *loaderPrivate)
-{
-
-}
-
-
-static void
-swrastPutImage(__DRIdrawable * draw, int op, int x, int y,
-	int w, int h, char *data, void *loaderPrivate)
-{
-
-}
-
-
-static void
-swrastGetImage(__DRIdrawable * read, int x, int y,
-	int w, int h, char *data, void *loaderPrivate)
-{
-
-}
-*/
-
-
 static void
 haiku_log(EGLint level, const char *msg)
 {
@@ -263,44 +221,12 @@ EGLBoolean
 init_haiku(_EGLDriver *drv, _EGLDisplay *dpy)
 {
 	_eglLog(_EGL_DEBUG,"\nInitializing Haiku EGL\n");
-	//_EGLDisplay* egl_dpy;
 
 	printf("Initializing Haiku EGL\n");
 	_eglSetLogProc(haiku_log);
 
 	loader_set_logger(_eglLog);
 
-	/*egl_dpy = (_EGLDisplay*) calloc(1, sizeof(_EGLDisplay));
-	if (!egl_dpy)
-		return _eglError(EGL_BAD_ALLOC, "eglInitialize");
-
-	dpy->DriverData=(void*) egl_dpy;
-	if (!dpy->PlatformDisplay) {
-		// OPEN DEVICE 
-		//dri2_dpy->bwindow = (void*)haiku_create_window();
-		//dri2_dpy->own_device = true;
-	} else {
-		//dri2_dpy->bwindow = (BWindow*)dpy->PlatformDisplay;
-	}*/
-	
-	//dri2_dpy->driver_name = strdup("swrast");
-	//if (!dri2_load_driver_swrast(dpy))
-	//   goto cleanup_conn;
-
-	/*dri2_dpy->swrast_loader_extension.base.name = __DRI_SWRAST_LOADER;
-	dri2_dpy->swrast_loader_extension.base.version = __DRI_SWRAST_LOADER_VERSION;
-	dri2_dpy->swrast_loader_extension.getDrawableInfo = swrastGetDrawableInfo;
-	dri2_dpy->swrast_loader_extension.putImage = swrastPutImage;
-	dri2_dpy->swrast_loader_extension.getImage = swrastGetImage;
-
-	dri2_dpy->extensions[0] = &dri2_dpy->swrast_loader_extension.base;
-	dri2_dpy->extensions[1] = NULL;
-	dri2_dpy->extensions[2] = NULL;*/
-
-	/*if (dri2_dpy->bwindow) {
-		if (!dri2_haiku_add_configs_for_visuals(dri2_dpy, dpy))
-			goto cleanup_configs;
-	}*/
 	_eglLog(_EGL_DEBUG,"Add configs");
     haiku_add_configs_for_visuals(dpy);
 
@@ -403,27 +329,8 @@ _eglBuiltInDriverHaiku(const char *args)
 	driver->base.API.CreatePixmapSurface = haiku_create_pixmap_surface;
 	driver->base.API.CreatePbufferSurface = haiku_create_pbuffer_surface;
 	driver->base.API.DestroySurface = haiku_destroy_surface;
-	/*
-	driver->API.GetProcAddress = dri2_get_proc_address;
-	driver->API.WaitClient = dri2_wait_client;
-	driver->API.WaitNative = dri2_wait_native;
-	driver->API.BindTexImage = dri2_bind_tex_image;
-	driver->API.ReleaseTexImage = dri2_release_tex_image;
-	driver->API.SwapInterval = dri2_swap_interval;
-	*/
 
 	driver->base.API.SwapBuffers = haiku_swap_buffers;
-	/*
-	driver->API.SwapBuffersWithDamageEXT = dri2_swap_buffers_with_damage;
-	driver->API.SwapBuffersRegionNOK = dri2_swap_buffers_region;
-	driver->API.PostSubBufferNV = dri2_post_sub_buffer;
-	driver->API.CopyBuffers = dri2_copy_buffers,
-	driver->API.QueryBufferAge = dri2_query_buffer_age;
-	driver->API.CreateImageKHR = dri2_create_image;
-	driver->API.DestroyImageKHR = dri2_destroy_image_khr;
-	driver->API.CreateWaylandBufferFromImageWL = dri2_create_wayland_buffer_from_image;
-	driver->API.GetSyncValuesCHROMIUM = dri2_get_sync_values_chromium;
-	*/
 
 	driver->base.Name = "Haiku";
 	driver->base.Unload = haiku_unload;
