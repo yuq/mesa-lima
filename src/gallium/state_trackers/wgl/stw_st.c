0925e5f5bc843237e534313dd5b99095ecbdd987
Brian Paul <brianp@vmware.com>
2015-06-16 15:32:45 -0600

    st/wgl: respect sample count when creating framebuffer surfaces
    Use the visual/pixel format's sample count instead of zero.
    
    Reviewed-by: Matthew McClure <mcclurem@vmware.com>

--- src/gallium/state_trackers/wgl/stw_st.c
+++ src/gallium/state_trackers/wgl/stw_st.c
@@ -77,6 +77,7 @@ stw_st_framebuffer_validate_locked(struct st_framebuffer_iface *stfb,
    templ.depth0 = 1;
    templ.array_size = 1;
    templ.last_level = 0;
+   templ.nr_samples = stwfb->stvis.samples;
 
    for (i = 0; i < ST_ATTACHMENT_COUNT; i++) {
       enum pipe_format format;
