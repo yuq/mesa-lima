fa4e3c3c9f6f3a72a032499fccaa6e222d6a7fa4
Antia Puentes <apuentes@igalia.com>
2015-06-17 10:06:44 +0200

    i965/nir/vec4: Implement the dot product operation
    Adds NIR ALU operations:
       * nir_op_fdot2
       * nir_op_fdot3
       * nir_op_fdot4
    
    Reviewed-by: Jason Ekstrand <jason.ekstrand@intel.com>

--- src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
@@ -1213,6 +1213,21 @@ vec4_visitor::nir_emit_alu(nir_alu_instr *instr)
       inst->predicate = BRW_PREDICATE_NORMAL;
       break;
 
+   case nir_op_fdot2:
+      inst = emit(BRW_OPCODE_DP2, dst, op[0], op[1]);
+      inst->saturate = instr->dest.saturate;
+      break;
+
+   case nir_op_fdot3:
+      inst = emit(BRW_OPCODE_DP3, dst, op[0], op[1]);
+      inst->saturate = instr->dest.saturate;
+      break;
+
+   case nir_op_fdot4:
+      inst = emit(BRW_OPCODE_DP4, dst, op[0], op[1]);
+      inst->saturate = instr->dest.saturate;
+      break;
+
    default:
       unreachable("Unimplemented ALU operation");
    }
