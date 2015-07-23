00fb21e744b045cc9f945021305b85595c35dd69
Rhys Kidd <rhyskidd@gmail.com>
2015-07-22 22:14:00 -0600

    doxygen: Link GLvector4f struct members properly, avoiding invalid XML/HTML warning
    Signed-off-by: Rhys Kidd <rhyskidd@gmail.com>
    
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/mesa/math/m_vector.h
+++ src/mesa/math/m_vector.h
@@ -61,7 +61,7 @@
  */
 typedef struct {
    GLfloat (*data)[4];	/**< may be malloc'd or point to client data */
-   GLfloat *start;	/**< points somewhere inside of <data> */
+   GLfloat *start;	/**< points somewhere inside of GLvector4f::data */
    GLuint count;	/**< size of the vector (in elements) */
    GLuint stride;	/**< stride from one element to the next (in bytes) */
    GLuint size;		/**< 2-4 for vertices and 1-4 for texcoords */
