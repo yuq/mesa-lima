d335aad11b208bcdcc75a99d4b6c5fc8b69ce368
Marek Olšák <marek.olsak@amd.com>
2015-08-11 22:36:51 +0200

    r600g: fix polygon offset scale
    The value was copied from r300g, which uses 1/12 subpixels, but this hw
    uses 1/16 subpixels.
    
    Should fix piglit: gl-1.4-polygon-offset (formerly a glean test)
    (untested, ported from radeonsi)
    
    Reviewed-by: Edward O'Callaghan <eocallaghan at alterapraxis.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: mesa-stable@lists.freedesktop.org

--- src/gallium/drivers/r600/evergreen_state.c
+++ src/gallium/drivers/r600/evergreen_state.c
@@ -485,7 +485,7 @@ static void *evergreen_create_rs_state(struct pipe_context *ctx,
 
 	/* offset */
 	rs->offset_units = state->offset_units;
-	rs->offset_scale = state->offset_scale * 12.0f;
+	rs->offset_scale = state->offset_scale * 16.0f;
 	rs->offset_enable = state->offset_point || state->offset_line || state->offset_tri;
 
 	if (state->point_size_per_vertex) {
