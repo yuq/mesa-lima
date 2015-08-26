42c7be5877665dfcbeb317c7eb163a03d56fe661
Brian Paul <brianp@vmware.com>
2015-08-26 13:23:47 -0600

    glsl: fix comment typo: s/filed/field/

--- src/glsl/glsl_types.h
+++ src/glsl/glsl_types.h
@@ -613,7 +613,7 @@ struct glsl_type {
    const glsl_type *field_type(const char *name) const;
 
    /**
-    * Get the location of a filed within a record type
+    * Get the location of a field within a record type
     */
    int field_index(const char *name) const;
 
