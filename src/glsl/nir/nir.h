967a5ddb88d248aa317964c9b0951332b2494aab
Kenneth Graunke <kenneth@whitecape.org>
2015-09-17 08:38:10 -0700

    nir: Report progress from nir_lower_globals_vars_to_local().
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Jason Ekstrand <jason.ekstrand@intel.com>

--- src/glsl/nir/nir.h
+++ src/glsl/nir/nir.h
@@ -1809,7 +1809,7 @@ void nir_split_var_copies(nir_shader *shader);
 void nir_lower_var_copy_instr(nir_intrinsic_instr *copy, void *mem_ctx);
 void nir_lower_var_copies(nir_shader *shader);
 
-void nir_lower_global_vars_to_local(nir_shader *shader);
+bool nir_lower_global_vars_to_local(nir_shader *shader);
 
 void nir_lower_locals_to_regs(nir_shader *shader);
 
