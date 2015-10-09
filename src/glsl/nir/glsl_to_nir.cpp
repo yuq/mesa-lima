b705005584730cff75f5bbe057832d8b1106124a
Jason Ekstrand <jason.ekstrand@intel.com>
2015-10-08 18:36:27 -0700

    nir/glsl: Use shader_prog->Name for naming the NIR shader
    This has the better name to use. Aparently, sh->Name is usually 0.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Neil Roberts <neil@linux.intel.com>

--- src/glsl/nir/glsl_to_nir.cpp
+++ src/glsl/nir/glsl_to_nir.cpp
@@ -150,7 +150,7 @@ glsl_to_nir(const struct gl_shader_program *shader_prog,
       if (sh->Program->SamplersUsed & (1 << i))
          num_textures = i;
 
-   shader->info.name = ralloc_asprintf(shader, "GLSL%d", sh->Name);
+   shader->info.name = ralloc_asprintf(shader, "GLSL%d", shader_prog->Name);
    shader->info.num_textures = num_textures;
    shader->info.num_ubos = sh->NumBufferInterfaceBlocks;
    shader->info.num_abos = shader_prog->NumAtomicBuffers;
