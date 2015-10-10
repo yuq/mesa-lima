60690cb3b3082b7397c48769ec28b5570f6b7d7e
Rob Clark <robclark@freedesktop.org>
2015-10-10 12:39:57 -0400

    glsl: move builtin vector types to glsl_types.cpp
    First step at untangling NIR's dependency on glsl_types without bringing
    in the dependency on glsl_symbol_table.  The builtin types are now in
    glsl_types (which will end up in NIR), but adding them to the symbol-
    table stays in builtin_types.cpp (which will not be part of NIR).
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Rob Clark <robclark@freedesktop.org>

--- src/glsl/glsl_types.cpp
+++ src/glsl/glsl_types.cpp
@@ -1729,3 +1729,17 @@ glsl_type::coordinate_components() const
 
    return size;
 }
+
+/**
+ * Declarations of type flyweights (glsl_type::_foo_type) and
+ * convenience pointers (glsl_type::foo_type).
+ * @{
+ */
+#define DECL_TYPE(NAME, ...)                                    \
+   const glsl_type glsl_type::_##NAME##_type = glsl_type(__VA_ARGS__, #NAME); \
+   const glsl_type *const glsl_type::NAME##_type = &glsl_type::_##NAME##_type;
+
+#define STRUCT_TYPE(NAME)
+
+#include "builtin_type_macros.h"
+/** @} */
