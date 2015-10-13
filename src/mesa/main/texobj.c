77eef8137056314c4d458f215a899e3eec42e910
Brian Paul <brianp@vmware.com>
2015-10-13 09:32:34 -0600

    mesa: remove unused texUnit local in _mesa_BindTextureUnit()
    The texture unit is error-checked before this and the texUnit var
    is unused, so remove it.
    
    Reviewed-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/mesa/main/texobj.c
+++ src/mesa/main/texobj.c
@@ -1759,16 +1759,9 @@ _mesa_BindTextureUnit(GLuint unit, GLuint texture)
 {
    GET_CURRENT_CONTEXT(ctx);
    struct gl_texture_object *texObj;
-   struct gl_texture_unit *texUnit;
 
    if (unit >= _mesa_max_tex_unit(ctx)) {
       _mesa_error(ctx, GL_INVALID_VALUE, "glBindTextureUnit(unit=%u)", unit);
-      return;
-   }
-
-   texUnit = _mesa_get_tex_unit(ctx, unit);
-   assert(texUnit);
-   if (!texUnit) {
       return;
    }
 
