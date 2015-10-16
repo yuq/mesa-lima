4627e8058ec1bc02cc2b6464ff83394b2d16879e
Brian Paul <brianp@vmware.com>
2015-10-16 09:10:22 -0600

    Revert "mesa: remove FLUSH_VERTICES() in _mesa_MatrixMode()"
    This reverts commit 0de5e0f3fb0f3671a3ecec6ab4473f9131ecd0ae.
    
    Michel Dänzer spotted two piglit regressions from the change.  I suspect
    that removing the FLUSH_VERTICES() actually exposed a bug elsewhere but
    I don't have time to hunt down the root issue at this time.

--- src/mesa/main/matrix.c
+++ src/mesa/main/matrix.c
@@ -151,6 +151,7 @@ _mesa_MatrixMode( GLenum mode )
 
    if (ctx->Transform.MatrixMode == mode && mode != GL_TEXTURE)
       return;
+   FLUSH_VERTICES(ctx, _NEW_TRANSFORM);
 
    switch (mode) {
    case GL_MODELVIEW:
