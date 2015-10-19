b23a4859f4dbbcca7d6a637010167d470211e45b
Jose Fonseca <jfonseca@vmware.com>
2015-10-19 14:29:28 +0100

    scons: Build nir/glsl_types.cpp once.
    Undoes early hacks, and ensures nir/glsl_types.cpp is built once, and
    only once.
    
    The root problem is that SCons doesn't know about NIR nor any source
    file in the NIR_FILES source list.
    
    Tested with libgl-gdi and libgl-xlib scons targets.
    
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/targets/libgl-gdi/glsl_types_hack.cpp
+++
@@ -1,3 +0,0 @@
-/* errrg scons.. otherwise "scons: *** Two environments with different actions were specified for the same target: $mesa/build/linux-x86_64-debug/glsl/nir/glsl_types.os" */
-#include "glsl_types.cpp"
-
