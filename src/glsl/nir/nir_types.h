4ff4310a783cb4815d99e2991697d9b300bc7d32
Jason Ekstrand <jason.ekstrand@intel.com>
2015-10-26 19:45:30 -0700

    nir/types: Expose glsl_type::count_attribute_slots()

--- src/glsl/nir/nir_types.h
+++ src/glsl/nir/nir_types.h
@@ -67,6 +67,8 @@ unsigned glsl_get_length(const struct glsl_type *type);
 
 unsigned glsl_get_aoa_size(const struct glsl_type *type);
 
+unsigned glsl_count_attribute_slots(const struct glsl_type *type);
+
 const char *glsl_get_struct_elem_name(const struct glsl_type *type,
                                       unsigned index);
 
