725fcdfbb11ff4c4399d58e08403e4e0064a8c5e
Timothy Arceri <timothy.arceri@collabora.com>
2015-10-28 07:42:49 +1100

    glsl: add helper to check for enhanced layouts support
    Reviewed-by: Emil Velikov <emil.velikov@collabora.co.uk>

--- src/glsl/glsl_parser_extras.h
+++ src/glsl/glsl_parser_extras.h
@@ -209,6 +209,11 @@ struct _mesa_glsl_parse_state {
       return ARB_shader_atomic_counters_enable || is_version(420, 310);
    }
 
+   bool has_enhanced_layouts() const
+   {
+      return ARB_enhanced_layouts_enable || is_version(440, 0);
+   }
+
    bool has_explicit_attrib_stream() const
    {
       return ARB_gpu_shader5_enable || is_version(400, 0);
