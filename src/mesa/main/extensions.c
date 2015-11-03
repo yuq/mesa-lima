13b19aa815661cd17b74c8694b6c466bfaf75740
Ryan Houdek <sonicadvance1@gmail.com>
2015-11-02 19:30:18 -0600

    mesa: expose support for GL_EXT_buffer_storage
    This extension requires ES 3.1 since it relies on glMemoryBarrier.
    For testing purposes I temporarily moved glMemoryBarrier to be an ES 3.0
    function.
    This has been tested with the piglit in the ML and the Dolphin emulator.
    
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/mesa/main/extensions.c
+++ src/mesa/main/extensions.c
@@ -222,6 +222,7 @@ static const struct extension extension_table[] = {
    { "GL_EXT_blend_color",                         o(EXT_blend_color),                         GLL,            1995 },
    { "GL_EXT_blend_equation_separate",             o(EXT_blend_equation_separate),             GL,             2003 },
    { "GL_EXT_blend_func_separate",                 o(EXT_blend_func_separate),                 GLL,            1999 },
+   { "GL_EXT_buffer_storage",                      o(ARB_buffer_storage),                                 ES31, 2015 },
    { "GL_EXT_discard_framebuffer",                 o(dummy_true),                                    ES1 | ES2, 2009 },
    { "GL_EXT_blend_minmax",                        o(EXT_blend_minmax),                        GLL | ES1 | ES2, 1995 },
    { "GL_EXT_blend_subtract",                      o(dummy_true),                              GLL,            1995 },
