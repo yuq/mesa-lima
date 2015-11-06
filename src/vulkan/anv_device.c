f8052351ac1cce196b99a78114d513695fbca4d6
Jason Ekstrand <jason.ekstrand@intel.com>
2015-11-06 13:29:47 -0800

    anv/device: Increase the block size for instructions

--- src/vulkan/anv_device.c
+++ src/vulkan/anv_device.c
@@ -661,7 +661,7 @@ VkResult anv_CreateDevice(
    anv_state_pool_init(&device->dynamic_state_pool,
                        &device->dynamic_state_block_pool);
 
-   anv_block_pool_init(&device->instruction_block_pool, device, 2048);
+   anv_block_pool_init(&device->instruction_block_pool, device, 4096);
    anv_block_pool_init(&device->surface_state_block_pool, device, 4096);
 
    anv_state_pool_init(&device->surface_state_pool,
