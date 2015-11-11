9fbd390dd4b60746c2ce60fb20c61c45c0e8a022
Jason Ekstrand <jason.ekstrand@intel.com>
2015-11-11 08:31:29 -0800

    nir: Add support for cloning shaders
    This commit is heavily based on one by Rob Clark <robdclark@gmail.com> but
    reworked to re-use nir_create functions and do less hashing.
    
    Signed-off-by: Jason Ekstrand <jason.ekstrand@intel.com>
    Reviewed-by: Rob Clark <robclark@freedesktop.org>

--- src/glsl/nir/nir.h
+++ src/glsl/nir/nir.h
@@ -1890,6 +1890,8 @@ void nir_index_blocks(nir_function_impl *impl);
 void nir_print_shader(nir_shader *shader, FILE *fp);
 void nir_print_instr(const nir_instr *instr, FILE *fp);
 
+nir_shader * nir_shader_clone(void *mem_ctx, const nir_shader *s);
+
 #ifdef DEBUG
 void nir_validate_shader(nir_shader *shader);
 void nir_metadata_set_validation_flag(nir_shader *shader);
