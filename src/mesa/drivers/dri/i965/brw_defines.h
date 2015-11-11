e9b0fa496ca3d8645ff528b0b44c76ebfa76b534
Kenneth Graunke <kenneth@whitecape.org>
2015-11-10 18:06:07 -0800

    i965: Add more MAX_*_URB_ENTRY_SIZE_BYTES #defines.
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>

--- src/mesa/drivers/dri/i965/brw_defines.h
+++ src/mesa/drivers/dri/i965/brw_defines.h
@@ -1938,8 +1938,14 @@ enum brw_message_target {
 
 /* Gen7 "GS URB Entry Allocation Size" is a U9-1 field, so the maximum gs_size
  * is 2^9, or 512.  It's counted in multiples of 64 bytes.
+ *
+ * Identical for VS, DS, and HS.
  */
 #define GEN7_MAX_GS_URB_ENTRY_SIZE_BYTES                (512*64)
+#define GEN7_MAX_DS_URB_ENTRY_SIZE_BYTES                (512*64)
+#define GEN7_MAX_HS_URB_ENTRY_SIZE_BYTES                (512*64)
+#define GEN7_MAX_VS_URB_ENTRY_SIZE_BYTES                (512*64)
+
 /* Gen6 "GS URB Entry Allocation Size" is defined as a number of 1024-bit
  * (128 bytes) URB rows and the maximum allowed value is 5 rows.
  */
