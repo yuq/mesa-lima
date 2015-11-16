91eefe850531adf7e1ed527e4b5bcb4a1f8c5d63
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2015-11-16 11:59:18 +0100

    glsl: initialize data.precision value in ir_variable constructor
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

--- src/glsl/ir.cpp
+++ src/glsl/ir.cpp
@@ -1676,6 +1676,7 @@ ir_variable::ir_variable(const struct glsl_type *type, const char *name,
    this->data.interpolation = INTERP_QUALIFIER_NONE;
    this->data.max_array_access = 0;
    this->data.atomic.offset = 0;
+   this->data.precision = GLSL_PRECISION_NONE;
    this->data.image_read_only = false;
    this->data.image_write_only = false;
    this->data.image_coherent = false;
