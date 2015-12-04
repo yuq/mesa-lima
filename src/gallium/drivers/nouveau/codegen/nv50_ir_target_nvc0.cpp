2b98914fe01f1c7b2de8a096c8923b3ab0a69578
Ilia Mirkin <imirkin@alum.mit.edu>
2015-12-03 22:37:22 -0500

    nv50/ir: avoid looking at uninitialized srcMods entries
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Cc: "11.0 11.1" <mesa-stable@lists.freedesktop.org>

--- src/gallium/drivers/nouveau/codegen/nv50_ir_target_nvc0.cpp
+++ src/gallium/drivers/nouveau/codegen/nv50_ir_target_nvc0.cpp
@@ -439,7 +439,7 @@ TargetNVC0::isModSupported(const Instruction *insn, int s, Modifier mod) const
          return false;
       }
    }
-   if (s >= 3)
+   if (s >= opInfo[insn->op].srcNr || s >= 3)
       return false;
    return (mod & Modifier(opInfo[insn->op].srcMods[s])) == mod;
 }
