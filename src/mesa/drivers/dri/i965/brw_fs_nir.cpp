efaf62a40a95b240cab7b0f371c7178aa19b7f3a
Iago Toral Quiroga <itoral@igalia.com>
2016-01-12 14:08:57 +0100

    i965/fs: implement i2d and u2d
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_fs_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_nir.cpp
@@ -724,6 +724,8 @@ fs_visitor::nir_emit_alu(const fs_builder &bld, nir_alu_instr *instr)
          return;
 
    case nir_op_f2d:
+   case nir_op_i2d:
+   case nir_op_u2d:
    case nir_op_d2f:
    case nir_op_d2i:
    case nir_op_d2u:
