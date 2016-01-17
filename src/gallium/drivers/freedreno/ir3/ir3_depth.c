02ac91d717036be0c8390b99860d37ff390c50e2
Rob Clark <robclark@freedesktop.org>
2016-01-17 12:21:45 -0500

    freedreno/ir3: fix mad 3rd src delay calc
    In fad158a0 ("freedreno/ir3: array rework") the src # (n) shifted by
    one, but missed updating delay-slot calc.
    
    Signed-off-by: Rob Clark <robclark@freedesktop.org>

--- src/gallium/drivers/freedreno/ir3/ir3_depth.c
+++ src/gallium/drivers/freedreno/ir3/ir3_depth.c
@@ -76,7 +76,7 @@ int ir3_delayslots(struct ir3_instruction *assigner,
 		return 6;
 	} else if ((consumer->category == 3) &&
 			(is_mad(consumer->opc) || is_madsh(consumer->opc)) &&
-			(n == 2)) {
+			(n == 3)) {
 		/* special case, 3rd src to cat3 not required on first cycle */
 		return 1;
 	} else {
