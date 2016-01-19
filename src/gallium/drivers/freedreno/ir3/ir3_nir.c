d00a239b288c5df7003f30ac80d156e70f27acf7
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-01-19 15:22:18 +0100

    freedreno/ir3: lower lrp when operating with double operands
    Lower lrp when operating with double operands because float version of
    lrp is also lowered.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Reviewed-by: Rob Clark <robdclark@gmail.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/gallium/drivers/freedreno/ir3/ir3_nir.c
+++ src/gallium/drivers/freedreno/ir3/ir3_nir.c
@@ -43,6 +43,7 @@ ir3_tgsi_to_nir(const struct tgsi_token *tokens)
 			.lower_fsat = true,
 			.lower_scmp = true,
 			.lower_flrp32 = true,
+			.lower_flrp64 = true,
 			.lower_ffract = true,
 			.native_integers = true,
 			.vertex_id_zero_based = true,
