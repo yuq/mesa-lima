1dc312e295c66ab8674d2f47f859e310f607b2ed
Matt Turner <mattst88@gmail.com>
2016-01-20 18:56:37 -0800

    i965/fs: Implement support for extract_word.
    The vec4 backend will lower it.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

--- src/mesa/drivers/dri/i965/brw_fs_cse.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_cse.cpp
@@ -78,6 +78,8 @@ is_expression(const fs_visitor *v, const fs_inst *const inst)
    case FS_OPCODE_LINTERP:
    case SHADER_OPCODE_FIND_LIVE_CHANNEL:
    case SHADER_OPCODE_BROADCAST:
+   case SHADER_OPCODE_EXTRACT_BYTE:
+   case SHADER_OPCODE_EXTRACT_WORD:
    case SHADER_OPCODE_MOV_INDIRECT:
       return true;
    case SHADER_OPCODE_RCP:
