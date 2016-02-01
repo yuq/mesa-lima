64abf6d404be9030d1232e1533a64edb09ad8b13
Rob Clark <robclark@freedesktop.org>
2016-02-01 17:29:22 -0500

    nir: clamp-color-output support
    Handled by tgsi_emulate for glsl->tgsi case.
    
    Signed-off-by: Rob Clark <robclark@freedesktop.org>
    Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2409,6 +2409,8 @@ void nir_lower_clip_fs(nir_shader *shader, unsigned ucp_enables);
 
 void nir_lower_two_sided_color(nir_shader *shader);
 
+void nir_lower_clamp_color_outputs(nir_shader *shader);
+
 void nir_lower_atomics(nir_shader *shader,
                        const struct gl_shader_program *shader_program);
 void nir_lower_to_source_mods(nir_shader *shader);
