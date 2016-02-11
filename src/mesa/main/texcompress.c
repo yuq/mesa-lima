54cac7ad962c6f9f87009f0c59f14c370ee6c48a
Anuj Phogat <anuj.phogat@gmail.com>
2016-02-11 12:28:31 -0800

    mesa: Handle astc 3d formats in _mesa_get_compressed_formats()
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/mesa/main/texcompress.c
+++ src/mesa/main/texcompress.c
@@ -438,6 +438,35 @@ _mesa_get_compressed_formats(struct gl_context *ctx, GLint *formats)
       }
    }
 
+   if (_mesa_is_gles3(ctx) &&
+       ctx->Extensions.OES_texture_compression_astc) {
+      if (formats) {
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_3x3x3_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_4x3x3_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_4x4x3_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_4x4x4_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_5x4x4_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_5x5x4_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_5x5x5_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_6x5x5_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_6x6x5_OES;
+         formats[n++] = GL_COMPRESSED_RGBA_ASTC_6x6x6_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_3x3x3_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_4x3x3_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4x3_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_4x4x4_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x4x4_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x5x4_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_5x5x5_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x5x5_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x6x5_OES;
+         formats[n++] = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_6x6x6_OES;
+      }
+      else {
+         n += 20;
+      }
+   }
+
    return n;
 }
 
