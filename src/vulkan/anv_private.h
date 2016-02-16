c2b2ebf1ed85b07d2e1ceffca89a7f7833dcb8d1
Kristian Høgsberg Kristensen <kristian.h.kristensen@intel.com>
2016-02-15 17:40:15 -0800

    anv: Add missing gen75_cmd_buffer_set_subpass() prototype

--- src/vulkan/anv_private.h
+++ src/vulkan/anv_private.h
@@ -1286,6 +1286,8 @@ void anv_cmd_state_setup_attachments(struct anv_cmd_buffer *cmd_buffer,
 
 void gen7_cmd_buffer_set_subpass(struct anv_cmd_buffer *cmd_buffer,
                                    struct anv_subpass *subpass);
+void gen75_cmd_buffer_set_subpass(struct anv_cmd_buffer *cmd_buffer,
+                                  struct anv_subpass *subpass);
 void gen8_cmd_buffer_set_subpass(struct anv_cmd_buffer *cmd_buffer,
                                    struct anv_subpass *subpass);
 void gen9_cmd_buffer_set_subpass(struct anv_cmd_buffer *cmd_buffer,
