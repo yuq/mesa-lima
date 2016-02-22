3da789f1e9e3eb027bcfdb9d9170e7b37160b5b9
Matt Turner <mattst88@gmail.com>
2016-02-22 09:36:48 -0800

    glsl: Consider ubo_load to be a horizontal operation.
    Unclear to me whether it actually is a horizontal operation that cannot
    be vectorized, but the fact that i965 generates the same code in either
    case makes me less interested in finding out.
    
    Cc: mesa-stable@lists.freedesktop.org
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=94199
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/compiler/glsl/ir.h
+++ src/compiler/glsl/ir.h
@@ -1686,6 +1686,7 @@ public:
              operation == ir_binop_dot ||
              operation == ir_binop_vector_extract ||
              operation == ir_triop_vector_insert ||
+             operation == ir_binop_ubo_load ||
              operation == ir_quadop_vector;
    }
 
