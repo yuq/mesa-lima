70cf0eb5c72c8115b1d2fe6532c97640b4043c0c
Ian Romanick <ian.d.romanick@intel.com>
2016-02-24 18:52:05 -0800

    i965/cfg: Slightly rearrange dead_control_flow_eliminate
    'git diff -w' is a bit more illustrative.  A couple declarations were
    moved, the continue was removed, and the code was reindented.  This will
    simplify future changes.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>

--- src/mesa/drivers/dri/i965/brw_dead_control_flow.cpp
+++ src/mesa/drivers/dri/i965/brw_dead_control_flow.cpp
@@ -41,76 +41,77 @@ dead_control_flow_eliminate(backend_shader *s)
    bool progress = false;
 
    foreach_block_safe (block, s->cfg) {
-      bblock_t *if_block = NULL, *else_block = NULL, *endif_block = block;
-      bool found = false;
+      backend_instruction *const inst = block->start();
 
       /* ENDIF instructions, by definition, can only be found at the start of
        * basic blocks.
        */
-      backend_instruction *endif_inst = endif_block->start();
-      if (endif_inst->opcode != BRW_OPCODE_ENDIF)
-         continue;
+      if (inst->opcode == BRW_OPCODE_ENDIF) {
+         bool found = false;
+         bblock_t *if_block = NULL, *else_block = NULL, *endif_block = block;
+         backend_instruction *endif_inst = inst;
 
-      backend_instruction *if_inst = NULL, *else_inst = NULL;
-      backend_instruction *prev_inst = endif_block->prev()->end();
-      if (prev_inst->opcode == BRW_OPCODE_ELSE) {
-         else_inst = prev_inst;
-         else_block = endif_block->prev();
-         found = true;
+         backend_instruction *if_inst = NULL, *else_inst = NULL;
+         backend_instruction *prev_inst = endif_block->prev()->end();
+         if (prev_inst->opcode == BRW_OPCODE_ELSE) {
+            else_inst = prev_inst;
+            else_block = endif_block->prev();
+            found = true;
 
-         if (else_block->start_ip == else_block->end_ip)
-            prev_inst = else_block->prev()->end();
-      }
+            if (else_block->start_ip == else_block->end_ip)
+               prev_inst = else_block->prev()->end();
+         }
 
-      if (prev_inst->opcode == BRW_OPCODE_IF) {
-         if_inst = prev_inst;
-         if_block = else_block != NULL ? else_block->prev()
-                                       : endif_block->prev();
-         found = true;
-      } else {
-         /* Don't remove the ENDIF if we didn't find a dead IF. */
-         endif_inst = NULL;
-      }
+         if (prev_inst->opcode == BRW_OPCODE_IF) {
+            if_inst = prev_inst;
+            if_block = else_block != NULL ? else_block->prev()
+               : endif_block->prev();
+            found = true;
+         } else {
+            /* Don't remove the ENDIF if we didn't find a dead IF. */
+            endif_inst = NULL;
+         }
 
-      if (found) {
-         bblock_t *earlier_block = NULL, *later_block = NULL;
+         if (found) {
+            bblock_t *earlier_block = NULL, *later_block = NULL;
 
-         if (if_inst) {
-            if (if_block->start_ip == if_block->end_ip) {
-               earlier_block = if_block->prev();
-            } else {
-               earlier_block = if_block;
+            if (if_inst) {
+               if (if_block->start_ip == if_block->end_ip) {
+                  earlier_block = if_block->prev();
+               } else {
+                  earlier_block = if_block;
+               }
+               if_inst->remove(if_block);
             }
-            if_inst->remove(if_block);
-         }
 
-         if (else_inst) {
-            else_inst->remove(else_block);
-         }
-
-         if (endif_inst) {
-            if (endif_block->start_ip == endif_block->end_ip) {
-               later_block = endif_block->next();
-            } else {
-               later_block = endif_block;
+            if (else_inst) {
+               else_inst->remove(else_block);
             }
-            endif_inst->remove(endif_block);
-         }
 
-         assert((earlier_block == NULL) == (later_block == NULL));
-         if (earlier_block && earlier_block->can_combine_with(later_block)) {
-            earlier_block->combine_with(later_block);
-
-            /* If ENDIF was in its own block, then we've now deleted it and
-             * merged the two surrounding blocks, the latter of which the
-             * __next block pointer was pointing to.
-             */
-            if (endif_block != later_block) {
-               __next = earlier_block->next();
+            if (endif_inst) {
+               if (endif_block->start_ip == endif_block->end_ip) {
+                  later_block = endif_block->next();
+               } else {
+                  later_block = endif_block;
+               }
+               endif_inst->remove(endif_block);
             }
-         }
 
-         progress = true;
+            assert((earlier_block == NULL) == (later_block == NULL));
+            if (earlier_block && earlier_block->can_combine_with(later_block)) {
+               earlier_block->combine_with(later_block);
+
+               /* If ENDIF was in its own block, then we've now deleted it and
+                * merged the two surrounding blocks, the latter of which the
+                * __next block pointer was pointing to.
+                */
+               if (endif_block != later_block) {
+                  __next = earlier_block->next();
+               }
+            }
+
+            progress = true;
+         }
       }
    }
 
