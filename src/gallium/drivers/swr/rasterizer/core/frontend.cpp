fee56fda6fd78f7fb10b0e8fced0a604ca43f0c0
Tim Rowley <timothy.o.rowley@intel.com>
2016-03-10 19:19:30 -0600

    swr: [rasterizer] Stop setting viewport size to larger than hottile array
    Guard against enquing work to invalid tiles

--- src/gallium/drivers/swr/rasterizer/core/frontend.cpp
+++ src/gallium/drivers/swr/rasterizer/core/frontend.cpp
@@ -243,6 +243,12 @@ void ProcessDiscardInvalidateTiles(
         macroTileEndY = (rect.bottom + macroHeight - 1) / macroHeight;
     }
 
+    SWR_ASSERT(macroTileEndX < KNOB_NUM_HOT_TILES_X);
+    SWR_ASSERT(macroTileEndY < KNOB_NUM_HOT_TILES_Y);
+
+    macroTileEndX = std::min<uint32_t>(macroTileEndX, KNOB_NUM_HOT_TILES_X);
+    macroTileEndY = std::min<uint32_t>(macroTileEndY, KNOB_NUM_HOT_TILES_Y);
+
     // load tiles
     BE_WORK work;
     work.type = DISCARDINVALIDATETILES;
