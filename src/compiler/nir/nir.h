c4c373f156a0b63a5789c0a8a3a1b641a58aa938
Jason Ekstrand <jason.ekstrand@intel.com>
2016-03-23 14:57:57 -0700

    nir: Fix whitespace
    Reviewed-by: Rob Clark <robclark@gmail.com>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2049,7 +2049,7 @@ void nir_index_blocks(nir_function_impl *impl);
 void nir_print_shader(nir_shader *shader, FILE *fp);
 void nir_print_instr(const nir_instr *instr, FILE *fp);
 
-nir_shader * nir_shader_clone(void *mem_ctx, const nir_shader *s);
+nir_shader *nir_shader_clone(void *mem_ctx, const nir_shader *s);
 nir_function_impl *nir_function_impl_clone(const nir_function_impl *fi);
 
 #ifdef DEBUG
