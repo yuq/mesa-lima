920fbecf57bc8e81db029a52f6ef1c9344d8ddab
Miklós Máté <mtmkls@gmail.com>
2016-03-24 01:12:58 +0100

    st/mesa: enable GL_ATI_fragment_shader
    Signed-off-by: Miklós Máté <mtmkls@gmail.com>
    Signed-off-by: Marek Olšák <marek.olsak@amd.com>

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -810,6 +810,7 @@ void st_init_extensions(struct pipe_screen *screen,
    extensions->EXT_texture_env_dot3 = GL_TRUE;
    extensions->EXT_vertex_array_bgra = GL_TRUE;
 
+   extensions->ATI_fragment_shader = GL_TRUE;
    extensions->ATI_texture_env_combine3 = GL_TRUE;
 
    extensions->MESA_pack_invert = GL_TRUE;
