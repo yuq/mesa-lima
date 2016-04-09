cf3bc91c063e71f7458d2e5645a8da87d2b0eaf4
Rhys Kidd <rhyskidd@gmail.com>
2016-04-09 16:43:18 -0400

    doxygen: Remove src/mesa/shader/ references
    Mesa has not had a src/mesa/shader/ folder since Mesa 7.9 removed it
    in October 2010, as part of a revised GLSL compiler written by Intel.
    
    Remove doxygen/shader.doxy and consequential changes made throughout.
    
    In addition to removing an unnecessary Doxygen doxyfile, this change also
    avoids a bug in the consolidated Doxygen output caused by
    doxygen/shader.doxy inadvertently overwriting doxygen/swrast.tag via its
    GENERATE_TAGFILE setting.
    
    This bug depended upon the specific order each *.tag was built.
    
    Signed-off-by: Rhys Kidd <rhyskidd@gmail.com>
    Acked-by: Emil Velikov <emil.velikov@collabora.com>

--- doxygen/doxy.bat
+++ doxygen/doxy.bat
@@ -6,7 +6,6 @@ doxygen swrast_setup.doxy
 doxygen tnl.doxy
 doxygen core.doxy
 doxygen glapi.doxy
-doxygen shader.doxy
 
 echo Building again, to resolve tags
 doxygen tnl_dd.doxy
@@ -16,4 +15,3 @@ doxygen swrast.doxy
 doxygen swrast_setup.doxy
 doxygen tnl.doxy
 doxygen glapi.doxy
-doxygen shader.doxy
