a0e777f6a12515cfadf8050eb63dfe24f84347b5
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2016-04-18 18:54:14 +0200

    nvc0: enable ARB_shader_image_load_store on GK110
    This exposes 8 images for all shader types.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -382,7 +382,7 @@ nvc0_screen_get_shader_param(struct pipe_screen *pscreen, unsigned shader,
    case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
       return 32;
    case PIPE_SHADER_CAP_MAX_SHADER_IMAGES:
-      if (class_3d == NVE4_3D_CLASS)
+      if (class_3d == NVE4_3D_CLASS || class_3d == NVF0_3D_CLASS)
          return NVC0_MAX_IMAGES;
       return 0;
    default:
