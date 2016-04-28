6028a67641741cb4e14265440282481441efa9c8
Jason Ekstrand <jason.ekstrand@intel.com>
2016-04-28 10:13:42 -0700

    anv: Fix a build error caused by recent fp64 NIR changes

--- src/intel/vulkan/anv_nir_apply_dynamic_offsets.c
+++ src/intel/vulkan/anv_nir_apply_dynamic_offsets.c
@@ -119,8 +119,9 @@ apply_dynamic_offsets_block(nir_block *block, void *void_state)
          exec_list_push_tail(&phi->srcs, &src1->node);
 
          b->cursor = nir_after_cf_list(&if_stmt->else_list);
-         nir_ssa_def *zero = nir_build_imm(b, intrin->num_components,
-            (nir_const_value) { .u32 = { 0, 0, 0, 0 } });
+         nir_const_value zero_val = { .u32 = { 0, 0, 0, 0 } };
+         nir_ssa_def *zero = nir_build_imm(b, intrin->dest.ssa.bit_size,
+                                           intrin->num_components, zero_val);
 
          nir_phi_src *src2 = ralloc(phi, nir_phi_src);
          struct exec_node *enode = exec_list_get_tail(&if_stmt->else_list);
