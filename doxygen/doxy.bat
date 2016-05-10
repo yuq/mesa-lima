0f738fa23ee2220841ad293654c6394dcc5943a4
Elie TOURNIER <tournier.elie@gmail.com>
2016-05-10 22:04:49 +0200

    doxygen: Add missing modules to Windows runner
    Acked-by: Rhys Kidd <rhyskidd@gmail.com>

--- doxygen/doxy.bat
+++ doxygen/doxy.bat
@@ -6,6 +6,9 @@ doxygen swrast_setup.doxy
 doxygen tnl.doxy
 doxygen core.doxy
 doxygen glapi.doxy
+doxygen glsl.doxy
+doxygen nir.doxy
+doxygen i965.doxy
 
 echo Building again, to resolve tags
 doxygen tnl_dd.doxy
@@ -14,4 +17,8 @@ doxygen math.doxy
 doxygen swrast.doxy
 doxygen swrast_setup.doxy
 doxygen tnl.doxy
+doxygen core.doxy
 doxygen glapi.doxy
+doxygen glsl.doxy
+doxygen nir.doxy
+doxygen i965.doxy
