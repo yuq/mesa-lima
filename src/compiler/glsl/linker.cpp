00441829e7317b2327c425cb8c364363b176defa
Timothy Arceri <timothy.arceri@collabora.com>
2016-05-17 11:34:38 +1000

    glsl: don't incorrectly eliminate patches with explicit locations
    These varying have a separate location domain from per-vertex varyings
    and need to be handled separately.
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>

--- src/compiler/glsl/linker.cpp
+++ src/compiler/glsl/linker.cpp
@@ -2888,7 +2888,8 @@ match_explicit_outputs_to_inputs(struct gl_shader_program *prog,
                                  gl_shader *consumer)
 {
    glsl_symbol_table parameters;
-   ir_variable *explicit_locations[MAX_VARYING][4] = { {NULL, NULL} };
+   ir_variable *explicit_locations[MAX_VARYINGS_INCL_PATCH][4] =
+      { {NULL, NULL} };
 
    /* Find all shader outputs in the "producer" stage.
     */
