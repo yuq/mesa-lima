7fe9a19302b8953cb267f3a1876543ad6aef8177
Kenneth Graunke <kenneth@whitecape.org>
2016-05-17 03:05:56 -0700

    nir: Fix wpos_ytransform lowering state_slot swizzle.
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Rob Clark <robdclark@gmail.com>

--- src/compiler/nir/nir_lower_wpos_ytransform.c
+++ src/compiler/nir/nir_lower_wpos_ytransform.c
@@ -23,6 +23,7 @@
 
 #include "nir.h"
 #include "nir_builder.h"
+#include "program/prog_instruction.h"
 
 /* Lower gl_FragCoord (and fddy) to account for driver's requested coordinate-
  * origin and pixel-center vs. shader.  If transformation is required, a
@@ -57,6 +58,7 @@ get_transform(lower_wpos_ytransform_state *state)
 
       var->num_state_slots = 1;
       var->state_slots = ralloc_array(var, nir_state_slot, 1);
+      var->state_slots[0].swizzle = SWIZZLE_XYZW;
       memcpy(var->state_slots[0].tokens, state->options->state_tokens,
              sizeof(var->state_slots[0].tokens));
 
