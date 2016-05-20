b19a0d506d33bff513d07a41b0ba51930464ab8f
Dave Airlie <airlied@redhat.com>
2016-05-21 06:19:29 +1000

    virgl: handle cull distance cap.
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/virgl/virgl_screen.c
+++ src/gallium/drivers/virgl/virgl_screen.c
@@ -241,6 +241,7 @@ virgl_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_PCI_FUNCTION:
    case PIPE_CAP_FRAMEBUFFER_NO_ATTACHMENT:
    case PIPE_CAP_ROBUST_BUFFER_ACCESS_BEHAVIOR:
+   case PIPE_CAP_CULL_DISTANCE:
       return 0;
    case PIPE_CAP_VENDOR_ID:
       return 0x1af4;
