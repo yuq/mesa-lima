469a1c56a68e8b2c1d55756868ca1951c383500b
Matt Turner <mattst88@gmail.com>
2016-05-24 13:13:04 -0700

    isl: Mark default cases unreachable.
    Reviewed-by: Anuj Phogat <anuj.phogat@gmail.com>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- src/intel/isl/isl_surface_state.c
+++ src/intel/isl/isl_surface_state.c
@@ -166,7 +166,7 @@ get_qpitch(const struct isl_surf *surf)
 {
    switch (surf->dim) {
    default:
-      assert(!"Bad isl_surf_dim");
+      unreachable("Bad isl_surf_dim");
    case ISL_SURF_DIM_1D:
       if (GEN_GEN >= 9) {
          /* QPitch is usually expressed as rows of surface elements (where
@@ -363,7 +363,7 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
                                             info->view->base_level) - 1;
       break;
    default:
-      unreachable(!"bad SurfaceType");
+      unreachable("bad SurfaceType");
    }
 
    if (info->view->usage & ISL_SURF_USAGE_RENDER_TARGET_BIT) {
