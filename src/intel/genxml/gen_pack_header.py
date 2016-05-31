a45a25418bd6310122d52f12a4640e8493246439
Dylan Baker <dylan@pnwbakers.com>
2016-05-31 11:36:26 -0700

    genxml: require future imports for python2 compatibility.
    Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    cc: 12.0 <mesa-stable@lists.freedesktop.org>

--- src/intel/genxml/gen_pack_header.py
+++ src/intel/genxml/gen_pack_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 #encoding=utf-8
 
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
 import xml.parsers.expat
 import re
 import sys
