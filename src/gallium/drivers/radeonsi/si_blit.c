ffd54d1936fcd07424265b780e1d049222a01e94
Marek Olšák <marek.olsak@amd.com>
2016-06-05 17:43:43 +0200

    radeonsi: allow direct hw MSAA resolve for scanout surfaces
    No idea why this was disabled, but it works fine.
    
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/gallium/drivers/radeonsi/si_blit.c
+++ src/gallium/drivers/radeonsi/si_blit.c
@@ -902,7 +902,6 @@ static bool do_hardware_msaa_resolve(struct pipe_context *ctx,
 	    info->src.box.height == dst_height &&
 	    info->src.box.depth == 1 &&
 	    dst->surface.level[info->dst.level].mode >= RADEON_SURF_MODE_1D &&
-	    !(dst->surface.flags & RADEON_SURF_SCANOUT) &&
 	    (!dst->cmask.size || !dst->dirty_level_mask) && /* dst cannot be fast-cleared */
 	    !dst->dcc_offset) {
 		si_blitter_begin(ctx, SI_COLOR_RESOLVE |
