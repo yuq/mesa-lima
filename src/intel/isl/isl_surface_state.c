d050ffbce950f8ececd12200145eb209819a421e
Jason Ekstrand <jason.ekstrand@intel.com>
2016-06-04 20:48:55 -0700

    isl/state: Don't force-disable L2 bypass for everything
    We already set the bit in the few cases where it's required by the docs so
    there's no need to set it all the time.  This has no noticable perf impact
    for Dota 2 on Vulkan with the time demo I have.
    
    Reviewed-by: Chad Versace <chad.versace@intel.com>
    Cc: "12.0" <mesa-stable@lists.freedesktop.org>

--- src/intel/isl/isl_surface_state.c
+++ src/intel/isl/isl_surface_state.c
@@ -311,10 +311,6 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
 #endif
 
 #if GEN_GEN >= 8
-   s.SamplerL2BypassModeDisable = true;
-#endif
-
-#if GEN_GEN >= 8
    s.RenderCacheReadWriteMode = WriteOnlyCache;
 #else
    s.RenderCacheReadWriteMode = 0;
@@ -431,7 +427,6 @@ isl_genX(buffer_fill_state_s)(void *state,
 #endif
 
 #if (GEN_GEN >= 8)
-      .SamplerL2BypassModeDisable = true,
       .RenderCacheReadWriteMode = WriteOnlyCache,
 #else
       .RenderCacheReadWriteMode = 0,
