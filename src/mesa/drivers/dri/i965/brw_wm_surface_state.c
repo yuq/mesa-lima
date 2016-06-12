367cf3a2e3e51466429a6446ef14ed398a5fb948
Jordan Justen <jordan.l.justen@intel.com>
2016-06-11 18:16:47 -0700

    i965: Use miptree to decide format on multi-plane images for gen < 7
    This wasn't handled correctly for multi-plane images on gen < 7 in
    727a9b24933d384f5440ed4318fb720ed11d6dd1.
    
    Reported-by: Mark Janes <mark.a.janes@intel.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=96674
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Cc: "12.0" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/mesa/drivers/dri/i965/brw_wm_surface_state.c
+++ src/mesa/drivers/dri/i965/brw_wm_surface_state.c
@@ -316,7 +316,8 @@ brw_update_texture_surface(struct gl_context *ctx,
    surf = brw_state_batch(brw, AUB_TRACE_SURFACE_STATE,
 			  6 * 4, 32, surf_offset);
 
-   uint32_t tex_format = translate_tex_format(brw, intelObj->_Format,
+   mesa_format mesa_fmt = plane == 0 ? intelObj->_Format : mt->format;
+   uint32_t tex_format = translate_tex_format(brw, mesa_fmt,
                                               sampler->sRGBDecode);
 
    if (for_gather) {
