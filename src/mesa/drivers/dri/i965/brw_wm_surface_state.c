ee39d3ba918de9d52d79bdee6db2c120bcf0f28e
Jason Ekstrand <jason.ekstrand@intel.com>
2016-06-21 21:58:23 -0700

    i965/gen4: Pull texture formats from the texture object not the miptree
    This makes texture views sort-of work.  It doesn't add full texture view
    support for gen4-5 but it is enough to fix the GL_ARB_copy_image formats
    piglit test on Iron Lake.
    
    Signed-off-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=83036
    Cc: "11.1 11.2 12.0" <mesa-stable@lists.freedesktop.org>

--- src/mesa/drivers/dri/i965/brw_wm_surface_state.c
+++ src/mesa/drivers/dri/i965/brw_wm_surface_state.c
@@ -335,7 +335,7 @@ brw_update_texture_surface(struct gl_context *ctx,
    surf = brw_state_batch(brw, AUB_TRACE_SURFACE_STATE,
 			  6 * 4, 32, surf_offset);
 
-   uint32_t tex_format = translate_tex_format(brw, mt->format,
+   uint32_t tex_format = translate_tex_format(brw, intelObj->_Format,
                                               sampler->sRGBDecode);
 
    if (tObj->Target == GL_TEXTURE_EXTERNAL_OES) {
