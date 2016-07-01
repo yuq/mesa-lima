c823ff8dfbd22d6c50df8b6a7fd5abc299f90776
Brian Paul <brianp@vmware.com>
2016-06-30 18:15:19 -0600

    gallium/util: check for window cliprects in util_can_blit_via_copy_region()
    We can't blit with resource_copy_region() if there are window clip rects.
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>

--- src/gallium/auxiliary/util/u_surface.c
+++ src/gallium/auxiliary/util/u_surface.c
@@ -723,6 +723,7 @@ util_can_blit_via_copy_region(const struct pipe_blit_info *blit,
    if ((blit->mask & mask) != mask ||
        blit->filter != PIPE_TEX_FILTER_NEAREST ||
        blit->scissor_enable ||
+       blit->num_window_rectangles > 0 ||
        blit->alpha_blend) {
       return FALSE;
    }
