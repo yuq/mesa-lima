26187f3890cd92d6871ffb431763b27cacd8d4fe
Dave Airlie <airlied@redhat.com>
2016-07-05 10:26:14 +1000

    st/glsl_to_tgsi: fix st_src_reg_for_double constant.
    This needs to set the src swizzle so it doesn't access the .zw
    members ever when we are just emitting a 0 constant here.
    
    This fixes:
    vert-conversion-explicit-dvec3-bvec3.shader_test
    and a bunch of other fp64 tests on softpipe and radeonsi.
    
    Cc: <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/mesa/state_tracker/st_glsl_to_tgsi.cpp
+++ src/mesa/state_tracker/st_glsl_to_tgsi.cpp
@@ -1133,7 +1133,7 @@ glsl_to_tgsi_visitor::st_src_reg_for_double(double val)
 
    memcpy(uval, &val, sizeof(uval));
    src.index = add_constant(src.file, uval, 1, GL_DOUBLE, &src.swizzle);
-
+   src.swizzle = MAKE_SWIZZLE4(SWIZZLE_X, SWIZZLE_Y, SWIZZLE_X, SWIZZLE_Y);
    return src;
 }
 
