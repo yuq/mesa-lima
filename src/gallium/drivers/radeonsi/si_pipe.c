14608ef920a6eb9113f8d5da903cab6f9dc357ea
Józef Kucia <joseph.kucia@gmail.com>
2016-07-19 13:07:26 +0200

    radeonsi: advertise 8 bits subpixel precision for viewport bounds
    Signed-off-by: Józef Kucia <joseph.kucia@gmail.com>
    Signed-off-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -444,7 +444,6 @@ static int si_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_PRIMITIVE_RESTART_FOR_PATCHES:
 	case PIPE_CAP_TGSI_VOTE:
 	case PIPE_CAP_MAX_WINDOW_RECTANGLES:
-	case PIPE_CAP_VIEWPORT_SUBPIXEL_BITS:
 		return 0;
 
 	case PIPE_CAP_MAX_SHADER_PATCH_VARYINGS:
@@ -490,6 +489,8 @@ static int si_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 
 	case PIPE_CAP_MAX_VIEWPORTS:
 		return R600_MAX_VIEWPORTS;
+	case PIPE_CAP_VIEWPORT_SUBPIXEL_BITS:
+		return 8;
 
 	/* Timer queries, present when the clock frequency is non zero. */
 	case PIPE_CAP_QUERY_TIMESTAMP:
