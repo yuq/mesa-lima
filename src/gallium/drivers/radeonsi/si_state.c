9c63fd90561c32bccd905d8e0abf4864941f359a
Marek Olšák <marek.olsak@amd.com>
2016-08-09 12:58:25 +0200

    radeonsi: set CB_COLORn_INFO.ROUND_MODE
    just do what the register spec says
    
    Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>
    Reviewed-by: Edward O'Callaghan <funfunctor@folklore1984.net>

--- src/gallium/drivers/radeonsi/si_state.c
+++ src/gallium/drivers/radeonsi/si_state.c
@@ -2067,6 +2067,11 @@ static void si_initialize_color_surface(struct si_context *sctx,
 		S_028C70_BLEND_CLAMP(blend_clamp) |
 		S_028C70_BLEND_BYPASS(blend_bypass) |
 		S_028C70_SIMPLE_FLOAT(1) |
+		S_028C70_ROUND_MODE(ntype != V_028C70_NUMBER_UNORM &&
+				    ntype != V_028C70_NUMBER_SNORM &&
+				    ntype != V_028C70_NUMBER_SRGB &&
+				    format != V_028C70_COLOR_8_24 &&
+				    format != V_028C70_COLOR_24_8) |
 		S_028C70_NUMBER_TYPE(ntype) |
 		S_028C70_ENDIAN(endian);
 
