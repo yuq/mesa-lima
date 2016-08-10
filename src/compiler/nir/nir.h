88a2a2e05302179c59e257533ed77d2d0a6a5021
Jason Ekstrand <jason.ekstrand@intel.com>
2016-08-10 14:34:49 -0700

    nir/gcm: Add global value numbering support
    Unlike the current CSE pass, global value numbering is capable of detecting
    common values even if one does not dominate the other.  For instance, in
    you have
    
    if (...) {
       ssa_1 = ssa_0 + 7;
       /* use ssa_1 */
    } else {
       ssa_2 = ssa_0 + 7;
       /* use ssa_2 */
    }
    
    Global value numbering doesn't care about dominance relationships so it
    figures out that ssa_1 and ssa_2 are the same and converts this to
    
    if (...) {
       ssa_1 = ssa_0 + 7;
       /* use ssa_1 */
    } else {
       /* use ssa_1 */
    }
    
    Obviously, we just broke SSA form which is bad.  Global code motion,
    however, will repair this for us by turning this into
    
    ssa_1 = ssa_0 + 7;
    if (...) {
       /* use ssa_1 */
    } else {
       /* use ssa_1 */
    }
    
    This intended to eventually mostly replace CSE.  However, conventional CSE
    may still be useful because it's less of a scorched-earth approach and
    doesn't require GCM.  This makes it a bit more appropriate for use as a
    clean-up in a late optimization run.
    
    Signed-off-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2587,7 +2587,7 @@ bool nir_opt_dce(nir_shader *shader);
 
 bool nir_opt_dead_cf(nir_shader *shader);
 
-void nir_opt_gcm(nir_shader *shader);
+bool nir_opt_gcm(nir_shader *shader, bool value_number);
 
 bool nir_opt_peephole_select(nir_shader *shader);
 
