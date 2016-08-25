4f5f9575d079376544a2ff70fb19f1dca417a931
Emil Velikov <emil.velikov@collabora.com>
2016-08-25 12:21:31 +0100

    egl/android: remove config post-processing
    No longer needed as of last commit, since we no longer add OPENGL to the
    ClientAPIs thus, RenderType and Conformant don't have the desktop GL
    bit set.
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -806,15 +806,6 @@ droid_add_configs_for_visuals(_EGLDriver *drv, _EGLDisplay *dpy)
       }
    }
 
-   /* post-process configs */
-   for (i = 0; i < dpy->Configs->Size; i++) {
-      struct dri2_egl_config *dri2_conf = dri2_egl_config(dpy->Configs->Elements[i]);
-
-      /* there is no front buffer so no OpenGL */
-      dri2_conf->base.RenderableType &= ~EGL_OPENGL_BIT;
-      dri2_conf->base.Conformant &= ~EGL_OPENGL_BIT;
-   }
-
    return (count != 0);
 }
 
