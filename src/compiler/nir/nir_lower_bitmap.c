a99d70d105df8a20f3ea98640afdeab7ed69b685
Eric Anholt <eric@anholt.net>
2016-08-26 13:45:47 -0700

    nir: Update shader info when adding discards
    vc4 is about to start using the shader info field to set up discard
    handling.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/compiler/nir/nir_lower_bitmap.c
+++ src/compiler/nir/nir_lower_bitmap.c
@@ -107,6 +107,8 @@ lower_bitmap(nir_shader *shader, nir_builder *b,
    discard = nir_intrinsic_instr_create(shader, nir_intrinsic_discard_if);
    discard->src[0] = nir_src_for_ssa(cond);
    nir_builder_instr_insert(b, &discard->instr);
+
+   shader->info.fs.uses_discard = true;
 }
 
 static void
