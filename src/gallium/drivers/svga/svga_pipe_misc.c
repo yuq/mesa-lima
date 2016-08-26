ea33df7b586807142f48f01380a77805d0e5be8f
Brian Paul <brianp@vmware.com>
2016-08-26 09:36:29 -0600

    svga: minor whitespace, etc clean-ups in svga_pipe_misc.c
    Reviewed-by: Neha Bhende <bhenden@vmware.com>

--- src/gallium/drivers/svga/svga_pipe_misc.c
+++ src/gallium/drivers/svga/svga_pipe_misc.c
@@ -23,26 +23,26 @@
  *
  **********************************************************/
 
-#include "svga_cmd.h"
-
 #include "util/u_framebuffer.h"
 #include "util/u_inlines.h"
 #include "util/u_pstipple.h"
 
+#include "svga_cmd.h"
 #include "svga_context.h"
 #include "svga_screen.h"
 #include "svga_surface.h"
 #include "svga_resource_texture.h"
 
 
-static void svga_set_scissor_states( struct pipe_context *pipe,
-                                     unsigned start_slot,
-                                     unsigned num_scissors,
-                                     const struct pipe_scissor_state *scissors )
+static void
+svga_set_scissor_states(struct pipe_context *pipe,
+                        unsigned start_slot,
+                        unsigned num_scissors,
+                        const struct pipe_scissor_state *scissors)
 {
    struct svga_context *svga = svga_context(pipe);
 
-   memcpy( &svga->curr.scissor, scissors, sizeof(*scissors) );
+   memcpy(&svga->curr.scissor, scissors, sizeof(*scissors));
    svga->dirty |= SVGA_NEW_SCISSOR;
 }
 
@@ -81,7 +81,8 @@ svga_set_polygon_stipple(struct pipe_context *pipe,
 }
 
 
-void svga_cleanup_framebuffer(struct svga_context *svga)
+void
+svga_cleanup_framebuffer(struct svga_context *svga)
 {
    struct svga_screen *svgascreen = svga_screen(svga->pipe.screen);
    struct pipe_framebuffer_state *curr = &svga->curr.framebuffer;
@@ -103,8 +104,9 @@ void svga_cleanup_framebuffer(struct svga_context *svga)
 #define DEPTH_BIAS_SCALE_FACTOR_D32    ((float)(1<<31))
 
 
-static void svga_set_framebuffer_state(struct pipe_context *pipe,
-				       const struct pipe_framebuffer_state *fb)
+static void
+svga_set_framebuffer_state(struct pipe_context *pipe,
+                           const struct pipe_framebuffer_state *fb)
 {
    struct svga_context *svga = svga_context(pipe);
    struct pipe_framebuffer_state *dst = &svga->curr.framebuffer;
@@ -169,8 +171,7 @@ static void svga_set_framebuffer_state(struct pipe_context *pipe,
       }
    }
 
-   if (svga->curr.framebuffer.zsbuf)
-   {
+   if (svga->curr.framebuffer.zsbuf) {
       switch (svga->curr.framebuffer.zsbuf->format) {
       case PIPE_FORMAT_Z16_UNORM:
          svga->curr.depthscale = 1.0f / DEPTH_BIAS_SCALE_FACTOR_D16;
@@ -207,9 +208,9 @@ static void svga_set_framebuffer_state(struct pipe_context *pipe,
 }
 
 
-
-static void svga_set_clip_state( struct pipe_context *pipe,
-                                 const struct pipe_clip_state *clip )
+static void
+svga_set_clip_state(struct pipe_context *pipe,
+                    const struct pipe_clip_state *clip)
 {
    struct svga_context *svga = svga_context(pipe);
 
@@ -219,14 +220,11 @@ static void svga_set_clip_state( struct pipe_context *pipe,
 }
 
 
-
-/* Called when driver state tracker notices changes to the viewport
- * matrix:
- */
-static void svga_set_viewport_states( struct pipe_context *pipe,
-                                      unsigned start_slot,
-                                      unsigned num_viewports,
-                                      const struct pipe_viewport_state *viewports )
+static void
+svga_set_viewport_states(struct pipe_context *pipe,
+                         unsigned start_slot,
+                         unsigned num_viewports,
+                         const struct pipe_viewport_state *viewports)
 {
    struct svga_context *svga = svga_context(pipe);
 
@@ -256,7 +254,8 @@ svga_set_debug_callback(struct pipe_context *pipe,
 }
 
 
-void svga_init_misc_functions( struct svga_context *svga )
+void
+svga_init_misc_functions(struct svga_context *svga)
 {
    svga->pipe.set_scissor_states = svga_set_scissor_states;
    svga->pipe.set_polygon_stipple = svga_set_polygon_stipple;
@@ -265,5 +264,3 @@ void svga_init_misc_functions( struct svga_context *svga )
    svga->pipe.set_viewport_states = svga_set_viewport_states;
    svga->pipe.set_debug_callback = svga_set_debug_callback;
 }
-
-
