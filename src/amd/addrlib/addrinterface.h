a489cdb20f0ae23527997c494a305da77ca2f040
Joe Ma <joe.ma@amd.com>
2016-09-02 02:13:40 -0400

    amd/addrlib: Bump version to 6.02

--- src/amd/addrlib/addrinterface.h
+++ src/amd/addrlib/addrinterface.h
@@ -41,7 +41,7 @@ extern "C"
 #endif
 
 #define ADDRLIB_VERSION_MAJOR 6
-#define ADDRLIB_VERSION_MINOR 1
+#define ADDRLIB_VERSION_MINOR 2
 #define ADDRLIB_VERSION ((ADDRLIB_VERSION_MAJOR << 16) | ADDRLIB_VERSION_MINOR)
 
 /// Virtually all interface functions need ADDR_HANDLE as first parameter
