4bb9efb59290c77ef4acd4abab5729efa9bf4e24
Eric Engestrom <eric.engestrom@imgtec.com>
2016-09-13 17:32:39 +0100

    gbm: remove left-over array
    e7c8c85785b3a8f29e3f ("gbm: Removed unused function.") forgot to remove
    the global array used only by that function.
    
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

--- src/gbm/main/gbm.c
+++ src/gbm/main/gbm.c
@@ -45,12 +45,6 @@
 #include "gbmint.h"
 #include "backend.h"
 
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
-
-static struct gbm_device *devices[16];
-
-static int device_num = 0;
-
 /** Returns the file description for the gbm device
  *
  * \return The fd that the struct gbm_device was created with
@@ -127,9 +121,6 @@ gbm_create_device(int fd)
       return NULL;
    }
 
-   if (device_num == 0)
-      memset(devices, 0, sizeof devices);
-
    gbm = _gbm_create_device(fd);
    if (gbm == NULL)
       return NULL;
@@ -137,9 +128,6 @@ gbm_create_device(int fd)
    gbm->dummy = gbm_create_device;
    gbm->stat = buf;
    gbm->refcount = 1;
-
-   if (device_num < ARRAY_SIZE(devices)-1)
-      devices[device_num++] = gbm;
 
    return gbm;
 }
