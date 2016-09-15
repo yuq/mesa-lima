8358e58f2534f7e60c452bed2adb628d75b055c3
Ian Romanick <ian.d.romanick@intel.com>
2016-09-15 11:21:28 -0700

    glsl/standalone: Enable ARB_gpu_shader_int64
    v2: Add missing break in GLSL_TYPE_INT64 case.  Notice by Matt.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- src/compiler/glsl/ir_builder_print_visitor.cpp
+++ src/compiler/glsl/ir_builder_print_visitor.cpp
@@ -396,13 +396,24 @@ ir_builder_print_visitor::visit(ir_constant *ir)
 
             memcpy(&v, &ir->value.d[i], sizeof(v));
             if (v != 0)
-               /* FIXME: This won't actually work until ARB_gpu_shader_int64
-                * support lands.
-                */
                print_without_indent("r%04X_data.u64[%u] = 0x%016" PRIx64 "; /* %g */\n",
                                     my_index, i, v, ir->value.d[i]);
             break;
          }
+         case GLSL_TYPE_UINT64:
+            if (ir->value.u64[i] != 0)
+               print_without_indent("r%04X_data.u64[%u] = %" PRIu64 ";\n",
+                                    my_index,
+                                    i,
+                                    ir->value.u64[i]);
+            break;
+         case GLSL_TYPE_INT64:
+            if (ir->value.i64[i] != 0)
+               print_without_indent("r%04X_data.i64[%u] = %" PRId64 ";\n",
+                                    my_index,
+                                    i,
+                                    ir->value.i64[i]);
+            break;
          case GLSL_TYPE_BOOL:
             if (ir->value.u[i] != 0)
                print_without_indent("r%04X_data.u[%u] = 1;\n", my_index, i);
