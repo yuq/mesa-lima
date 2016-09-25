eb2dc04127df64c43c7fc540e4c370a6005e7736
Timothy Arceri <timothy.arceri@collabora.com>
2016-09-25 22:50:28 +1000

    i965: stop passing stage as a function parameter
    We already pass the shader so we can just get the stage from this.
    
    Reviewed-by: Jason Ekstrand <jason.ekstrand@intel.com>

--- src/mesa/drivers/dri/i965/brw_link.cpp
+++ src/mesa/drivers/dri/i965/brw_link.cpp
@@ -86,8 +86,7 @@ brw_lower_packing_builtins(struct brw_context *brw,
 }
 
 static void
-process_glsl_ir(gl_shader_stage stage,
-                struct brw_context *brw,
+process_glsl_ir(struct brw_context *brw,
                 struct gl_shader_program *shader_prog,
                 struct gl_linked_shader *shader)
 {
@@ -140,8 +139,7 @@ process_glsl_ir(gl_shader_stage stage,
    do_copy_propagation(shader->ir);
 
    bool lowered_variable_indexing =
-      lower_variable_index_to_cond_assign((gl_shader_stage)stage,
-                                          shader->ir,
+      lower_variable_index_to_cond_assign(shader->Stage, shader->ir,
                                           options->EmitNoIndirectInput,
                                           options->EmitNoIndirectOutput,
                                           options->EmitNoIndirectTemp,
@@ -227,7 +225,7 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
 
       _mesa_copy_linked_program_data((gl_shader_stage) stage, shProg, prog);
 
-      process_glsl_ir((gl_shader_stage) stage, brw, shProg, shader);
+      process_glsl_ir(brw, shProg, shader);
 
       /* Make a pass over the IR to add state references for any built-in
        * uniforms that are used.  This has to be done now (during linking).
