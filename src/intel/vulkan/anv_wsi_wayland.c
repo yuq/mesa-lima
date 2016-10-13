a3834ebaf9ea8ff6826651392978cdb73b7fab96
Dave Airlie <airlied@redhat.com>
2016-10-14 05:08:56 +1000

    anv: fix the wayland wsi busy flag setting
    Cc: "12.0" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/intel/vulkan/anv_wsi_wayland.c
+++ src/intel/vulkan/anv_wsi_wayland.c
@@ -523,7 +523,7 @@ wsi_wl_swapchain_acquire_next_image(struct anv_swapchain *anv_chain,
          if (!chain->images[i].busy) {
             /* We found a non-busy image */
             *image_index = i;
-            chain->images[image_index].busy = true;
+            chain->images[i].busy = true;
             return VK_SUCCESS;
          }
       }
