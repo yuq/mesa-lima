yuq/mesa-lima

commit b3bd8bb611bb465d2e5efddd109cf633bf3cc9b9

swr: [rasterizer core] add support for "RAW" surface format
Signed-off-by: Tim Rowley <timothy.o.rowley@intel.com>

src/gallium/drivers/swr/rasterizer/common/formats.h
@@ -216,6 +216,7 @@ enum SWR_FORMAT
     R8G8B8_UINT = 0x1C8,
     R8G8B8_SINT = 0x1C9,
     NUM_SWR_FORMATS = 0x1CA,
+    RAW = 0x1FF,
 };
 
 //////////////////////////////////////////////////////////////////////////
@@ -248,6 +249,11 @@ extern const SWR_FORMAT_INFO gFormatInfo[];
 /// @param format - SWR format
 INLINE const SWR_FORMAT_INFO& GetFormatInfo(SWR_FORMAT format)
 {
+    if (format == RAW)
+    {
+        format = NUM_SWR_FORMATS;
+    }
+    SWR_ASSERT(format <= NUM_SWR_FORMATS, "Invalid Surface Format: %d", format);
     return gFormatInfo[format];
 }
 

src/gallium/drivers/swr/rasterizer/common/formats.cpp
@@ -6082,4 +6082,27 @@ const SWR_FORMAT_INFO gFormatInfo[] = {
         1, // bcWidth
         1, // bcHeight
     },
+    // <---- Add new "supported" formats here!
+
+
+    // ... skip a long range ...
+    // RAW (0x1ff)
+    {
+        "RAW",
+        { SWR_TYPE_FLOAT, SWR_TYPE_UNKNOWN, SWR_TYPE_UNKNOWN, SWR_TYPE_UNKNOWN, },
+        { 0, 0, 0, 0 }, // Defaults for missing components
+        { 0, 1, 2, 3 }, // Swizzle
+        { 32, 0, 0, 0 }, // Bits per component
+        32, // Bits per element
+        4, // Bytes per element
+        1, // Num components
+        false, // isSRGB
+        false, // isBC
+        false, // isSubsampled
+        false, // isLuminance
+        { false, false, false, false }, // Is normalized?
+        { 1.0f, 1.0f, 1.0f, 1.0f }, // To float scale factor
+        1, // bcWidth
+        1, // bcHeight
+    }
 };

commit 827e038062bb7247a7b59b8f97d07b4c1b7be276

swr: [rasterizer common] fix assert index
Fixes: b3bd8bb611bb465d2e5e ("swr: [rasterizer core] add support
       for "RAW" surface format")
CovID: 1373647
Signed-off-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Tim Rowley <timothy.o.rowley@intel.com>

src/gallium/drivers/swr/rasterizer/common/formats.h
@@ -248,7 +248,7 @@ extern const SWR_FORMAT_INFO gFormatInfo[NUM_SWR_FORMATS];
 /// @param format - SWR format
 INLINE const SWR_FORMAT_INFO& GetFormatInfo(SWR_FORMAT format)
 {
-    SWR_ASSERT(format <= NUM_SWR_FORMATS, "Invalid Surface Format: %d", format);
+    SWR_ASSERT(format < NUM_SWR_FORMATS, "Invalid Surface Format: %d", format);
     SWR_ASSERT(gFormatInfo[format].name != nullptr, "Invalid Surface Format: %d", format);
     return gFormatInfo[format];
 }