1d1ddbaa562fb3a60c4c612e1b6a8d0e2e402d0a
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-10-17 13:58:27 +0200

    i965: unify the code to enable of ARB_gpu_shader_fp64 and ARB_vertex_attrib_64bit for HSW+
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -218,11 +218,6 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.OES_primitive_bounding_box = true;
       ctx->Extensions.OES_texture_buffer = true;
 
-      if (brw->is_haswell) {
-         ctx->Extensions.ARB_gpu_shader_fp64 = true;
-         ctx->Extensions.ARB_vertex_attrib_64bit = true;
-      }
-
       if (can_do_pipelined_register_writes(brw->screen)) {
          ctx->Extensions.ARB_draw_indirect = true;
          ctx->Extensions.ARB_transform_feedback2 = true;
@@ -242,8 +237,10 @@ intelInitExtensions(struct gl_context *ctx)
    }
 
    if (brw->gen >= 8 || brw->is_haswell) {
+      ctx->Extensions.ARB_gpu_shader_fp64 = true;
       ctx->Extensions.ARB_stencil_texturing = true;
       ctx->Extensions.ARB_texture_stencil8 = true;
+      ctx->Extensions.ARB_vertex_attrib_64bit = true;
       ctx->Extensions.OES_geometry_shader = true;
       ctx->Extensions.OES_texture_cube_map_array = true;
       ctx->Extensions.OES_viewport_array = true;
@@ -266,9 +263,7 @@ intelInitExtensions(struct gl_context *ctx)
    }
 
    if (brw->gen >= 8) {
-      ctx->Extensions.ARB_gpu_shader_fp64 = true;
       ctx->Extensions.ARB_shader_precision = true;
-      ctx->Extensions.ARB_vertex_attrib_64bit = true;
       ctx->Extensions.ARB_ES3_2_compatibility = true;
    }
 
