9b9fb6d212147d4d240285b70fd7346d1326d212
Jason Ekstrand <jason.ekstrand@intel.com>
2016-10-27 01:33:39 -0700

    util/vk_alloc: Add a vk_zalloc2 helper
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

--- src/util/vk_alloc.h
+++ src/util/vk_alloc.h
@@ -25,6 +25,7 @@
 
 /* common allocation inlines for vulkan drivers */
 
+#include <string.h>
 #include <vulkan/vulkan.h>
 
 static inline void *
@@ -62,6 +63,21 @@ vk_alloc2(const VkAllocationCallbacks *parent_alloc,
       return vk_alloc(alloc, size, align, scope);
    else
       return vk_alloc(parent_alloc, size, align, scope);
+}
+
+static inline void *
+vk_zalloc2(const VkAllocationCallbacks *parent_alloc,
+           const VkAllocationCallbacks *alloc,
+           size_t size, size_t align,
+           VkSystemAllocationScope scope)
+{
+   void *mem = vk_alloc2(parent_alloc, alloc, size, align, scope);
+   if (mem == NULL)
+      return NULL;
+
+   memset(mem, 0, size);
+
+   return mem;
 }
 
 static inline void
