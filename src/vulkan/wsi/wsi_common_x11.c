daeb21e47845795d0320811db1e202540fba356b
Jason Ekstrand <jason.ekstrand@intel.com>
2016-11-01 17:18:30 -0700

    vulkan/wsi/x11: Clean up connections in finish_wsi
    Signed-off-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Cc: "13.0" <mesa-stable@lists.freedesktop.org>

--- src/vulkan/wsi/wsi_common_x11.c
+++ src/vulkan/wsi/wsi_common_x11.c
@@ -946,6 +946,10 @@ wsi_x11_finish_wsi(struct wsi_device *wsi_device,
       (struct wsi_x11 *)wsi_device->wsi[VK_ICD_WSI_PLATFORM_XCB];
 
    if (wsi) {
+      struct hash_entry *entry;
+      hash_table_foreach(wsi->connections, entry)
+         wsi_x11_connection_destroy(alloc, entry->data);
+
       _mesa_hash_table_destroy(wsi->connections, NULL);
 
       pthread_mutex_destroy(&wsi->mutex);
