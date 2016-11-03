7bcb94bc2fc45fde806ad3fd062bf2ce97342359
Jordan Justen <jordan.l.justen@intel.com>
2016-11-03 15:22:11 -0700

    i965/compute: Allow ARB_compute_shader in compat profile
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=97447
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Tested-by: Evan Odabashian <eodabash@gmail.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -501,7 +501,7 @@ brw_initialize_context_constants(struct brw_context *brw)
       [MESA_SHADER_GEOMETRY] = brw->gen >= 6,
       [MESA_SHADER_FRAGMENT] = true,
       [MESA_SHADER_COMPUTE] =
-         (ctx->API == API_OPENGL_CORE &&
+         ((ctx->API == API_OPENGL_COMPAT || ctx->API == API_OPENGL_CORE) &&
           ctx->Const.MaxComputeWorkGroupSize[0] >= 1024) ||
          (ctx->API == API_OPENGLES2 &&
           ctx->Const.MaxComputeWorkGroupSize[0] >= 128) ||
