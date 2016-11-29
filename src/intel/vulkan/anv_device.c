eaf0768b8f9a9fd76b44a4d60826ef1f42fc6a46
Dave Airlie <airlied@redhat.com>
2016-11-29 11:16:56 +1000

    radv: set maxFragmentDualSrcAttachments to 1
    Reported-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Cc: "13.0" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -555,7 +555,7 @@ void anv_GetPhysicalDeviceProperties(
       .maxGeometryTotalOutputComponents         = 1024,
       .maxFragmentInputComponents               = 128,
       .maxFragmentOutputAttachments             = 8,
-      .maxFragmentDualSrcAttachments            = 2,
+      .maxFragmentDualSrcAttachments            = 1,
       .maxFragmentCombinedOutputResources       = 8,
       .maxComputeSharedMemorySize               = 32768,
       .maxComputeWorkGroupCount                 = { 65535, 65535, 65535 },
