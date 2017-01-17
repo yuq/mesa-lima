0079df0b2d126c476a3e8b40adb25a994393b7f2
Eric Anholt <eric@anholt.net>
2017-01-18 10:38:55 +1100

    vc4: Flip the switch to run the GLSL compiler optimization loop once.
    This has almost no effect on shader-db:
    
    total instructions in shared programs: 92572 -> 92611 (0.04%)
    instructions in affected programs:     4486 -> 4525 (0.87%)
    
    Looking at 2 of the 7 different shaders that were hurt (all of which were
    in mupen64), they all appear to be just differences in order of
    instructions at the NIR level.
    
    The advantage is that this should significantly reduce time in the compiler.

--- src/gallium/drivers/vc4/vc4_screen.c
+++ src/gallium/drivers/vc4/vc4_screen.c
@@ -126,6 +126,7 @@ vc4_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
         case PIPE_CAP_USER_INDEX_BUFFERS:
         case PIPE_CAP_TEXTURE_MULTISAMPLE:
         case PIPE_CAP_TEXTURE_SWIZZLE:
+        case PIPE_CAP_GLSL_OPTIMIZE_CONSERVATIVELY:
                 return 1;
 
                 /* lying for GL 2.0 */
@@ -241,7 +242,6 @@ vc4_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
         case PIPE_CAP_TGSI_ARRAY_COMPONENTS:
         case PIPE_CAP_TGSI_CAN_READ_OUTPUTS:
         case PIPE_CAP_NATIVE_FENCE_FD:
-        case PIPE_CAP_GLSL_OPTIMIZE_CONSERVATIVELY:
         case PIPE_CAP_TGSI_FS_FBFETCH:
         case PIPE_CAP_TGSI_MUL_ZERO_WINS:
         case PIPE_CAP_INT64:
