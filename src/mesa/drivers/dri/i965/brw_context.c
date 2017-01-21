2f7a7ae13196c58eddea75fc51637f8c2a8579b0
Kenneth Graunke <kenneth@whitecape.org>
2017-01-20 20:33:57 -0800

    i965: Support the force_glsl_version driconf option.
    Gallium drivers have had this for a while.  It makes sense to support
    it consistently across drivers, so expose it in i965 as well.
    
    Cc: "17.0" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -908,6 +908,9 @@ brw_process_driconf_options(struct brw_context *brw)
    ctx->Const.ForceGLSLExtensionsWarn =
       driQueryOptionb(options, "force_glsl_extensions_warn");
 
+   ctx->Const.ForceGLSLVersion =
+      driQueryOptioni(options, "force_glsl_version");
+
    ctx->Const.DisableGLSLLineContinuations =
       driQueryOptionb(options, "disable_glsl_line_continuations");
 
