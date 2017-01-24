b01f26e0056b6efb96d91cb769cda0ef18c0044a
Tim Rowley <timothy.o.rowley@intel.com>
2017-01-24 12:37:13 -0600

    swr: [rasterizer jitter] Multisample blend jit fix
    Reviewed-by: Bruce Cherniak <bruce.cherniak@intel.com>

--- src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
+++ src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
@@ -776,9 +776,9 @@ struct BlendJit : public Builder
         if(state.desc.sampleMaskEnable || state.desc.alphaToCoverageEnable ||
            state.desc.oMaskEnable)
         {
-            // load current mask
+            // load coverage mask
             Value* pMask = LOAD(ppMask);
-            currentMask = S_EXT(ICMP_SGT(currentMask, VBROADCAST(C(0))), mSimdInt32Ty);
+            currentMask = S_EXT(ICMP_UGT(currentMask, VBROADCAST(C(0))), mSimdInt32Ty);
             Value* outputMask = AND(pMask, currentMask);
             // store new mask
             STORE(outputMask, GEP(ppMask, C(0)));
