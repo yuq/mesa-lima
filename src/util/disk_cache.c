yuq/mesa-lima

commit d7b3707c612027b354deea6bc5eae56a02d5f8d5

util/disk_cache: use stat() to check if entry is a directory
d_type is not supported on all systems.

Tested-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=97967

src/util/disk_cache.c
@@ -375,7 +375,8 @@ make_cache_file_directory(struct disk_cache *cache, cache_key key)
  */
 static char *
 choose_random_file_matching(const char *dir_path,
-                            bool (*predicate)(struct dirent *))
+                            bool (*predicate)(struct dirent *,
+                                              const char *dir_path))
 {
    DIR *dir;
    struct dirent *entry;
@@ -392,7 +393,7 @@ choose_random_file_matching(const char *dir_path,
       entry = readdir(dir);
       if (entry == NULL)
          break;
-      if (! predicate(entry))
+      if (!predicate(entry, dir_path))
          continue;
 
       count++;
@@ -412,7 +413,7 @@ choose_random_file_matching(const char *dir_path,
       entry = readdir(dir);
       if (entry == NULL)
          break;
-      if (! predicate(entry))
+      if (!predicate(entry, dir_path))
          continue;
       if (count == victim)
          break;
@@ -437,14 +438,20 @@ choose_random_file_matching(const char *dir_path,
  * ".tmp"
  */
 static bool
-is_regular_non_tmp_file(struct dirent *entry)
+is_regular_non_tmp_file(struct dirent *entry, const char *path)
 {
-   size_t len;
-
-   if (entry->d_type != DT_REG)
+   char *filename;
+   if (asprintf(&filename, "%s/%s", path, entry->d_name) == -1)
       return false;
 
-   len = strlen (entry->d_name);
+   struct stat sb;
+   stat(filename, &sb);
+   free(filename);
+
+   if (!S_ISREG(sb.st_mode))
+      return false;
+
+   size_t len = strlen (entry->d_name);
    if (len >= 4 && strcmp(&entry->d_name[len-4], ".tmp") == 0)
       return false;
 
@@ -478,9 +485,17 @@ unlink_random_file_from_directory(const char *path)
  * special name of "..")
  */
 static bool
-is_two_character_sub_directory(struct dirent *entry)
+is_two_character_sub_directory(struct dirent *entry, const char *path)
 {
-   if (entry->d_type != DT_DIR)
+   char *subdir;
+   if (asprintf(&subdir, "%s/%s", path, entry->d_name) == -1)
+      return false;
+
+   struct stat sb;
+   stat(path, &sb);
+   free(subdir);
+
+   if (!S_ISDIR(sb.st_mode))
       return false;
 
    if (strlen(entry->d_name) != 2)

commit 0cbde643eb29c8bf19eb8551ac8e821f7f733212

util/disk_cache: correctly use stat(3)
I forgot to error check stat() and also I wasn't using the subdir in
is_two_character_sub_directory().

Fixes: d7b3707c612 "util/disk_cache: use stat() to check if entry is a directory"
Reviewed-by: Plamena Manolova <plamena.manolova@intel.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

src/util/disk_cache.c
@@ -445,10 +445,10 @@ is_regular_non_tmp_file(struct dirent *entry, const char *path)
       return false;
 
    struct stat sb;
-   stat(filename, &sb);
+   int res = stat(filename, &sb);
    free(filename);
 
-   if (!S_ISREG(sb.st_mode))
+   if (res == -1 || !S_ISREG(sb.st_mode))
       return false;
 
    size_t len = strlen (entry->d_name);
@@ -492,10 +492,10 @@ is_two_character_sub_directory(struct dirent *entry, const char *path)
       return false;
 
    struct stat sb;
-   stat(path, &sb);
+   int res = stat(subdir, &sb);
    free(subdir);
 
-   if (!S_ISDIR(sb.st_mode))
+   if (res == -1 || !S_ISDIR(sb.st_mode))
       return false;
 
    if (strlen(entry->d_name) != 2)