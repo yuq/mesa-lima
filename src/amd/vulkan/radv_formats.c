2b3c490e23a6cd7f4f6bb489267c2b9b4fde5b20
Dave Airlie <airlied@redhat.com>
2017-02-19 16:38:16 +1000

    radv: fix typo in a2b10g10r10 fast clear calculation.
    This fixes:
    dEQP-VK.renderpass.formats.a2b10g10r10_unorm_pack32*
    regressions.
    
    Fixes:
    f22836dbdd radv: Add CPU color packing for VK_FORMAT_A2B10G10R10_UNORM_PACK32.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_formats.c
+++ src/amd/vulkan/radv_formats.c
@@ -944,7 +944,7 @@ bool radv_format_pack_clear_color(VkFormat format,
 		clear_vals[0] = ((uint16_t)util_iround(CLAMP(value->float32[0], 0.0f, 1.0f) * 0x3ff)) & 0x3ff;
 		clear_vals[0] |= (((uint16_t)util_iround(CLAMP(value->float32[1], 0.0f, 1.0f) * 0x3ff)) & 0x3ff) << 10;
 		clear_vals[0] |= (((uint16_t)util_iround(CLAMP(value->float32[2], 0.0f, 1.0f) * 0x3ff)) & 0x3ff) << 20;
-		clear_vals[0] |= (((uint16_t)util_iround(CLAMP(value->float32[1], 0.0f, 1.0f) * 0x3)) & 0x3) << 30;
+		clear_vals[0] |= (((uint16_t)util_iround(CLAMP(value->float32[3], 0.0f, 1.0f) * 0x3)) & 0x3) << 30;
 		clear_vals[1] = 0;
 		return true;
 	case VK_FORMAT_R32G32_SFLOAT:
