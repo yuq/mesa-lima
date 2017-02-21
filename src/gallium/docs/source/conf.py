89af6bf2cb1c705cac2a9a7dd77bde5c1d3b0b99
Eric Engestrom <eric.engestrom@imgtec.com>
2017-02-21 14:15:39 +0000

    gallium/docs: use imgmath instead of pngmath
    	WARNING: sphinx.ext.pngmath has been deprecated. Please use
    	sphinx.ext.imgmath instead.
    
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/docs/source/conf.py
+++ src/gallium/docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.append(os.path.abspath('exts'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath', 'sphinx.ext.graphviz', 'formatting']
+extensions = ['sphinx.ext.imgmath', 'sphinx.ext.graphviz', 'formatting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
