yuq/mesa-lima

commit 207e3a6e4b6d70a965f2852204c35ccf42fe3533

util/radv: move *_get_function_timestamp() to utils
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/util/disk_cache.h
@@ -24,8 +24,10 @@
 #ifndef DISK_CACHE_H
 #define DISK_CACHE_H
 
+#include <dlfcn.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,6 +39,21 @@ extern "C" {
 typedef uint8_t cache_key[CACHE_KEY_SIZE];
 
 struct disk_cache;
+
+static inline bool
+disk_cache_get_function_timestamp(void *ptr, uint32_t* timestamp)
+{
+	Dl_info info;
+	struct stat st;
+	if (!dladdr(ptr, &info) || !info.dli_fname) {
+		return false;
+	}
+	if (stat(info.dli_fname, &st)) {
+		return false;
+	}
+	*timestamp = st.st_mtim.tv_sec;
+	return true;
+}
 
 /* Provide inlined stub functions if the shader cache is disabled. */
 

src/amd/vulkan/radv_device.c
@@ -25,14 +25,13 @@
  * IN THE SOFTWARE.
  */
 
-#include <dlfcn.h>
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include "radv_private.h"
 #include "radv_cs.h"
+#include "util/disk_cache.h"
 #include "util/strtod.h"
 
 #include <xf86drm.h>
@@ -46,28 +45,13 @@
 #include "util/debug.h"
 
 static int
-radv_get_function_timestamp(void *ptr, uint32_t* timestamp)
-{
-	Dl_info info;
-	struct stat st;
-	if (!dladdr(ptr, &info) || !info.dli_fname) {
-		return -1;
-	}
-	if (stat(info.dli_fname, &st)) {
-		return -1;
-	}
-	*timestamp = st.st_mtim.tv_sec;
-	return 0;
-}
-
-static int
 radv_device_get_cache_uuid(enum radeon_family family, void *uuid)
 {
 	uint32_t mesa_timestamp, llvm_timestamp;
 	uint16_t f = family;
 	memset(uuid, 0, VK_UUID_SIZE);
-	if (radv_get_function_timestamp(radv_device_get_cache_uuid, &mesa_timestamp) ||
-	    radv_get_function_timestamp(LLVMInitializeAMDGPUTargetInfo, &llvm_timestamp))
+	if (!disk_cache_get_function_timestamp(radv_device_get_cache_uuid, &mesa_timestamp) ||
+	    !disk_cache_get_function_timestamp(LLVMInitializeAMDGPUTargetInfo, &llvm_timestamp))
 		return -1;
 
 	memcpy(uuid, &mesa_timestamp, 4);

commit 30a4b25efe005c922f048beaf62852714d2462a1

util/disk_cache: Use backward compatible st_mtime.
Fix Mac OS X build error.

  CC       libmesautil_la-disk_cache.lo
In file included from disk_cache.c:46:
./disk_cache.h:57:20: error: no member named 'st_mtim' in 'struct stat'
   *timestamp = st.st_mtim.tv_sec;
                ~~ ^

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=99918
Fixes: 207e3a6e4b ("util/radv: move *_get_function_timestamp() to utils")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/util/disk_cache.h
@@ -54,7 +54,7 @@ disk_cache_get_function_timestamp(void *ptr, uint32_t* timestamp)
    if (stat(info.dli_fname, &st)) {
       return false;
    }
-   *timestamp = st.st_mtim.tv_sec;
+   *timestamp = st.st_mtime;
    return true;
 #else
    return false;