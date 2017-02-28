182d48ceb9e58eb53b52436b2cd6010de072d29b
Emil Velikov <emil.velikov@collabora.com>
2017-02-28 12:08:52 +0000

    glcpp/tests/glcpp-test: print only the test basename
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Acked-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- src/compiler/glsl/glcpp/tests/glcpp-test.sh
+++ src/compiler/glsl/glcpp/tests/glcpp-test.sh
@@ -73,7 +73,7 @@ echo "====== Testing for correctness ======"
 for test in $testdir/*.c; do
     out=$outdir/${test##*/}.out
 
-    printf "Testing $test... > $out ($test.expected) "
+    printf "Testing `basename $test`... "
     $glcpp $(test_specific_args $test) < $test > $out 2>&1
     total=$((total+1))
     if cmp $test.expected $out >/dev/null 2>&1; then
@@ -92,7 +92,7 @@ echo ""
 if [ "$do_valgrind" = "yes" ]; then
     echo "====== Testing for valgrind cleanliness ======"
     for test in $testdir/*.c; do
-	printf "Testing $test with valgrind..."
+	printf "Testing `basename $test` with valgrind..."
 	valgrind --error-exitcode=31 --log-file=$test.valgrind-errors $glcpp $(test_specific_args $test) < $test >/dev/null 2>&1
 	if [ "$?" = "31" ]; then
 	    echo "ERRORS"
