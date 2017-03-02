01548f9f01644623ff7eb9778fc78f2eb90fc99a
Matt Turner <mattst88@gmail.com>
2017-03-02 11:24:19 -0800

    nir: Return progress from nir_lower_atomics().
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2540,7 +2540,7 @@ typedef struct nir_lower_bitmap_options {
 
 void nir_lower_bitmap(nir_shader *shader, const nir_lower_bitmap_options *options);
 
-void nir_lower_atomics(nir_shader *shader,
+bool nir_lower_atomics(nir_shader *shader,
                        const struct gl_shader_program *shader_program);
 bool nir_lower_to_source_mods(nir_shader *shader);
 
