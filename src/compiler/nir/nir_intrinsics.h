60d1aac28a1f44ac166e72262e378e063155d6fd
Jason Ekstrand <jason.ekstrand@intel.com>
2017-03-02 17:39:11 -0800

    nir/intrinsics: Make load_barycentric_input take a 2-component coor
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
    Cc: "17.0 13.0" <mesa-stable@lists.freedesktop.org>

--- src/compiler/nir/nir_intrinsics.h
+++ src/compiler/nir/nir_intrinsics.h
@@ -401,7 +401,9 @@ LOAD(input, 1, 2, BASE, COMPONENT, xx, NIR_INTRINSIC_CAN_ELIMINATE | NIR_INTRINS
 /* src[] = { vertex, offset }. const_index[] = { base, component } */
 LOAD(per_vertex_input, 2, 2, BASE, COMPONENT, xx, NIR_INTRINSIC_CAN_ELIMINATE | NIR_INTRINSIC_CAN_REORDER)
 /* src[] = { barycoord, offset }. const_index[] = { base, component } */
-LOAD(interpolated_input, 2, 2, BASE, COMPONENT, xx, NIR_INTRINSIC_CAN_ELIMINATE | NIR_INTRINSIC_CAN_REORDER)
+INTRINSIC(load_interpolated_input, 2, ARR(2, 1), true, 0, 0,
+          2, BASE, COMPONENT, xx,
+          NIR_INTRINSIC_CAN_ELIMINATE | NIR_INTRINSIC_CAN_REORDER)
 
 /* src[] = { buffer_index, offset }. No const_index */
 LOAD(ssbo, 2, 0, xx, xx, xx, NIR_INTRINSIC_CAN_ELIMINATE)
