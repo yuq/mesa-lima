yuq/mesa-lima

commit 633c959faeae5099fd095f27da7b954e4a36254b

getteximage: Return correct error value when texure object is not found
glGetTextureSubImage() and glGetCompressedTextureSubImage() are currently
returning INVALID_OPERATION error when the passed texture argument does not
correspond to an existing texture object. However, the error should be
INVALID_VALUE instead. From OpenGL 4.5 spec PDF, section '8.11. Texture
Queries', page 236:

    "An INVALID_VALUE error is generated if texture is not the name of
     an existing texture object."

Same wording applies to the compressed version.

The INVALID_OPERATION error is coming from the call to
_mesa_lookup_texture_err(). This patch uses _mesa_lookup_texture() instead
and emits the correct error in the caller.

Fixes: GL45-CTS.get_texture_sub_image.errors_test

Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/mesa/main/texgetimage.c
@@ -1459,9 +1459,10 @@ _mesa_GetTextureSubImage(GLuint texture, GLint level,
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetTextureSubImage";
    struct gl_texture_object *texObj =
-      _mesa_lookup_texture_err(ctx, texture, caller);
+      _mesa_lookup_texture(ctx, texture);
 
    if (!texObj) {
+      _mesa_error(ctx, GL_INVALID_VALUE, "%s(texture)", caller);
       return;
    }
 
@@ -1775,8 +1776,9 @@ _mesa_GetCompressedTextureSubImage(GLuint texture, GLint level,
    static const char *caller = "glGetCompressedTextureImage";
    struct gl_texture_object *texObj;
 
-   texObj = _mesa_lookup_texture_err(ctx, texture, caller);
+   texObj = _mesa_lookup_texture(ctx, texture);
    if (!texObj) {
+      _mesa_error(ctx, GL_INVALID_VALUE, "%s(texture)", caller);
       return;
    }
 

commit 5ae54c0cf7acfb792d2ca9b22cd5fb5ba811d32c

getteximage: avoid to lookup textures with id 0
This fixes the following assertion when the key is 0.

main/hash.c:181: _mesa_HashLookup_unlocked: Assertion `key' failed.

Fixes: 633c959fae ("getteximage: Return correct error value when texure object is not found")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/mesa/main/texgetimage.c
@@ -1458,7 +1458,9 @@ _mesa_GetTextureSubImage(GLuint texture, GLint level,
 {
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetTextureSubImage";
-   struct gl_texture_object *texObj =
+   struct gl_texture_object *texObj = NULL;
+
+   if (texture > 0)
       _mesa_lookup_texture(ctx, texture);
 
    if (!texObj) {
@@ -1774,9 +1776,11 @@ _mesa_GetCompressedTextureSubImage(GLuint texture, GLint level,
 {
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetCompressedTextureImage";
-   struct gl_texture_object *texObj;
+   struct gl_texture_object *texObj = NULL;
 
-   texObj = _mesa_lookup_texture(ctx, texture);
+   if (texture > 0)
+      texObj = _mesa_lookup_texture(ctx, texture);
+
    if (!texObj) {
       _mesa_error(ctx, GL_INVALID_VALUE, "%s(texture)", caller);
       return;