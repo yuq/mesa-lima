3d253d330a47eb06bd4745c3ee41c4aa9d6143be
Matt Turner <mattst88@gmail.com>
2017-03-10 23:15:33 -0800

    i965: Initialize compaction tables in unit test.
    Fixes: fa4b792e833a "i965: Move brw_init_compaction_tables() to brw_create_compiler()."
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=100154

--- src/mesa/drivers/dri/i965/test_eu_compact.c
+++ src/mesa/drivers/dri/i965/test_eu_compact.c
@@ -251,6 +251,7 @@ struct {
 static bool
 run_tests(const struct gen_device_info *devinfo)
 {
+   brw_init_compaction_tables(devinfo);
    bool fail = false;
 
    for (int i = 0; i < ARRAY_SIZE(tests); i++) {
