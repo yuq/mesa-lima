e325fc12dbdea2a0fdf10b279be39cf15cf2ca8b
Emil Velikov <emil.l.velikov@gmail.com>
2017-03-20 16:04:41 +0000

    util: inline strndup implementation in the header
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Acked-by: Vedran Miletić <vedran@miletic.net>
    Acked-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
    Reviewed-by: Edward O'Callaghan <funfunctor@folklore1984.net>

--- src/util/strndup.h
+++ src/util/strndup.h
@@ -28,16 +28,34 @@
 
 #if defined(_WIN32)
 
+#include <string.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-char *strndup(const char *str, size_t max);
+static inline char *
+strndup(const char *str, size_t max)
+{
+   size_t n;
+   char *ptr;
+
+   if (!str)
+      return NULL;
+
+   n = strnlen(str, max);
+   ptr = (char *) calloc(n + 1, sizeof(char));
+   if (!ptr)
+      return NULL;
+
+   memcpy(ptr, str, n);
+   return ptr;
+}
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* _WIN32 */
 
 #endif /* STRNDUP_H */
