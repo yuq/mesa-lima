b3375800d741bfc51d62049294f297a03d7cec76
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-04-11 13:54:44 +0200

    virgl: add missing PIPE_CAP_DOUBLES
    Fixes the following Clang warning.
    
    virgl_screen.c:60:12: warning: enumeration value 'PIPE_CAP_DOUBLES' not handled in switch [-Wswitch]
       switch (param) {
               ^
    1 warning generated.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/drivers/virgl/virgl_screen.c
+++ src/gallium/drivers/virgl/virgl_screen.c
@@ -260,6 +260,7 @@ virgl_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_POLYGON_MODE_FILL_RECTANGLE:
    case PIPE_CAP_SPARSE_BUFFER_PAGE_SIZE:
    case PIPE_CAP_TGSI_BALLOT:
+   case PIPE_CAP_DOUBLES:
       return 0;
    case PIPE_CAP_VENDOR_ID:
       return 0x1af4;
