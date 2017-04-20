019d5d534682145a3d5921b061ea46f8c872d6a0
Charmaine Lee <charmainel@vmware.com>
2017-04-19 18:34:36 -0700

    svga: use the winsys interface to invalidate surface
    Instead of directly sending the InvalidateGBSurface command,
    this patch uses the invalidate_surface interface.
    
    Fixes Linux VM piglit failures including
       ext_texture_array-gen-mipmap, fbo-generatemipmap-array S3TC_DXT1
    
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/drivers/svga/svga_surface.c
+++ src/gallium/drivers/svga/svga_surface.c
@@ -524,11 +524,7 @@ svga_validate_surface_view(struct svga_context *svga, struct svga_surface *s)
           * need to update the host-side copy with the invalid
           * content when the associated mob is first bound to the surface.
           */
-         ret = SVGA3D_InvalidateGBSurface(svga->swc, stex->handle);
-         if (ret != PIPE_OK) {
-            s = NULL;
-            goto done;
-         }
+         svga->swc->surface_invalidate(svga->swc, stex->handle);
          stex->validated = TRUE;
       }
 
