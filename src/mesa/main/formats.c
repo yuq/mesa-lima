30782962263d21e984826aef616e25f4ec82de2d
Eric Anholt <eric@anholt.net>
2017-05-01 11:21:27 -0700

    mesa: Add X1B5G5R5 along with A1B5G5R5.
    For supporting RGB5 in hardware with A in the low bit (vc4), we need this
    format as well.
    
    v2: Add proper _mesa_format_matches_format_and_type() support (from
        Nicolai).
    
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com> (v1)

--- src/mesa/main/formats.c
+++ src/mesa/main/formats.c
@@ -875,6 +875,7 @@ _mesa_uncompressed_format_to_type_and_comps(mesa_format format,
 
    case MESA_FORMAT_A1B5G5R5_UNORM:
    case MESA_FORMAT_A1B5G5R5_UINT:
+   case MESA_FORMAT_X1B5G5R5_UNORM:
       *datatype = GL_UNSIGNED_SHORT_5_5_5_1;
       *comps = 4;
       return;
@@ -1524,6 +1525,10 @@ _mesa_format_matches_format_and_type(mesa_format mesa_format,
 
    case MESA_FORMAT_A1B5G5R5_UNORM:
       return format == GL_RGBA && type == GL_UNSIGNED_SHORT_5_5_5_1 &&
+         !swapBytes;
+
+   case MESA_FORMAT_X1B5G5R5_UNORM:
+      return format == GL_RGB && type == GL_UNSIGNED_SHORT_5_5_5_1 &&
          !swapBytes;
 
    case MESA_FORMAT_B5G5R5A1_UNORM:
