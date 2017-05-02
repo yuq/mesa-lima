396f9ae52f9622e564a7c0912277411d4228292f
Fredrik Höglund <fredrik@kde.org>
2017-05-02 17:09:27 +0200

    radv/meta: fix restoring a push descriptor set
    radv_bind_descriptor_set cannot be used to bind a push descriptor set
    since a push descriptor set does not have a buffer list. However,
    there is no need to add the buffers again when restoring a set, so
    this fix is also an optimization.
    
    Cc: "17.1" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Fredrik Höglund <fredrik@kde.org>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    (cherry picked from commit 5ff48581119258214801de24a327bdd6a29e0ccc)
    [Emil Velikov: resolve trivial conflicts]
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    
    Conflicts:
    	src/amd/vulkan/radv_meta.c

--- src/amd/vulkan/radv_meta.c
+++ src/amd/vulkan/radv_meta.c
@@ -52,7 +52,9 @@ radv_meta_restore(const struct radv_meta_saved_state *state,
 		  struct radv_cmd_buffer *cmd_buffer)
 {
 	cmd_buffer->state.pipeline = state->old_pipeline;
-	radv_bind_descriptor_set(cmd_buffer, state->old_descriptor_set0, 0);
+
+	cmd_buffer->state.descriptors[0] = state->old_descriptor_set0;
+	cmd_buffer->state.descriptors_dirty |= (1u << 0);
 	memcpy(cmd_buffer->state.vertex_bindings, state->old_vertex_bindings,
 	       sizeof(state->old_vertex_bindings));
 
@@ -110,7 +112,9 @@ radv_meta_restore_compute(const struct radv_meta_saved_compute_state *state,
 {
 	radv_CmdBindPipeline(radv_cmd_buffer_to_handle(cmd_buffer), VK_PIPELINE_BIND_POINT_COMPUTE,
 			     radv_pipeline_to_handle(state->old_pipeline));
-	radv_bind_descriptor_set(cmd_buffer, state->old_descriptor_set0, 0);
+
+	cmd_buffer->state.descriptors[0] = state->old_descriptor_set0;
+	cmd_buffer->state.descriptors_dirty |= (1u << 0);
 
 	if (push_constant_size) {
 		memcpy(cmd_buffer->push_constants, state->push_constants, push_constant_size);
