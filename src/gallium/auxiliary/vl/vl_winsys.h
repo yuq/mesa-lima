369e5dd939b4af6c653d6cbbe9be257a9c2c950e
Emil Velikov <emil.velikov@collabora.com>
2017-05-02 17:53:17 +0100

    auxiliary/vl: use vl_*_screen_create stubs when building w/o platform
    Provide a dummy stub when the user has opted w/o said platform, thus
    we can build the binaries without unnecessarily requiring X11/other
    headers.
    
    In order to avoid build and link-time issues, we remove the HAVE_DRI3
    guards in the VA and VDPAU state-trackers.
    
    With this change st/va will return VA_STATUS_ERROR_ALLOCATION_FAILED
    instead of VA_STATUS_ERROR_UNIMPLEMENTED. That is fine since upstream
    users of libva such as vlc and mpv do little error checking, let
    alone distinguish between the two.
    
    Cc: Leo Liu <leo.liu@amd.com>
    Cc: Guttula, Suresh <Suresh.Guttula@amd.com>
    Cc: mesa-stable@lists.freedesktop.org
    Cc: Christian König <christian.koenig@amd.com>
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

--- src/gallium/auxiliary/vl/vl_winsys.h
+++ src/gallium/auxiliary/vl/vl_winsys.h
@@ -32,7 +32,9 @@
 #ifndef vl_winsys_h
 #define vl_winsys_h
 
+#ifdef HAVE_X11_PLATFORM
 #include <X11/Xlib.h>
+#endif
 #include "pipe/p_defines.h"
 #include "pipe/p_format.h"
 
@@ -68,15 +70,28 @@ struct vl_screen
    struct pipe_loader_device *dev;
 };
 
+#ifdef HAVE_X11_PLATFORM
 struct vl_screen *
 vl_dri2_screen_create(Display *display, int screen);
+#else
+static inline struct vl_screen *
+vl_dri2_screen_create(void *display, int screen) { return NULL; };
+#endif
 
-struct vl_screen *
-vl_drm_screen_create(int fd);
-
-#if defined(HAVE_DRI3)
+#if defined(HAVE_X11_PLATFORM) && defined(HAVE_DRI3)
 struct vl_screen *
 vl_dri3_screen_create(Display *display, int screen);
+#else
+static inline struct vl_screen *
+vl_dri3_screen_create(void *display, int screen) { return NULL; };
+#endif
+
+#ifdef HAVE_DRM_PLATFORM
+struct vl_screen *
+vl_drm_screen_create(int fd);
+#else
+static inline struct vl_screen *
+vl_drm_screen_create(int fd) { return NULL; };
 #endif
 
 #endif
--- src/gallium/state_trackers/vdpau/device.c
+++ src/gallium/state_trackers/vdpau/device.c
@@ -63,9 +63,7 @@ vdp_imp_device_create_x11(Display *display, int screen, VdpDevice *device,
 
    pipe_reference_init(&dev->reference, 1);
 
-#if defined(HAVE_DRI3)
    dev->vscreen = vl_dri3_screen_create(display, screen);
-#endif
    if (!dev->vscreen)
       dev->vscreen = vl_dri2_screen_create(display, screen);
    if (!dev->vscreen) {
