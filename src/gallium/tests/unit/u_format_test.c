d970f773f4d72814721f6f58a71c307a608ab30d
Jose Fonseca <jfonseca@vmware.com>
2017-05-22 18:04:12 +0100

    u_format_test: Ignore S3TC errors.
    This prevents spurious failures when libtxc-dxtn-s2tc is installed.
    
    Note: lp_test_format doesn't need any change since we were already
    ignoring S3TC failures there.
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Reviewed-by: Rhys Kidd <rhyskidd@gmail.com>

--- src/gallium/tests/unit/u_format_test.c
+++ src/gallium/tests/unit/u_format_test.c
@@ -220,6 +220,11 @@ test_format_fetch_rgba_float(const struct util_format_description *format_desc,
       }
    }
 
+   /* Ignore S3TC errors */
+   if (format_desc->layout == UTIL_FORMAT_LAYOUT_S3TC) {
+      success = TRUE;
+   }
+
    if (!success) {
       print_unpacked_rgba_float(format_desc, "FAILED: ", unpacked, " obtained\n");
       print_unpacked_rgba_doubl(format_desc, "        ", test->unpacked, " expected\n");
@@ -250,6 +255,11 @@ test_format_unpack_rgba_float(const struct util_format_description *format_desc,
             }
          }
       }
+   }
+
+   /* Ignore S3TC errors */
+   if (format_desc->layout == UTIL_FORMAT_LAYOUT_S3TC) {
+      success = TRUE;
    }
 
    if (!success) {
@@ -301,6 +311,11 @@ test_format_pack_rgba_float(const struct util_format_description *format_desc,
    /* Ignore NaN */
    if (util_is_double_nan(test->unpacked[0][0][0]))
       success = TRUE;
+
+   /* Ignore S3TC errors */
+   if (format_desc->layout == UTIL_FORMAT_LAYOUT_S3TC) {
+      success = TRUE;
+   }
 
    if (!success) {
       print_packed(format_desc, "FAILED: ", packed, " obtained\n");
@@ -365,6 +380,11 @@ test_format_unpack_rgba_8unorm(const struct util_format_description *format_desc
    if (util_is_double_nan(test->unpacked[0][0][0]))
       success = TRUE;
 
+   /* Ignore S3TC errors */
+   if (format_desc->layout == UTIL_FORMAT_LAYOUT_S3TC) {
+      success = TRUE;
+   }
+
    if (!success) {
       print_unpacked_rgba_8unorm(format_desc, "FAILED: ", unpacked, " obtained\n");
       print_unpacked_rgba_8unorm(format_desc, "        ", expected, " expected\n");
@@ -421,6 +441,11 @@ test_format_pack_rgba_8unorm(const struct util_format_description *format_desc,
    /* Multiple of 255 */
    if ((test->unpacked[0][0][0] * 255.0) != (int)(test->unpacked[0][0][0] * 255.0))
       success = TRUE;
+
+   /* Ignore S3TC errors */
+   if (format_desc->layout == UTIL_FORMAT_LAYOUT_S3TC) {
+      success = TRUE;
+   }
 
    if (!success) {
       print_packed(format_desc, "FAILED: ", packed, " obtained\n");
