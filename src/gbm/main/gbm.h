71b78b6b0c8f42f09d4f9d3a0e72004bded4fbe8
Lucas Stach <l.stach@pengutronix.de>
2017-06-08 20:56:16 +0200

    gbm: add API to to import FD with modifier
    This allows to import an FD with an explicit modifier passed through
    userspace protocols.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Tested-by: Robert Foss <robert.foss@collabora.com>
    Reviewed-by: Daniel Stone <daniels@collabora.com>

--- src/gbm/main/gbm.h
+++ src/gbm/main/gbm.h
@@ -252,6 +252,7 @@ gbm_bo_create_with_modifiers(struct gbm_device *gbm,
 #define GBM_BO_IMPORT_WL_BUFFER         0x5501
 #define GBM_BO_IMPORT_EGL_IMAGE         0x5502
 #define GBM_BO_IMPORT_FD                0x5503
+#define GBM_BO_IMPORT_FD_MODIFIER       0x5504
 
 struct gbm_import_fd_data {
    int fd;
@@ -259,6 +260,17 @@ struct gbm_import_fd_data {
    uint32_t height;
    uint32_t stride;
    uint32_t format;
+};
+
+struct gbm_import_fd_modifier_data {
+   uint32_t width;
+   uint32_t height;
+   uint32_t format;
+   uint32_t num_fds;
+   int fds[4];
+   int strides[4];
+   int offsets[4];
+   uint64_t modifier;
 };
 
 struct gbm_bo *
