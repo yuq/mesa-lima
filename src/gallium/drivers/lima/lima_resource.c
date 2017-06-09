589935793f02258c2195718c3ce5bac34347c095
Qiang Yu <yuq825@gmail.com>
2017-06-09 19:28:44 +0800

    lima: init commit
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

---
+++ src/gallium/drivers/lima/lima_resource.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2017 Lima Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sub license,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "util/u_memory.h"
+#include "util/u_format.h"
+#include "util/u_inlines.h"
+
+#include "state_tracker/drm_driver.h"
+
+#include "lima_screen.h"
+#include "lima_resource.h"
+
+
+static struct pipe_resource *
+lima_resource_create(struct pipe_screen *pscreen,
+                     const struct pipe_resource *templat)
+{
+   struct lima_screen *screen = lima_screen(pscreen);
+   struct lima_resource *res;
+
+   res = CALLOC_STRUCT(lima_resource);
+   if (!res)
+      return NULL;
+
+   res->base = *templat;
+   res->base.screen = pscreen;
+   pipe_reference_init(&res->base.reference, 1);
+
+   /* TODO: mipmap, padding */
+   struct pipe_resource *pres = &res->base;
+   res->stride = util_format_get_stride(pres->format, pres->width0);
+
+   struct lima_bo_create_request req = {
+      .size = res->stride *
+         util_format_get_nblocksy(pres->format, pres->height0) *
+         pres->array_size * pres->depth0,
+      .flags = 0,
+   };
+   if (lima_bo_create(screen->dev, &req, &res->bo)) {
+      FREE(res);
+      return NULL;
+   }
+
+   return pres;
+}
+
+static void
+lima_resource_destroy(struct pipe_screen *pscreen, struct pipe_resource *pres)
+{
+   struct lima_resource *res = lima_resource(pres);
+
+   if (res->bo)
+      lima_bo_free(res->bo);
+
+   FREE(res);
+}
+
+static boolean
+lima_resource_get_handle(struct pipe_screen *pscreen,
+                         struct pipe_context *pctx,
+                         struct pipe_resource *pres,
+                         struct winsys_handle *handle, unsigned usage)
+{
+   struct lima_resource *res = lima_resource(pres);
+   int err;
+
+   switch (handle->type) {
+   case DRM_API_HANDLE_TYPE_SHARED:
+      err = lima_bo_export(res->bo, lima_bo_handle_type_gem_flink_name, &handle->handle);
+      if (err)
+         return FALSE;
+      break;
+   case DRM_API_HANDLE_TYPE_KMS:
+      err = lima_bo_export(res->bo, lima_bo_handle_type_kms, &handle->handle);
+      if (err)
+         return FALSE;
+   default:
+      return FALSE;
+   }
+
+   handle->stride = res->stride;
+   return TRUE;
+}
+
+void
+lima_resource_screen_init(struct lima_screen *screen)
+{
+   screen->base.resource_create = lima_resource_create;
+   screen->base.resource_destroy = lima_resource_destroy;
+   screen->base.resource_get_handle = lima_resource_get_handle;
+}
