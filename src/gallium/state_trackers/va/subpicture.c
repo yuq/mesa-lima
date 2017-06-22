b1a359b7d8a0559412d253101e930a6a45d9af7a
Chandu Babu N <chandu@amd.com>
2017-06-22 10:48:41 +0530

    st/va: Fix leak in VAAPI subpictures
    sampler view allocated in vaAssociateSubpicture is not cleared
    in vaiDeassociateSubpicture.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- src/gallium/state_trackers/va/subpicture.c
+++ src/gallium/state_trackers/va/subpicture.c
@@ -283,6 +283,7 @@ vlVaDeassociateSubpicture(VADriverContextP ctx, VASubpictureID subpicture,
       while (surf->subpics.size && util_dynarray_top(&surf->subpics, vlVaSubpicture *) == NULL)
          (void)util_dynarray_pop(&surf->subpics, vlVaSubpicture *);
    }
+   pipe_sampler_view_reference(&sub->sampler,NULL);
    mtx_unlock(&drv->mutex);
 
    return VA_STATUS_SUCCESS;
