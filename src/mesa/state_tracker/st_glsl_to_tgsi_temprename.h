978c437b1290124619dc7decc6c2e929722eda01
Gert Wollny <gw.fossdev@gmail.com>
2017-06-30 08:35:06 +0200

    mesa/st: glsl_to_tgsi: implement new temporary register lifetime tracker
    This patch adds a class for tracking the life times of temporary registers
    in the glsl to tgsi translation. The algorithm runs in three steps:
    First, in order to minimize the number of needed memory allocations the
    program is scanned to evaluate the number of scopes.
    Then, the program is scanned  second time to record the important register
    access time points: first and last reads and writes and their link to the
    execution scope (loop, if/else branch, switch case).
    In the third step for each register the actual minimal life time is
    evaluated.
    
    In addition, when compiled in debug mode (i.e. NDEBUG is not defined)
    the shaders and estimated temporary life times can be logged to stderr
    by setting the environment variable GLSL_TO_TGSI_RENAME_DEBUG.
    
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

---
+++ src/mesa/state_tracker/st_glsl_to_tgsi_temprename.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2017 Gert Wollny
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef MESA_GLSL_TO_TGSI_TEMPRENAME_H
+#define MESA_GLSL_TO_TGSI_TEMPRENAME_H
+
+#include "st_glsl_to_tgsi_private.h"
+
+/** Storage to record the required life time of a temporary register
+ * begin == end == -1 indicates that the register can be reused without
+ * limitations. Otherwise, "begin" indicates the first instruction in which
+ * a write operation may target this temporary, and end indicates the
+ * last instruction in which a value can be read from this temporary.
+ * Hence, a register R2 can be merged with a register R1 if R1.end <= R2.begin.
+ */
+struct lifetime {
+   int begin;
+   int end;
+};
+
+/** Evaluates the required life times of temporary registers in a shader
+ * @param[in] mem_ctx a memory context that can be used with the ralloc_* functions
+ * @param[in] instructions the shader to be anlzyed
+ * @param[in] ntemps number of temporaries reserved for this shader
+ * @param[in,out] lifetimes memory location to store the estimated required
+ *   life times for each temporary register. The parameter must point to
+ *   allocated memory that can hold ntemps lifetime structures. On output
+ *   the life times contains the life times for the registers with the
+ *   exception of TEMP[0].
+ */
+void
+get_temp_registers_required_lifetimes(void *mem_ctx, exec_list *instructions,
+                                      int ntemps, struct lifetime *lifetimes);
+
+#endif
