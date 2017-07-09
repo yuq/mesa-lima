1ad24faa116d9f8fab78ad3b81ff6a3625016e50
Grigori Goronzy <greg@chown.ath.cx>
2017-07-09 03:27:12 +0200

    mesa/marshal: add marshalling for glClearBuffer*
    Add async marshalling/unmarshalling for all glClearBuffer variants.
    These entry points are commonly used in general and Alien Isolation
    specifically uses glClearBufferiv. Slightly reduces the number of
    thread synchronizations with glthread in that game.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/mesa/main/marshal.h
+++ src/mesa/main/marshal.h
@@ -183,7 +183,10 @@ struct marshal_cmd_BufferSubData;
 struct marshal_cmd_NamedBufferData;
 struct marshal_cmd_NamedBufferSubData;
 struct marshal_cmd_ClearBuffer;
-#define marshal_cmd_ClearBufferfv marshal_cmd_ClearBuffer
+#define marshal_cmd_ClearBufferfv   marshal_cmd_ClearBuffer
+#define marshal_cmd_ClearBufferiv   marshal_cmd_ClearBuffer
+#define marshal_cmd_ClearBufferuiv  marshal_cmd_ClearBuffer
+#define marshal_cmd_ClearBufferfi   marshal_cmd_ClearBuffer
 
 void
 _mesa_unmarshal_Enable(struct gl_context *ctx,
@@ -253,5 +256,29 @@ _mesa_unmarshal_ClearBufferfv(struct gl_context *ctx,
 void GLAPIENTRY
 _mesa_marshal_ClearBufferfv(GLenum buffer, GLint drawbuffer,
                             const GLfloat *value);
+
+void GLAPIENTRY
+_mesa_unmarshal_ClearBufferiv(struct gl_context *ctx,
+                              const struct marshal_cmd_ClearBuffer *cmd);
+
+void GLAPIENTRY
+_mesa_marshal_ClearBufferiv(GLenum buffer, GLint drawbuffer,
+                            const GLint *value);
+
+void GLAPIENTRY
+_mesa_unmarshal_ClearBufferuiv(struct gl_context *ctx,
+                               const struct marshal_cmd_ClearBuffer *cmd);
+
+void GLAPIENTRY
+_mesa_marshal_ClearBufferuiv(GLenum buffer, GLint drawbuffer,
+                             const GLuint *value);
+
+void GLAPIENTRY
+_mesa_unmarshal_ClearBufferfi(struct gl_context *ctx,
+                              const struct marshal_cmd_ClearBuffer *cmd);
+
+void GLAPIENTRY
+_mesa_marshal_ClearBufferfi(GLenum buffer, GLint drawbuffer,
+                            const GLfloat depth, const GLint stencil);
 
 #endif /* MARSHAL_H */
