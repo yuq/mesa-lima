030cfa8eed9a91fe5b5ae59670a3001ac0b0f339
Tim Rowley <timothy.o.rowley@intel.com>
2017-07-19 17:49:17 -0500

    swr/rast: enable USE_SIMD16_FRONTEND by default
    Reviewed-by: Bruce Cherniak <bruce.cherniak@intel.com>

--- src/gallium/drivers/swr/rasterizer/core/knobs.h
+++ src/gallium/drivers/swr/rasterizer/core/knobs.h
@@ -40,7 +40,7 @@
 
 #define ENABLE_AVX512_SIMD16    1
 #define USE_8x2_TILE_BACKEND    1
-#define USE_SIMD16_FRONTEND     0
+#define USE_SIMD16_FRONTEND     1
 
 ///////////////////////////////////////////////////////////////////////////////
 // Architecture validation
