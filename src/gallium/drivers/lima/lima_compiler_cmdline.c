7913857a3eac84cc655dc1e622eb8ef731f63300
Qiang Yu <yuq825@gmail.com>
2017-07-20 21:21:45 +0800

    lima: add nir process to vs state create
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/lima_compiler_cmdline.c
+++ src/gallium/drivers/lima/lima_compiler_cmdline.c
@@ -31,6 +31,7 @@
 #include "mesa/state_tracker/st_nir.h"
 #include "mesa/state_tracker/st_glsl_types.h"
 
+#include "lima_program.h"
 #include "ir/gp/gpir.h"
 
 static void
@@ -38,40 +39,6 @@ print_usage(void)
 {
    printf("Usage: lima_compiler [OPTIONS]... FILE\n");
    printf("    --help            - show this message\n");
-}
-
-static const nir_shader_compiler_options nir_options = {
-   .lower_fpow = true,
-   .lower_ffract = true,
-   .lower_fdiv = true,
-   .lower_fsqrt = true,
-};
-
-static void
-lima_optimize_nir(struct nir_shader *s)
-{
-   bool progress;
-
-   do {
-      progress = false;
-
-      NIR_PASS_V(s, nir_lower_vars_to_ssa);
-      NIR_PASS(progress, s, nir_lower_alu_to_scalar);
-      NIR_PASS(progress, s, nir_lower_phis_to_scalar);
-      NIR_PASS(progress, s, nir_copy_prop);
-      NIR_PASS(progress, s, nir_opt_remove_phis);
-      NIR_PASS(progress, s, nir_opt_dce);
-      NIR_PASS(progress, s, nir_opt_dead_cf);
-      NIR_PASS(progress, s, nir_opt_cse);
-      NIR_PASS(progress, s, nir_opt_peephole_select, 8);
-      NIR_PASS(progress, s, nir_opt_algebraic);
-      NIR_PASS(progress, s, nir_opt_constant_folding);
-      NIR_PASS(progress, s, nir_opt_undef);
-      NIR_PASS(progress, s, nir_opt_loop_unroll,
-               nir_var_shader_in |
-               nir_var_shader_out |
-               nir_var_local);
-   } while (progress);
 }
 
 static void
@@ -129,12 +96,16 @@ main(int argc, char **argv)
    filename[0] = argv[n];
 
    char *ext = rindex(filename[0], '.');
-   unsigned stage = 0;
+   unsigned stage = 0, shader = 0;
 
-   if (!strcmp(ext, ".frag"))
+   if (!strcmp(ext, ".frag")) {
       stage = MESA_SHADER_FRAGMENT;
-   else if (!strcmp(ext, ".vert"))
+      shader = PIPE_SHADER_FRAGMENT;
+   }
+   else if (!strcmp(ext, ".vert")) {
       stage = MESA_SHADER_VERTEX;
+      shader = PIPE_SHADER_VERTEX;
+   }
    else {
       print_usage();
       return -1;
@@ -150,7 +121,8 @@ main(int argc, char **argv)
    if (!prog)
       errx(1, "couldn't parse `%s'", filename[0]);
 
-   nir_shader *nir = glsl_to_nir(prog, stage, &nir_options);
+   nir_shader *nir = glsl_to_nir(prog, stage,
+                                 lima_program_get_compiler_options(shader));
 
    standalone_compiler_cleanup(prog);
 
@@ -241,26 +213,9 @@ main(int argc, char **argv)
    //nir_print_shader(nir, stdout);
 //*/
 
-   lima_optimize_nir(nir);
+   lima_program_optimize_nir(nir);
    printf("\nlima_optimize_nir\n");
    //nir_print_shader(nir, stdout);
-
-   NIR_PASS_V(nir, nir_lower_locals_to_regs);
-   printf("\nnir_lower_locals_to_regs\n");
-   //nir_print_shader(nir, stdout);
-
-   NIR_PASS_V(nir, nir_convert_from_ssa, true);
-   printf("\nnir_convert_from_ssa\n");
-   //nir_print_shader(nir, stdout);
-/*
-   NIR_PASS_V(nir, nir_move_vec_src_uses_to_dest);
-   printf("\nnir_move_vec_src_uses_to_dest\n");
-   //nir_print_shader(nir, stdout);
-
-   NIR_PASS_V(nir, nir_lower_vec_to_movs);
-   printf("\nnir_lower_vec_to_movs\n");
-   //nir_print_shader(nir, stdout);
-//*/
 
    nir_print_shader(nir, stdout);
 
