02badb1f9706514f0ab0f4ebe53e95753d6ed087
Qiang Yu <yuq825@gmail.com>
2017-07-20 18:31:15 +0800

    lima: more nir command compiler
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/lima_compiler_cmdline.c
+++ src/gallium/drivers/lima/lima_compiler_cmdline.c
@@ -29,6 +29,7 @@
 #include "compiler/glsl/standalone.h"
 #include "compiler/glsl/glsl_to_nir.h"
 #include "mesa/state_tracker/st_nir.h"
+#include "mesa/state_tracker/st_glsl_types.h"
 
 #include "ir/gp/gpir.h"
 
@@ -73,7 +74,42 @@ lima_optimize_nir(struct nir_shader *s)
    } while (progress);
 }
 
-#define PRINT_NIR 0
+static void
+insert_sorted(struct exec_list *var_list, nir_variable *new_var)
+{
+   nir_foreach_variable(var, var_list) {
+      if (var->data.location > new_var->data.location) {
+         exec_node_insert_node_before(&var->node, &new_var->node);
+         return;
+      }
+   }
+   exec_list_push_tail(var_list, &new_var->node);
+}
+
+static void
+sort_varyings(struct exec_list *var_list)
+{
+   struct exec_list new_list;
+   exec_list_make_empty(&new_list);
+   nir_foreach_variable_safe(var, var_list) {
+      exec_node_remove(&var->node);
+      insert_sorted(&new_list, var);
+   }
+   exec_list_move_nodes_to(&new_list, var_list);
+}
+
+static void
+fixup_varying_slots(struct exec_list *var_list)
+{
+   nir_foreach_variable(var, var_list) {
+      if (var->data.location >= VARYING_SLOT_VAR0) {
+         var->data.location += 9;
+      } else if ((var->data.location >= VARYING_SLOT_TEX0) &&
+                 (var->data.location <= VARYING_SLOT_TEX7)) {
+         var->data.location += VARYING_SLOT_VAR0 - VARYING_SLOT_TEX0;
+      }
+   }
+}
 
 int
 main(int argc, char **argv)
@@ -139,10 +175,65 @@ main(int argc, char **argv)
    printf("\nnir_lower_var_copies\n");
    //nir_print_shader(nir, stdout);
 
-   NIR_PASS_V(nir, st_nir_lower_builtin);
-   printf("\nst_nir_lower_builtin\n");
+   //NIR_PASS_V(nir, st_nir_lower_builtin);
+   //printf("\nst_nir_lower_builtin\n");
    //nir_print_shader(nir, stdout);
 //*/
+   NIR_PASS_V(nir, nir_split_var_copies);
+   NIR_PASS_V(nir, nir_lower_var_copies);
+   NIR_PASS_V(nir, nir_lower_io_types);
+   printf("\nnir_lower_io_types\n");
+   //nir_print_shader(nir, stdout);
+
+   switch (stage) {
+   case MESA_SHADER_VERTEX:
+      nir_assign_var_locations(&nir->inputs,
+                               &nir->num_inputs,
+                               st_glsl_type_size);
+
+      /* Re-lower global vars, to deal with any dead VS inputs. */
+      NIR_PASS_V(nir, nir_lower_global_vars_to_local);
+
+      sort_varyings(&nir->outputs);
+      nir_assign_var_locations(&nir->outputs,
+                               &nir->num_outputs,
+                               st_glsl_type_size);
+      fixup_varying_slots(&nir->outputs);
+      break;
+   case MESA_SHADER_FRAGMENT:
+      sort_varyings(&nir->inputs);
+      nir_assign_var_locations(&nir->inputs,
+                               &nir->num_inputs,
+                               st_glsl_type_size);
+      fixup_varying_slots(&nir->inputs);
+      nir_assign_var_locations(&nir->outputs,
+                               &nir->num_outputs,
+                               st_glsl_type_size);
+      break;
+   default:
+      errx(1, "unhandled shader stage: %d", stage);
+   }
+   printf("\nfixup_varying_slots\n");
+   //nir_print_shader(nir, stdout);
+
+   nir_assign_var_locations(&nir->uniforms,
+                            &nir->num_uniforms,
+                            st_glsl_type_size);
+   printf("\nnir_assign_var_locations\n");
+   //nir_print_shader(nir, stdout);
+
+   NIR_PASS_V(nir, nir_lower_system_values);
+   printf("\nnir_lower_system_values\n");
+   //nir_print_shader(nir, stdout);
+
+   NIR_PASS_V(nir, nir_lower_io, nir_var_all, st_glsl_type_size, 0);
+   printf("\nnir_lower_io\n");
+   //nir_print_shader(nir, stdout);
+
+   NIR_PASS_V(nir, nir_lower_samplers, prog);
+   printf("\nnir_lower_samplers\n");
+   //nir_print_shader(nir, stdout);
+
 /*
    NIR_PASS_V(nir, nir_lower_io_to_scalar,
               nir_var_shader_out);
