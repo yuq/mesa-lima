42888fa9ba91b3ed0803b96480562d7b83da479b
Qiang Yu <yuq825@gmail.com>
2017-07-23 13:16:29 +0800

    lima: nir to gpir init framework
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/ir/gp/gpir.h
+++ src/gallium/drivers/lima/ir/gp/gpir.h
@@ -24,15 +24,137 @@
 #ifndef LIMA_IR_GP_GPIR_H
 #define LIMA_IR_GP_GPIR_H
 
-typedef struct gpir_prog {
-   int dummy;
-} gpir_prog;
+#include "util/list.h"
 
-gpir_prog *gpir_prog_create(void);
-void gpir_prog_delete(gpir_prog *prog);
+/* list of operations that a node can do. */
+typedef enum {
+   gpir_op_mov,
+
+   /* mul ops */
+   gpir_op_mul,
+   gpir_op_select,
+   gpir_op_complex1,
+   gpir_op_complex2,
+
+   /* add ops */
+   gpir_op_add,
+   gpir_op_floor,
+   gpir_op_sign,
+   gpir_op_ge,
+   gpir_op_lt,
+   gpir_op_min,
+   gpir_op_max,
+
+   /* mul/add ops */
+   gpir_op_neg,
+
+   /* passthrough ops */
+   gpir_op_clamp_const,
+   gpir_op_preexp2,
+   gpir_op_postlog2,
+
+   /* complex ops */
+   gpir_op_exp2_impl,
+   gpir_op_log2_impl,
+   gpir_op_rcp_impl,
+   gpir_op_rsqrt_impl,
+
+   /* load/store ops */
+   gpir_op_load_uniform,
+   gpir_op_load_temp,
+   gpir_op_load_attribute,
+   gpir_op_load_reg,
+   gpir_op_store_temp,
+   gpir_op_store_reg,
+   gpir_op_store_varying,
+   gpir_op_store_temp_load_off0,
+   gpir_op_store_temp_load_off1,
+   gpir_op_store_temp_load_off2,
+
+   /* branch */
+   gpir_op_branch_cond,
+
+   /* const (emulated) */
+   gpir_op_const,
+
+   /* emulated ops */
+   gpir_op_exp2,
+   gpir_op_log2,
+   gpir_op_rcp,
+   gpir_op_rsqrt,
+   gpir_op_ceil,
+   gpir_op_exp,
+   gpir_op_log,
+   gpir_op_sin,
+   gpir_op_cos,
+   gpir_op_tan,
+   gpir_op_branch_uncond,
+} gpir_op;
+
+typedef struct gpir_node {
+   struct list_head list;
+   gpir_op op;
+
+   struct gpir_node *children[4];
+   unsigned num_child;
+
+   /* point to the end of the sub-struct */
+   struct gpir_node **parents;
+   unsigned num_parent;
+   unsigned max_parent;
+} gpir_node;
+
+typedef struct {
+   gpir_node node;
+   bool dest_negate;
+   bool children_negate[3];
+} gpir_alu_node;
+
+typedef struct {
+   gpir_node node;
+   float constant;
+} gpir_const_node;
+
+typedef struct {
+   gpir_node node;
+   unsigned index;
+   unsigned component;
+
+   /* for uniforms/temporaries only */
+   bool offset;
+   unsigned off_reg;
+} gpir_load_node;
+
+typedef struct {
+   gpir_node node;
+   unsigned index; /* must be 0 when storing temporaries */
+   bool mask[4]; /* which components to store to */
+} gpir_store_node;
+
+typedef struct gpir_block {
+   struct list_head list;
+   struct list_head node_list;
+} gpir_block;
+
+typedef struct {
+   gpir_node node;
+   gpir_block *dest;
+} gpir_branch_node;
+
+typedef struct gpir_compiler {
+   struct list_head block_list;
+   /* array for searching ssa/reg node */
+   gpir_node **var_nodes;
+   unsigned reg_base;
+} gpir_compiler;
+
+typedef struct gpir_prog {
+   void *prog;
+   unsigned prog_size;
+} gpir_prog;
 
 typedef struct nir_shader nir_shader;
 
-gpir_prog *nir_to_gpir(nir_shader *nir);
+gpir_prog *gpir_compile_nir(nir_shader *nir);
 
 #endif
