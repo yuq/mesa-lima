4e0f29ed0b844dd59773d7047af73a482461fa2b
Eric Anholt <eric@anholt.net>
2017-07-24 12:34:23 -0700

    broadcom/vc4: Prefer blit via rendering to the software fallback.
    I don't know how I managed to leave this here for so long.  Found when
    working on a 1:1 overlapping blit extension for X11.
    
    Cc: mesa-stable@lists.freedesktop.org
    (cherry picked from commit 93fec49a75ce799bb6fe167f9409fd553a5781c6)

--- src/gallium/drivers/vc4/vc4_blit.c
+++ src/gallium/drivers/vc4/vc4_blit.c
@@ -212,14 +212,16 @@ vc4_blit(struct pipe_context *pctx, const struct pipe_blit_info *blit_info)
         if (vc4_tile_blit(pctx, blit_info))
                 return;
 
-        if (util_try_blit_via_copy_region(pctx, &info)) {
-                return; /* done */
-        }
-
         if (info.mask & PIPE_MASK_S) {
-                fprintf(stderr, "cannot blit stencil, skipping\n");
+                if (util_try_blit_via_copy_region(pctx, &info))
+                        return;
+
                 info.mask &= ~PIPE_MASK_S;
+                fprintf(stderr, "cannot blit stencil, skipping\n");
         }
 
-        vc4_render_blit(pctx, &info);
+        if (vc4_render_blit(pctx, &info))
+                return;
+
+        fprintf(stderr, "Unsupported blit\n");
 }
