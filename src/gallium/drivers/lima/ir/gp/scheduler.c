915c489a7cec5c7c60f1d4586a5b885a9df2b048
Qiang Yu <yuq825@gmail.com>
2017-08-07 22:50:18 +0800

    lima/gpir: calculate distance of the DAG
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/ir/gp/scheduler.c
+++ src/gallium/drivers/lima/ir/gp/scheduler.c
@@ -22,10 +22,43 @@
  *
  */
 
-
+#include "util/u_dynarray.h"
 #include "gpir.h"
+
+static void gpir_update_distance(gpir_node *node, int d)
+{
+   if (d > node->distance) {
+      node->distance = d;
+      for (int i = 0; i < node->num_child; i++)
+         gpir_update_distance(node->children[i], d + gpir_op_infos[node->op].latency);
+   }
+}
+
+static void gpir_schedule_block(gpir_block *block)
+{
+   struct util_dynarray root, leaf;
+
+   util_dynarray_init(&root);
+   util_dynarray_init(&leaf);
+
+   /* collect root and leaf nodes */
+   list_for_each_entry(gpir_node, node, &block->node_list, list) {
+      if (!node->num_child)
+         util_dynarray_append(&leaf, gpir_node *, node);
+      if (!node->num_parent)
+         util_dynarray_append(&root, gpir_node *, node);
+   }
+
+   for (int i = 0; i < root.size / sizeof(gpir_node *); i++)
+      gpir_update_distance(*util_dynarray_element(&root, gpir_node *, i), 0);
+
+   util_dynarray_fini(&root);
+   util_dynarray_fini(&leaf);
+}
 
 void gpir_schedule_prog(gpir_compiler *comp)
 {
-   
+   list_for_each_entry(gpir_block, block, &comp->block_list, list) {
+      gpir_schedule_block(block);
+   }
 }
