42e8f3ccdf3137fc2810f745d2ba5a93fa8a5b8e
Frank Richter <frank.richter@dynardo.de>
2017-08-15 15:46:35 +0200

    gallium/os: fix os_time_get_nano() to roll over less
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=102241
    Cc: mesa-stable@lists.freedesktop.org
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
    (cherry picked from commit 7fb7287ce72066db7dffd918226bf15c3131a871)

--- src/gallium/auxiliary/os/os_time.c
+++ src/gallium/auxiliary/os/os_time.c
@@ -69,10 +69,17 @@ os_time_get_nano(void)
 
    static LARGE_INTEGER frequency;
    LARGE_INTEGER counter;
+   int64_t secs, nanosecs;
    if(!frequency.QuadPart)
       QueryPerformanceFrequency(&frequency);
    QueryPerformanceCounter(&counter);
-   return counter.QuadPart*INT64_C(1000000000)/frequency.QuadPart;
+   /* Compute seconds and nanoseconds parts separately to
+    * reduce severity of precision loss.
+    */
+   secs = counter.QuadPart / frequency.QuadPart;
+   nanosecs = (counter.QuadPart % frequency.QuadPart) * INT64_C(1000000000)
+      / frequency.QuadPart;
+   return secs*INT64_C(1000000000) + nanosecs;
 
 #else
 
