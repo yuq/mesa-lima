cf2f05ff9177ee1b095071e11419f464bf176631
Marek Olšák <marek.olsak@amd.com>
2017-08-16 00:54:45 +0200

    radeonsi/gfx9: add a temporary workaround for a tessellation driver bug
    The workaround will do for now. The root cause is still unknown.
    
    This fixes new piglit: 16in-1out
    
    Cc: 17.1 17.2 <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
    (cherry picked from commit 166823bfd26ff7e9b88099598305967336525716)

--- src/gallium/drivers/radeonsi/si_state_draw.c
+++ src/gallium/drivers/radeonsi/si_state_draw.c
@@ -194,7 +194,11 @@ static void si_emit_derived_tess_state(struct si_context *sctx,
 	 */
 	*num_patches = MIN2(*num_patches, 40);
 
-	if (sctx->b.chip_class == SI) {
+	if (sctx->b.chip_class == SI ||
+	    /* TODO: fix GFX9 where a threadgroup contains more than 1 wave and
+	     * LS vertices per patch > HS vertices per patch. Piglit: 16in-1out */
+	    (sctx->b.chip_class == GFX9 &&
+	     num_tcs_input_cp > num_tcs_output_cp)) {
 		/* SI bug workaround, related to power management. Limit LS-HS
 		 * threadgroups to only one wave.
 		 */
