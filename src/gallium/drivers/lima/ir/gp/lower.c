74e6c35b642157a919afb4d3453fc0cbfb74e7f3
Qiang Yu <yuq825@gmail.com>
2017-08-25 11:46:39 +0800

    lima/gpir: lower neg node
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/ir/gp/lower.c
+++ src/gallium/drivers/lima/ir/gp/lower.c
@@ -90,8 +90,72 @@ static void gpir_lower_copy(gpir_compiler *comp)
    }
 }
 
+static void gpir_lower_negate(gpir_compiler *comp)
+{
+   list_for_each_entry(gpir_block, block, &comp->block_list, list) {
+      list_for_each_entry_safe(gpir_node, node, &block->node_list, list) {
+         if (node->op == gpir_op_neg) {
+            gpir_alu_node *neg = gpir_node_to_alu(node);
+            gpir_node *child = neg->children[0];
+
+            /* check if child can dest negate */
+            if (child->type == gpir_node_type_alu) {
+               /* negate must be its only successor */
+               bool only = true;
+               gpir_node_foreach_succ(child, entry) {
+                  gpir_node *succ = gpir_node_from_entry(entry, succ);
+                  if (succ != node) {
+                     only = false;
+                     break;
+                  }
+               }
+
+               if (only && gpir_op_infos[child->op].dest_neg) {
+                  gpir_alu_node *alu = gpir_node_to_alu(child);
+                  alu->dest_negate = !alu->dest_negate;
+
+                  gpir_node_replace_succ(child, node);
+                  gpir_node_delete(node);
+                  continue;
+               }
+            }
+
+            /* check if child can src negate */
+            gpir_node_foreach_succ(node, entry) {
+               gpir_node *succ = gpir_node_from_entry(entry, succ);
+
+               if (succ->type != gpir_node_type_alu)
+                  continue;
+
+               bool success = true;
+               gpir_alu_node *alu = gpir_node_to_alu(succ);
+               for (int i = 0; i < alu->num_child; i++) {
+                  if (alu->children[i] == node) {
+                     if (gpir_op_infos[succ->op].src_neg[i]) {
+                        alu->children_negate[i] = !alu->children_negate[i];
+                        alu->children[i] = child;
+                     }
+                     else
+                        success = false;
+                  }
+               }
+
+               if (success) {
+                  gpir_node_remove_entry(entry);
+                  gpir_node_add_child(succ, child);
+               }
+            }
+
+            if (gpir_node_is_root(node))
+               gpir_node_delete(node);
+         }
+      }
+   }
+}
+
 void gpir_lower_prog(gpir_compiler *comp)
 {
+   gpir_lower_negate(comp);
    gpir_lower_const(comp);
    gpir_lower_copy(comp);
 }
