1a172fb113554fa03053052b161d5e22fc2fcb1f
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2017-08-28 00:18:44 +0200

    radv: Don't allocate CMASK for linear images.
    We can't use it anyway in fast clears, and on GFX9 it seems to
    actually hange the card if we specify it.
    
    Fixes: f4e499ec791 "radv: add initial non-conformant radv vulkan driver"

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -839,8 +839,10 @@ radv_image_create(VkDevice _device,
 
 	if ((pCreateInfo->usage & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT) &&
 	    pCreateInfo->mipLevels == 1 &&
-	    !image->surface.dcc_size && image->info.depth == 1 && can_cmask_dcc)
+	    !image->surface.dcc_size && image->info.depth == 1 && can_cmask_dcc &&
+	    !image->surface.is_linear)
 		radv_image_alloc_cmask(device, image);
+
 	if (image->info.samples > 1 && vk_format_is_color(pCreateInfo->format)) {
 		radv_image_alloc_fmask(device, image);
 	} else if (vk_format_is_depth(pCreateInfo->format)) {
