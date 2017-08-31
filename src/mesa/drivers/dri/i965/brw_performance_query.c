b5f6b9b0eb65a9e095618d00724b74f4f9f1dc62
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2017-08-31 11:28:30 +0100

    i965: perf: add support for CoffeeLake GT2
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Acked-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_performance_query.c
+++ src/mesa/drivers/dri/i965/brw_performance_query.c
@@ -81,6 +81,7 @@
 #include "brw_oa_kblgt2.h"
 #include "brw_oa_kblgt3.h"
 #include "brw_oa_glk.h"
+#include "brw_oa_cflgt2.h"
 #include "intel_batchbuffer.h"
 
 #define FILE_DEBUG_FLAG DEBUG_PERFMON
@@ -2090,6 +2091,11 @@ get_register_queries_function(const struct gen_device_info *devinfo)
    }
    if (devinfo->is_geminilake)
       return brw_oa_register_queries_glk;
+   if (devinfo->is_coffeelake) {
+      if (devinfo->gt == 2)
+         return brw_oa_register_queries_cflgt2;
+   }
+
    return NULL;
 }
 
