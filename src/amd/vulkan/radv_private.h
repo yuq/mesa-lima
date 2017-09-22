6f8c40734bee276325ed718ccd015d4794567027
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-09-22 18:21:35 +0200

    radv: make radv_pipeline_init() static
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1136,13 +1136,6 @@ struct radv_graphics_pipeline_create_info {
 };
 
 VkResult
-radv_pipeline_init(struct radv_pipeline *pipeline, struct radv_device *device,
-		   struct radv_pipeline_cache *cache,
-		   const VkGraphicsPipelineCreateInfo *pCreateInfo,
-		   const struct radv_graphics_pipeline_create_info *extra,
-		   const VkAllocationCallbacks *alloc);
-
-VkResult
 radv_graphics_pipeline_create(VkDevice device,
 			      VkPipelineCache cache,
 			      const VkGraphicsPipelineCreateInfo *pCreateInfo,
