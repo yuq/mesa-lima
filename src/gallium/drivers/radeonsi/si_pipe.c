80bbdb148335c55303960bab841d98f4fbd1feea
Dave Airlie <airlied@redhat.com>
2017-10-10 13:23:40 +1000

    radeonsi: lower ffma in nir to mad.
    This lowers ffma to a * b + c.
    
    This seems like it should keep Marek happiest, so
    we'd never get to the fma instruction emission code.
    
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -795,6 +795,7 @@ static const struct nir_shader_compiler_options nir_options = {
 	.lower_fsat = true,
 	.lower_fdiv = true,
 	.lower_sub = true,
+	.lower_ffma = true,
 	.lower_pack_snorm_2x16 = true,
 	.lower_pack_snorm_4x8 = true,
 	.lower_pack_unorm_2x16 = true,
