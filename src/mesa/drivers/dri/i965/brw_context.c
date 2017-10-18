19e0dd1ad3ad43ddc3aeba1e11852dce3f55d1f1
Ian Romanick <ian.d.romanick@intel.com>
2017-10-18 08:59:47 -0700

    i965: Don't request GLSL IR lowering of gl_VertexID
    Let the lowering in NIR handle it instead.
    
    This hurts one shader that occurs twice in shader-db (SynMark GSCloth)
    on IVB and HSW.  No other shaders or platforms were affected.
    
    total cycles in shared programs: 253438422 -> 253438426 (0.00%)
    cycles in affected programs: 412 -> 416 (0.97%)
    helped: 0
    HURT: 2
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Antia Puentes <apuentes@igalia.com>

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -595,7 +595,6 @@ brw_initialize_context_constants(struct brw_context *brw)
       ctx->Const.QuadsFollowProvokingVertexConvention = false;
 
    ctx->Const.NativeIntegers = true;
-   ctx->Const.VertexID_is_zero_based = true;
 
    /* Regarding the CMP instruction, the Ivybridge PRM says:
     *
