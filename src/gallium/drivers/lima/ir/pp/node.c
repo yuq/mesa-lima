35e291e4069dde6de34a3bf02543d6e36db9bab2
Qiang Yu <yuq825@gmail.com>
2017-10-20 21:50:53 +0800

    lima/ppir: add rsqrt codegen
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/ir/pp/node.c
+++ src/gallium/drivers/lima/ir/pp/node.c
@@ -68,6 +68,10 @@ const ppir_op_info ppir_op_infos[] = {
       .name = "sum4",
       .type = ppir_node_type_alu,
    },
+   [ppir_op_rsqrt] = {
+      .name = "rsqrt",
+      .type = ppir_node_type_alu,
+   },
    [ppir_op_load_varying] = {
       .name = "ld_var",
       .type = ppir_node_type_load,
