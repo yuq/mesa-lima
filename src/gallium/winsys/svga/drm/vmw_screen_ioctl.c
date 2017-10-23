cece4ff6a328bff015e0905442254e27d339f6a0
Greg V <greg@unrelenting.technology>
2017-10-23 23:20:42 +0300

    winsys/svga/drm: add ERESTART define for *BSD
    Obtained from: FreeBSD ports
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
+++ src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
@@ -70,6 +70,11 @@ vmw_region_size(struct vmw_region *region)
    return region->size;
 }
 
+#if defined(__DragonFly__) || defined(__FreeBSD__) || \
+    defined(__NetBSD__) || defined(__OpenBSD__)
+#define ERESTART EINTR
+#endif
+
 uint32
 vmw_ioctl_context_create(struct vmw_winsys_screen *vws)
 {
