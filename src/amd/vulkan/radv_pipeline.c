f0a2bbd1a4a0caf85d91469e61d01a126cfc2a36
Timothy Arceri <tarceri@itsqueeze.com>
2017-10-24 09:08:55 +1100

    radv: move nir print after linking is done
    We now have linking optimisations so we want to delay dumping the
    nir until after these are complete.
    
    Fixes: 06f05040eb73 (radv: Link shaders)
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -1870,8 +1870,13 @@ void radv_create_shaders(struct radv_pipeline *pipeline,
 
 	for (int i = 0; i < MESA_SHADER_STAGES; ++i) {
 		free(codes[i]);
-		if (modules[i] && !pipeline->device->trace_bo)
-			ralloc_free(nir[i]);
+		if (modules[i]) {
+			if (device->instance->debug_flags & RADV_DEBUG_DUMP_SHADERS)
+				nir_print_shader(nir[i], stderr);
+
+			if (!pipeline->device->trace_bo)
+				ralloc_free(nir[i]);
+		}
 	}
 
 	if (fs_m.nir)
