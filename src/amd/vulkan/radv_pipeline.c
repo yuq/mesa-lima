4fb6b83968f235ea727b55d366e4f412df148c49
Timothy Arceri <tarceri@itsqueeze.com>
2017-10-23 11:10:47 +1100

    radv: clone meta shaders before linking
    The IR is reused in different pipeline combinations so we need
    to clone it to avoid link time optimistaions messing up the
    original copy.
    
    Fixes: 06f05040eb73 (radv: Link shaders)
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 013313cf895e4d47b2631d5f4dd32fcbce2b0a17)

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -1766,6 +1766,13 @@ void radv_create_shaders(struct radv_pipeline *pipeline,
 						    stage ? stage->pName : "main", i,
 						    stage ? stage->pSpecializationInfo : NULL);
 		pipeline->active_stages |= mesa_to_vk_shader_stage(i);
+		/* We don't want to alter meta shaders IR directly so clone it
+		 * first.
+		 */
+		if (nir[i]->info.name) {
+			nir[i] = nir_shader_clone(NULL, nir[i]);
+		}
+
 	}
 
 	if (nir[MESA_SHADER_TESS_CTRL]) {
@@ -1863,7 +1870,7 @@ void radv_create_shaders(struct radv_pipeline *pipeline,
 
 	for (int i = 0; i < MESA_SHADER_STAGES; ++i) {
 		free(codes[i]);
-		if (modules[i] && !modules[i]->nir && !pipeline->device->trace_bo)
+		if (modules[i] && !pipeline->device->trace_bo)
 			ralloc_free(nir[i]);
 	}
 
