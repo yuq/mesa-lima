50776fb1cb5db92bb9138af41ecf29e733f4010c
Heiko Stuebner <heiko@sntech.de>
2017-11-15 23:59:34 +0100

    lima: add renderonly-based support for lima rockchip
    A mesa driver for rockchip is defined which will call into lima when 3D
    rendering is requested from it.
    This uses the renderonly library from mesa, which hides the buffer
    sharing details between the two drivers from the application.
    This allows applications that expect a single device node like
    /dev/dri/card0 (such as kmscube) to run unmodified on Rockchip devices
    that use the rockchip-drm driver for display and have a Mali400 gpu using
    lima.
    
    This follows the model of the lima+sun4i drivers existing in mesa.
    The code implemented here is mostly borrowed from that particular
    implementation.
    
    Rockchip socs use the same drm/kms driver throughout all socs but
    multiple gpus are in use like Mali-Utgard, Mali-Midgard and also
    PowerVR and Vivante.
    To help with this the winsys part is set up to iterate through multiple
    gpus and try each until a valid one is found. Right now of course only
    the Lima-supported GPUs are implemented but it can be easily adapted
    for the other ones, once Mesa drivers are available for them.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

---
+++ src/gallium/winsys/rockchip/drm/rockchip_drm_winsys.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Lima Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "rockchip_drm_public.h"
+#include "lima/drm/lima_drm_public.h"
+#include "xf86drm.h"
+
+#include "pipe/p_screen.h"
+#include "renderonly/renderonly.h"
+
+#if defined(GALLIUM_LIMA)
+static struct pipe_screen *rockchip_screen_create_lima(int fd)
+{
+   struct renderonly ro = {
+      /* Passes the lima-allocated BO through to the rockchip DRM device using
+       * PRIME buffer sharing.  The lima BO must be linear, which the SCANOUT
+       * flag on allocation will have ensured.
+       */
+      .create_for_resource = renderonly_create_gpu_import_for_resource,
+      .kms_fd = fd,
+      .gpu_fd = drmOpenWithType("lima", NULL, DRM_NODE_RENDER),
+   };
+
+   if (ro.gpu_fd < 0)
+      return NULL;
+
+   struct pipe_screen *screen = lima_drm_screen_create_renderonly(&ro);
+   if (!screen)
+      close(ro.gpu_fd);
+
+   return screen;
+}
+#else
+static struct pipe_screen *rockchip_screen_create_lima(int fd)
+{
+   return NULL;
+}
+#endif
+
+/*
+ * Rockchip SoCs use a plethora of 3D GPUs combined with the same
+ * KMS device. Malis from the Utgard and Midgard branches as well
+ * as PowerVR and Vivante cores.
+ * So to we try to find a suitable GPU by trying to talk to each
+ * of them one after the other.
+ */
+struct pipe_screen *rockchip_screen_create(int fd)
+{
+   struct pipe_screen *screen;
+
+   screen = rockchip_screen_create_lima(fd);
+   if (screen)
+      return screen;
+
+   return NULL;
+}
