fa985de2c02be8d353bfa2a3d09ed7769e6bc48c
Erico Nunes <nunes.erico@gmail.com>
2017-11-15 23:59:34 +0100

    lima: add renderonly-based support for lima sun4i-drm
    A mesa driver for sun4i-drm is defined which will call into lima when 3D
    rendering is requested from it.
    This uses the renderonly library from mesa, which hides the buffer
    sharing details between the two drivers from the application.
    This allows applications that expect a single device node like
    /dev/dri/card0 (such as kmscube) to run unmodified on Allwinner devices
    that use the sun4i-drm driver for display and have a Mali400 gpu using
    lima.
    
    This follows the model of the vc4+pl111 drivers existing in mesa.
    The code implemented here is mostly borrowed from that particular
    implementation.
    
    The driver is called sun4i in mesa and it will intentionally generate
    sun4i-drm_dri.so.
    This is because mesa will look for a *_dri.so user space driver named
    after the corresponding kernel driver, which in this case is called
    sun4i-drm.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

--- src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
+++ src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -111,6 +111,11 @@ static const struct drm_driver_descriptor driver_descriptors[] = {
         .configuration = pipe_default_configuration_query,
     },
     {
+       .driver_name = "sun4i-drm",
+        .create_screen = pipe_sun4i_create_screen,
+        .configuration = pipe_default_configuration_query,
+    },
+    {
         .driver_name = "virtio_gpu",
         .create_screen = pipe_virgl_create_screen,
         .configuration = pipe_default_configuration_query,
--- src/gallium/auxiliary/target-helpers/drm_helper.h
+++ src/gallium/auxiliary/target-helpers/drm_helper.h
@@ -106,6 +106,29 @@ pipe_pl111_create_screen(int fd, const struct pipe_screen_config *config)
 
 #endif
 
+#ifdef GALLIUM_SUN4I
+#include "sun4i/drm/sun4i_drm_public.h"
+
+struct pipe_screen *
+pipe_sun4i_create_screen(int fd, const struct pipe_screen_config *config)
+{
+   struct pipe_screen *screen;
+
+   screen = sun4i_screen_create(fd);
+   return screen ? debug_screen_wrap(screen) : NULL;
+}
+
+#else
+
+struct pipe_screen *
+pipe_sun4i_create_screen(int fd, const struct pipe_screen_config *config)
+{
+   fprintf(stderr, "sun4i-drm: driver missing\n");
+   return NULL;
+}
+
+#endif
+
 #ifdef GALLIUM_R300
 #include "radeon/radeon_winsys.h"
 #include "radeon/drm/radeon_drm_public.h"
