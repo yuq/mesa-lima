676761252b731a6bf408e4dca694c31d74a995fc
Matt Turner <mattst88@gmail.com>
2017-11-25 16:45:27 -0800

    util: Just give up and define PIPE_ARCH_LITTLE_ENDIAN on MSVC
    MSVC doesn't support #warning?! Getting really tired of this.

--- src/util/u_endian.h
+++ src/util/u_endian.h
@@ -65,9 +65,10 @@
 # define PIPE_ARCH_BIG_ENDIAN
 #endif
 
-#endif
+#elif defined(_MSC_VER)
 
-#warning Unknown Endianness for this platform. Assuming little endian
 #define PIPE_ARCH_LITTLE_ENDIAN
+
+#endif
 
 #endif
