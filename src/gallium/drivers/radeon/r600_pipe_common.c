4038db72d4f28f3eae811173839a926ea4979b5e
Marek Olšák <marek.olsak@amd.com>
2017-12-01 03:08:16 +0100

    radeonsi: make const and stream uploaders allocate read-only memory
    and anything that clones these uploaders, like u_threaded_context.
    
    Tested-by: Dieter Nützel <Dieter@nuetzel-hh.de>
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

--- src/gallium/drivers/radeon/r600_pipe_common.c
+++ src/gallium/drivers/radeon/r600_pipe_common.c
@@ -445,12 +445,15 @@ bool si_common_context_init(struct r600_common_context *rctx,
 		return false;
 
 	rctx->b.stream_uploader = u_upload_create(&rctx->b, 1024 * 1024,
-						  0, PIPE_USAGE_STREAM, 0);
+						  0, PIPE_USAGE_STREAM,
+						  R600_RESOURCE_FLAG_READ_ONLY);
 	if (!rctx->b.stream_uploader)
 		return false;
 
 	rctx->b.const_uploader = u_upload_create(&rctx->b, 128 * 1024,
-						 0, PIPE_USAGE_DEFAULT, 0);
+						 0, PIPE_USAGE_DEFAULT,
+						 sscreen->cpdma_prefetch_writes_memory ?
+							0 : R600_RESOURCE_FLAG_READ_ONLY);
 	if (!rctx->b.const_uploader)
 		return false;
 
