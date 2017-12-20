6edf1fcdf524e7815245ee308b575e319f2e65b8
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2017-12-20 20:55:51 +0100

    amd/common: remove unnecessary user_sgpr_count
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/common/ac_nir_to_llvm.c
+++ src/amd/common/ac_nir_to_llvm.c
@@ -252,7 +252,6 @@ struct arg_info {
 	LLVMValueRef *assign[MAX_ARGS];
 	unsigned array_params_mask;
 	uint8_t count;
-	uint8_t user_sgpr_count;
 	uint8_t sgpr_count;
 	uint8_t num_user_sgprs_used;
 	uint8_t num_sgprs_used;
@@ -285,7 +284,6 @@ add_user_sgpr_argument(struct arg_info *info,
 {
 	add_sgpr_argument(info, type, param_ptr);
 	info->num_user_sgprs_used += ac_get_type_size(type) / 4;
-	info->user_sgpr_count++;
 }
 
 static inline void
