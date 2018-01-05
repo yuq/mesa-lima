89dcaf5d5b80babb5e0f959d38637d2ff6cbcf65
Qiang Yu <yuq825@gmail.com>
2018-01-05 20:46:48 +0800

    lima/gpir: fix two acc slot have different op
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/gallium/drivers/lima/ir/gp/gpir.h
+++ src/gallium/drivers/lima/ir/gp/gpir.h
@@ -373,6 +373,8 @@ bool gpir_instr_try_insert_node(gpir_instr *instr, gpir_node *node);
 void gpir_instr_remove_node(gpir_instr *instr, gpir_node *node);
 void gpir_instr_print_prog(gpir_compiler *comp);
 
+bool gpir_codegen_acc_same_op(gpir_op op1, gpir_op op2);
+
 bool gpir_pre_rsched_lower_prog(gpir_compiler *comp);
 bool gpir_post_rsched_lower_prog(gpir_compiler *comp);
 bool gpir_reduce_reg_pressure_schedule_prog(gpir_compiler *comp);
