338638a8afc9f330bacc1cdd7e6392a3ea9d828a
Indrajit Das <indrajit-kumar.das@amd.com>
2018-01-10 15:13:37 +0530

    st/va: clear pointers for mpeg2 quantiser matrices
    This is to fix VA-API issues with GStreamer and MPEG2.
    Since gstreamer does not pass quantiser matrices with each frame, invalid
    pointers were being passed to the driver. This patch addresses the same.
    
    Signed-off-by: Indrajit Das <indrajit-kumar.das@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- src/gallium/state_trackers/va/picture.c
+++ src/gallium/state_trackers/va/picture.c
@@ -57,6 +57,11 @@ vlVaBeginPicture(VADriverContextP ctx, VAContextID context_id, VASurfaceID rende
       return VA_STATUS_ERROR_INVALID_CONTEXT;
    }
 
+   if (u_reduce_video_profile(context->templat.profile) == PIPE_VIDEO_FORMAT_MPEG12) {
+      context->desc.mpeg12.intra_matrix = NULL;
+      context->desc.mpeg12.non_intra_matrix = NULL;
+   }
+
    surf = handle_table_get(drv->htab, render_target);
    mtx_unlock(&drv->mutex);
    if (!surf || !surf->buffer)
