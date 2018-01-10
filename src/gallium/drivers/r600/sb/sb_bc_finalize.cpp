046cf68cadd04a1e1eb89476614c334ac702f0ce
Dave Airlie <airlied@redhat.com>
2018-01-10 04:25:41 +0000

    r600/sb: handle lds special dest registers.
    This adds lds to the geom emit handling
    
    Acked-By: Roland Scheidegger <sroland@vmware.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/r600/sb/sb_bc_finalize.cpp
+++ src/gallium/drivers/r600/sb/sb_bc_finalize.cpp
@@ -294,7 +294,7 @@ void bc_finalizer::finalize_alu_group(alu_group_node* g, node *prev_node) {
 		value *d = n->dst.empty() ? NULL : n->dst[0];
 
 		if (d && d->is_special_reg()) {
-			assert((n->bc.op_ptr->flags & AF_MOVA) || d->is_geometry_emit());
+			assert((n->bc.op_ptr->flags & AF_MOVA) || d->is_geometry_emit() || d->is_lds_oq() || d->is_lds_access());
 			d = NULL;
 		}
 
