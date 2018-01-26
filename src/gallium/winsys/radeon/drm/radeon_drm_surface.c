25583470fc4bc8c43a4cd55079ba0276d2759078
Michel Dänzer <michel.daenzer@amd.com>
2018-01-26 18:32:32 +0100

    winsys/radeon: Compute is_displayable in surf_drm_to_winsys
    It was always 0, breaking (at least) DRI3 with Xwayland.
    
    Bugzilla: https://bugs.freedesktop.org/104306
    Fixes: 5f2073be3282 ("ac/surface: add ac_surface::is_displayable")
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    (cherry picked from commit 1cf1bf32eff5ffca0b928c0884b0e792207b61b7)

--- src/gallium/winsys/radeon/drm/radeon_drm_surface.c
+++ src/gallium/winsys/radeon/drm/radeon_drm_surface.c
@@ -215,6 +215,9 @@ static void surf_drm_to_winsys(struct radeon_drm_winsys *ws,
     }
 
     set_micro_tile_mode(surf_ws, &ws->info);
+    surf_ws->is_displayable = surf_ws->is_linear ||
+			      surf_ws->micro_tile_mode == RADEON_MICRO_MODE_DISPLAY ||
+			      surf_ws->micro_tile_mode == RADEON_MICRO_MODE_ROTATED;
 }
 
 static int radeon_winsys_surface_init(struct radeon_winsys *rws,
