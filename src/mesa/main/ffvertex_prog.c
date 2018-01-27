1169791c18d2b291870fc68fef4f3ff5e4a81674
Mathias Fröhlich <mathias.froehlich@web.de>
2018-01-27 12:09:00 -0700

    mesa: Remove unused bit in ffvertex_prog state_key.
    Remove set but not read field from the state key used for hashing
    fixed function vertex shaders.
    
    Signed-off-by: Mathias Fröhlich <Mathias.Froehlich@web.de>
    Reviewed-by: Brian Paul <brianp@vmware.com>

--- src/mesa/main/ffvertex_prog.c
+++ src/mesa/main/ffvertex_prog.c
@@ -63,7 +63,6 @@ struct state_key {
    unsigned fog_distance_mode:2;
    unsigned separate_specular:1;
    unsigned point_attenuated:1;
-   unsigned texture_enabled_global:1;
    unsigned fragprog_inputs_read:12;
 
    GLbitfield varying_vp_inputs;
@@ -229,11 +228,6 @@ static void make_state_key( struct gl_context *ctx, struct state_key *key )
 
    if (ctx->Point._Attenuated)
       key->point_attenuated = 1;
-
-   if (ctx->Texture._TexGenEnabled ||
-       ctx->Texture._TexMatEnabled ||
-       ctx->Texture._MaxEnabledTexImageUnit != -1)
-      key->texture_enabled_global = 1;
 
    mask = ctx->Texture._EnabledCoordUnits | ctx->Texture._TexGenEnabled
       | ctx->Texture._TexMatEnabled | ctx->Point.CoordReplace;
