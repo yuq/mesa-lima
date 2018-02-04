9a508b719be32ef10ca929250b7aafba313104c6
Mauro Rossi <issor.oruam@gmail.com>
2018-02-04 22:57:09 +0100

    android: anv/extensions: fix generated sources build
    Building rules are aligned to automake ones
    
    The correct script to build anv_extensions.{c,h} is anv_extensions_gen.py
    Generation rules for anv_extensions.c requires --out-c option
    Generation rules for anv_extensions.h were missing
    Necessary include paths are added to avoid following build errors:
    
    cp: cannot stat '.../gen/STATIC_LIBRARIES/libmesa_vulkan_common_intermediates/vulkan/anv_extensions.c':
    No such file or directory
    
    In file included from external/mesa/src/intel/vulkan/anv_gem.c:32:
    external/mesa/src/intel/vulkan/anv_private.h:75:10: fatal error: 'anv_extensions.h' file not found
             ^~~~~~~~~~~~~~~~~~
    1 error generated.
    
    In file included from external/mesa/src/intel/vulkan/anv_batch_chain.c:30:
    external/mesa/src/intel/vulkan/anv_private.h:75:10: fatal error: 'anv_extensions.h' file not found
             ^~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Fixes: dd088d4bec7 ("anv/extensions: Generate a header file with extension tables")
    Cc: "18.0" <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

--- src/intel/Android.vulkan.mk
+++ src/intel/Android.vulkan.mk
@@ -25,7 +25,7 @@ include $(LOCAL_PATH)/Makefile.sources
 
 VK_ENTRYPOINTS_SCRIPT := $(MESA_PYTHON2) $(LOCAL_PATH)/vulkan/anv_entrypoints_gen.py
 
-VK_EXTENSIONS_SCRIPT := $(MESA_PYTHON2) $(LOCAL_PATH)/vulkan/anv_extensions.py
+VK_EXTENSIONS_SCRIPT := $(MESA_PYTHON2) $(LOCAL_PATH)/vulkan/anv_extensions_gen.py
 
 VULKAN_COMMON_INCLUDES := \
 	$(MESA_TOP)/include \
@@ -82,6 +82,7 @@ ANV_INCLUDES := \
 	$(VULKAN_COMMON_INCLUDES) \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_anv_entrypoints,,)/vulkan \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir \
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_vulkan_common,,)/vulkan \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_vulkan_util,,)/util
 
 #
@@ -232,6 +233,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 
 LOCAL_GENERATED_SOURCES += $(intermediates)/vulkan/anv_entrypoints.c
 LOCAL_GENERATED_SOURCES += $(intermediates)/vulkan/anv_extensions.c
+LOCAL_GENERATED_SOURCES += $(intermediates)/vulkan/anv_extensions.h
 
 $(intermediates)/vulkan/anv_entrypoints.c:
 	@mkdir -p $(dir $@)
@@ -245,7 +247,14 @@ $(intermediates)/vulkan/anv_extensions.c:
 	$(VK_EXTENSIONS_SCRIPT) \
 		--xml $(MESA_TOP)/src/vulkan/registry/vk.xml \
 		--xml $(MESA_TOP)/src/vulkan/registry/vk_android_native_buffer.xml \
-		--out $@
+		--out-c $@
+
+$(intermediates)/vulkan/anv_extensions.h:
+	@mkdir -p $(dir $@)
+	$(VK_EXTENSIONS_SCRIPT) \
+		--xml $(MESA_TOP)/src/vulkan/registry/vk.xml \
+		--xml $(MESA_TOP)/src/vulkan/registry/vk_android_native_buffer.xml \
+		--out-h $@
 
 LOCAL_SHARED_LIBRARIES := libdrm
 
@@ -272,7 +281,8 @@ LOCAL_SRC_FILES := \
 
 LOCAL_C_INCLUDES := \
 	$(VULKAN_COMMON_INCLUDES) \
-	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_anv_entrypoints,,)/vulkan
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_anv_entrypoints,,)/vulkan \
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_vulkan_common,,)/vulkan
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(MESA_TOP)/src/intel/vulkan
 
