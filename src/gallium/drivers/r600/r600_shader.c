b9d2ff05a64f751f5814314b2cce45e6270ddf7f
Dave Airlie <airlied@redhat.com>
2018-02-14 13:59:09 +1000

    r600: fix regression in gl_FragColor drawing
    This fixes a regression in the broadcast color to all color bufs case.
    
    Fixes: 6c691081a (r600: fixup sparse color exports.)
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/r600/r600_shader.c
+++ src/gallium/drivers/r600/r600_shader.c
@@ -4123,6 +4123,8 @@ static int r600_shader_from_tgsi(struct r600_context *rctx,
 							output[j].op = CF_OP_EXPORT;
 							output[j].type = V_SQ_CF_ALLOC_EXPORT_WORD0_SQ_EXPORT_PIXEL;
 							shader->nr_ps_color_exports++;
+							if (k > shader->ps_export_highest)
+								shader->ps_export_highest = k;
 							shader->ps_color_export_mask |= (0xf << (j * 4));
 						}
 					}
