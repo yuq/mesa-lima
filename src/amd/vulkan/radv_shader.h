2012dae19a1ef73224c4e54eab4cd731f1d59ddf
Dave Airlie <airlied@redhat.com>
2018-02-19 05:48:40 +0000

    radv: migrate unique index info shader info (v2)
    This just moves this function to an inline so the shader_info
    pass can use it.
    
    v2: use inline (Samuel)
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/amd/vulkan/radv_shader.h
+++ src/amd/vulkan/radv_shader.h
@@ -338,4 +338,25 @@ radv_can_dump_shader_stats(struct radv_device *device,
 	       module && !module->nir;
 }
 
+static inline unsigned shader_io_get_unique_index(gl_varying_slot slot)
+{
+	/* handle patch indices separate */
+	if (slot == VARYING_SLOT_TESS_LEVEL_OUTER)
+		return 0;
+	if (slot == VARYING_SLOT_TESS_LEVEL_INNER)
+		return 1;
+	if (slot >= VARYING_SLOT_PATCH0 && slot <= VARYING_SLOT_TESS_MAX)
+		return 2 + (slot - VARYING_SLOT_PATCH0);
+	if (slot == VARYING_SLOT_POS)
+		return 0;
+	if (slot == VARYING_SLOT_PSIZ)
+		return 1;
+	if (slot == VARYING_SLOT_CLIP_DIST0)
+		return 2;
+	/* 3 is reserved for clip dist as well */
+	if (slot >= VARYING_SLOT_VAR0 && slot <= VARYING_SLOT_VAR31)
+		return 4 + (slot - VARYING_SLOT_VAR0);
+	unreachable("illegal slot in get unique index\n");
+}
+
 #endif
