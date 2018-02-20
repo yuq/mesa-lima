e72062b66d9b646c0df269da5982b26237a77fab
Christian Gmeiner <christian.gmeiner@gmail.com>
2018-02-20 20:47:18 +0100

    etnaviv: npot_tex_any_wrap needs one bit only
    Reduces size of struct etna_specs from 100 to 94 bytes.
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Reviewed-by: Lucas Stach <l.stach@pengutronix.de>

--- src/gallium/drivers/etnaviv/etnaviv_internal.h
+++ src/gallium/drivers/etnaviv/etnaviv_internal.h
@@ -87,7 +87,7 @@ struct etna_specs {
    /* has BLT engine instead of RS */
    unsigned use_blt : 1;
    /* can use any kind of wrapping mode on npot textures */
-   unsigned npot_tex_any_wrap;
+   unsigned npot_tex_any_wrap : 1;
    /* number of bits per TS tile */
    unsigned bits_per_tile;
    /* clear value for TS (dependent on bits_per_tile) */
