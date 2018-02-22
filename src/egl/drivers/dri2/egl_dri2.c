6160bf97db8bc493512795b1fa49c072a703df50
Frank Binns <francisbinns@gmail.com>
2018-02-22 13:37:54 +0000

    egl/dri2: fix segfault when display initialisation fails
    dri2_display_destroy() is called when platform specific display
    initialisation fails. However, this would typically lead to a
    segfault due to the dri2_egl_display vbtl not having been set up.
    
    Fixes: 2db95482964 ("loader_dri3/glx/egl: Optionally use a blit
    context for blitting operations")
    Signed-off-by: Frank Binns <francisbinns@gmail.com>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

--- src/egl/drivers/dri2/egl_dri2.c
+++ src/egl/drivers/dri2/egl_dri2.c
@@ -973,7 +973,7 @@ dri2_display_destroy(_EGLDisplay *disp)
    struct dri2_egl_display *dri2_dpy = dri2_egl_display(disp);
 
    if (dri2_dpy->own_dri_screen) {
-      if (dri2_dpy->vtbl->close_screen_notify)
+      if (dri2_dpy->vtbl && dri2_dpy->vtbl->close_screen_notify)
          dri2_dpy->vtbl->close_screen_notify(disp);
       dri2_dpy->core->destroyScreen(dri2_dpy->dri_screen);
    }
