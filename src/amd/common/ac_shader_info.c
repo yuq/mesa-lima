yuq/mesa-lima

commit 1c57a6da5e38c18926eca22b96901e4640ec430f

ac/shader: scan vertex inputs usage mask
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/common/ac_shader_info.c
@@ -133,6 +133,19 @@ gather_intrinsic_info(const nir_shader *nir, const nir_intrinsic_instr *instr,
 		if (nir->info.stage == MESA_SHADER_FRAGMENT)
 			info->ps.writes_memory = true;
 		break;
+	case nir_intrinsic_load_var:
+		if (nir->info.stage == MESA_SHADER_VERTEX) {
+			nir_deref_var *dvar = instr->variables[0];
+			nir_variable *var = dvar->var;
+
+			if (var->data.mode == nir_var_shader_in) {
+				unsigned idx = var->data.location;
+				uint8_t mask =
+					nir_ssa_def_components_read(&instr->dest.ssa);
+				info->vs.input_usage_mask[idx] |= mask;
+			}
+		}
+		break;
 	default:
 		break;
 	}

src/amd/common/ac_shader_info.h
@@ -24,6 +24,8 @@
 #ifndef AC_SHADER_INFO_H
 #define AC_SHADER_INFO_H
 
+#include "compiler/shader_enums.h"
+
 struct nir_shader;
 struct ac_nir_compiler_options;
 
@@ -34,6 +36,7 @@ struct ac_shader_info {
 	bool uses_invocation_id;
 	bool uses_prim_id;
 	struct {
+		uint8_t input_usage_mask[VERT_ATTRIB_MAX];
 		bool has_vertex_buffers; /* needs vertex buffers and base/start */
 		bool needs_draw_id;
 		bool needs_instance_id;

commit 3401b028df1074a06a7fbc3fb1cda949646ef75d

ac/shader: fix vertex input with components.
This fixes:
dEQP-VK.glsl.440.linkage.varying.component.*

Fixes: 1c57a6da5e3 (ac/shader: scan vertex inputs usage mask)
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/amd/common/ac_shader_info.c
@@ -141,7 +141,7 @@ gather_intrinsic_info(const nir_shader *nir, const nir_intrinsic_instr *instr,
 			if (var->data.mode == nir_var_shader_in) {
 				unsigned idx = var->data.location;
 				uint8_t mask =
-					nir_ssa_def_components_read(&instr->dest.ssa);
+					nir_ssa_def_components_read(&instr->dest.ssa) << var->data.location_frac;
 				info->vs.input_usage_mask[idx] |= mask;
 			}
 		}