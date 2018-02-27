ae47af50d6ff3a97110b77535f86f7a76d1631a9
Timothy Arceri <tarceri@itsqueeze.com>
2018-02-27 12:48:11 +1100

    radeonsi/nir: disable vertex_id_zero_based lowering
    The lowering is incompatible with how the radeonsi backend works.
    
    Fixes piglit test:
    ./bin/arb_shader_draw_parameters-basevertex vertexid-zerobased -auto
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/drivers/radeonsi/si_get.c
+++ src/gallium/drivers/radeonsi/si_get.c
@@ -492,7 +492,6 @@ static int si_get_shader_param(struct pipe_screen* pscreen,
 }
 
 static const struct nir_shader_compiler_options nir_options = {
-	.vertex_id_zero_based = true,
 	.lower_scmp = true,
 	.lower_flrp32 = true,
 	.lower_flrp64 = true,
