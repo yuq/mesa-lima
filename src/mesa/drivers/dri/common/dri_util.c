c6067fcd0747c85dee333b9b5726fc7fec998c1b
Andres Gomez <agomez@igalia.com>
2018-03-03 00:55:46 +0200

    dri_util: don't fail when not supporting ARB_compatibility with GL3.1
    Currently, any driver that does not support the ARB_compatibility
    extension will fail on GL3.1 context creation if the application does
    not request the forward-compatiblity flag.
    
    Restore the original check which changes mesa_api to API_OPENGL_CORE,
    only when:
     - GL3.1 is requested, without the forward-compatiblity flag.
     - driver does not support ARB_compatibility - as deduced by
    max_gl_compat_version.
    
    Fixes: a0c8b49284e ("mesa: enable OpenGL 3.1 with ARB_compatibility")
    
    v2:
     - Improve commit log (Emil).
     - Provide a correct explanation on the features documentation (Ian).
    
    Cc: Marek Olšák <marek.olsak@amd.com>
    Cc: Ian Romanick <ian.d.romanick@intel.com>
    Cc: Kenneth Graunke <kenneth@whitecape.org>
    Cc: Eric Engestrom <eric.engestrom@imgtec.com>
    Cc: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/mesa/drivers/dri/common/dri_util.c
+++ src/mesa/drivers/dri/common/dri_util.c
@@ -379,6 +379,16 @@ driCreateContextAttribs(__DRIscreen *screen, int api,
 	}
     }
 
+    /* The specific Mesa driver may not support the GL_ARB_compatibilty
+     * extension or the compatibility profile.  In that case, we treat an
+     * API_OPENGL_COMPAT 3.1 as API_OPENGL_CORE. We reject API_OPENGL_COMPAT
+     * 3.2+ in any case.
+     */
+    if (mesa_api == API_OPENGL_COMPAT &&
+        ctx_config.major_version == 3 && ctx_config.minor_version == 1 &&
+        screen->max_gl_compat_version < 31)
+       mesa_api = API_OPENGL_CORE;
+
     if (mesa_api == API_OPENGL_COMPAT
         && ((ctx_config.major_version > 3)
             || (ctx_config.major_version == 3 &&
