yuq/mesa-lima

commit b38b208ff8886e799d6a27522853c9afc8be8772

radeonsi:create uvd hevc enc entry
Add UVD hevc encode pipe video codec creation entry

Signed-off-by: James Zhu <James.Zhu@amd.com>
Reviewed-by: Boyuan Zhang <boyuan.zhang@amd.com>

src/gallium/drivers/radeonsi/si_uvd.c
@@ -31,6 +31,8 @@
 #include "radeon/radeon_vce.h"
 #include "radeon/radeon_vcn_dec.h"
 #include "radeon/radeon_vcn_enc.h"
+#include "radeon/radeon_uvd_enc.h"
+#include "util/u_video.h"
 
 /**
  * creates an video buffer with an UVD compatible memory layout
@@ -146,9 +148,16 @@ struct pipe_video_codec *si_uvd_create_decoder(struct pipe_context *context,
 	struct si_context *ctx = (struct si_context *)context;
 	bool vcn = (ctx->b.family == CHIP_RAVEN) ? true : false;
 
-	if (templ->entrypoint == PIPE_VIDEO_ENTRYPOINT_ENCODE)
-		return (vcn) ? radeon_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer) :
-               si_vce_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
+	if (templ->entrypoint == PIPE_VIDEO_ENTRYPOINT_ENCODE) {
+		if (vcn) {
+			radeon_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
+		} else {
+			if (u_reduce_video_profile(templ->profile) == PIPE_VIDEO_FORMAT_HEVC)
+				return radeon_uvd_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
+			else
+				return si_vce_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
+		}
+	}
 
 	return (vcn) ? 	radeon_create_decoder(context, templ) :
 		si_common_uvd_create_decoder(context, templ, si_uvd_set_dtb);

commit 6a62e455f2a703125127161f4c39b98947e8a0f3

radeonsi: fix radeon create encoder return
Previous patch missed a "return" when trying to modify the create encoder
function, which made the whole logic fail. Therefore, add the return back.

Fixes: b38b208ff8886e799d6a2 "radeonsi:create uvd hevc enc entry"

Signed-off-by: Boyuan Zhang <boyuan.zhang@amd.com>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

src/gallium/drivers/radeonsi/si_uvd.c
@@ -150,7 +150,7 @@ struct pipe_video_codec *si_uvd_create_decoder(struct pipe_context *context,
 
 	if (templ->entrypoint == PIPE_VIDEO_ENTRYPOINT_ENCODE) {
 		if (vcn) {
-			radeon_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
+			return radeon_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);
 		} else {
 			if (u_reduce_video_profile(templ->profile) == PIPE_VIDEO_FORMAT_HEVC)
 				return radeon_uvd_create_encoder(context, templ, ctx->b.ws, si_vce_get_buffer);