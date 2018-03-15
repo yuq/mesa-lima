f7f95310f0ac3a89419c866caf962c17ea1d599d
Emil Velikov <emil.velikov@collabora.com>
2018-03-15 12:12:58 +0000

    tgsi: move tgsi_processor_to_shader_stage() to a header
    This way we can utilise it with later patches.
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/auxiliary/tgsi/tgsi_from_mesa.h
+++ src/gallium/auxiliary/tgsi/tgsi_from_mesa.h
@@ -68,4 +68,19 @@ pipe_shader_type_from_mesa(gl_shader_stage stage)
    }
 }
 
+static inline gl_shader_stage
+tgsi_processor_to_shader_stage(unsigned processor)
+{
+   switch (processor) {
+   case PIPE_SHADER_FRAGMENT:  return MESA_SHADER_FRAGMENT;
+   case PIPE_SHADER_VERTEX:    return MESA_SHADER_VERTEX;
+   case PIPE_SHADER_GEOMETRY:  return MESA_SHADER_GEOMETRY;
+   case PIPE_SHADER_TESS_CTRL: return MESA_SHADER_TESS_CTRL;
+   case PIPE_SHADER_TESS_EVAL: return MESA_SHADER_TESS_EVAL;
+   case PIPE_SHADER_COMPUTE:   return MESA_SHADER_COMPUTE;
+   default:
+      unreachable("invalid TGSI processor");
+   }
+}
+
 #endif /* TGSI_FROM_MESA_H */
