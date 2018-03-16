54db78b196d883a878301651af3313fd50c39fd5
Matt Turner <mattst88@gmail.com>
2018-03-16 10:50:51 -0700

    intel: Add cfl to gen_device_name_to_pci_device_id()
    Reviewed-by: Rafael Antognolli <rafael.antognolli@intel.com>

--- src/intel/dev/gen_device_info.c
+++ src/intel/dev/gen_device_info.c
@@ -55,6 +55,7 @@ gen_device_name_to_pci_device_id(const char *name)
       { "bxt", 0x5A85 },
       { "kbl", 0x5912 },
       { "glk", 0x3185 },
+      { "cfl", 0x3E9B },
       { "cnl", 0x5a52 },
       { "icl", 0x8a52 },
    };
