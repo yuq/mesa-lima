cb619a3c9a49d27f60a2a616f719af17bc9f54d2
Brian Paul <brianp@vmware.com>
2018-03-30 11:09:07 -0600

    nir/spirv: fix MSVC syntax error in vtn_handle_texture()
    Reviewed-by: Neil Roberts <nroberts@igalia.com>

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -2086,8 +2086,9 @@ vtn_handle_texture(struct vtn_builder *b, SpvOp opcode,
          (*p++) = vtn_tex_src(b, w[idx++], nir_tex_src_offset);
 
       if (operands & SpvImageOperandsConstOffsetsMask) {
+         nir_tex_src none = {0};
          gather_offsets = vtn_ssa_value(b, w[idx++]);
-         (*p++) = (nir_tex_src){};
+         (*p++) = none;
       }
 
       if (operands & SpvImageOperandsSampleMask) {
