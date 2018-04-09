ab6cadd3ecc7fbdd9079808b407674e0b19c52f0
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2018-04-09 13:02:14 +0200

    radv: Don't store buffer references in the descriptor set.
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_descriptor_set.h
+++ src/amd/vulkan/radv_descriptor_set.h
@@ -35,7 +35,6 @@ struct radv_descriptor_set_binding_layout {
    uint32_t array_size;
 
    uint32_t offset;
-   uint32_t buffer_offset;
    uint16_t dynamic_offset_offset;
 
    uint16_t dynamic_offset_count;
@@ -61,9 +60,6 @@ struct radv_descriptor_set_layout {
    /* Shader stages affected by this descriptor set */
    uint16_t shader_stages;
    uint16_t dynamic_shader_stages;
-
-   /* Number of buffers in this descriptor set */
-   uint32_t buffer_count;
 
    /* Number of dynamic offsets used by this descriptor set */
    uint16_t dynamic_offset_count;
