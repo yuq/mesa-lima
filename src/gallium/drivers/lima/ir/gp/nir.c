780a2723e0b65bff8ee13b292fd0e3da92add491
Erico Nunes <nunes.erico@gmail.com>
2018-04-11 01:45:24 +0200

    lima/gpir: support select alu opcode
    Implement 'gpir_op_select' alu opcode from nir 'nir_op_bcsel'.
    
    Tested on Mali400 on the Allwinner A20.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

--- src/gallium/drivers/lima/ir/gp/nir.c
+++ src/gallium/drivers/lima/ir/gp/nir.c
@@ -109,6 +109,7 @@ static int nir_to_gpir_opcodes[nir_num_opcodes] = {
    [nir_op_frcp] = gpir_op_rcp,
    [nir_op_frsq] = gpir_op_rsqrt,
    [nir_op_slt] = gpir_op_lt,
+   [nir_op_bcsel] = gpir_op_select,
 };
 
 static bool gpir_emit_alu(gpir_block *block, nir_instr *ni)
