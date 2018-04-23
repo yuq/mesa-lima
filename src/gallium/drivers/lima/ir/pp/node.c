2cfcf46af2d46983193c205aff987104f35f1de1
Vasily Khoruzhick <anarsoul@gmail.com>
2018-04-22 22:43:42 -0700

    lima/ppir: implement nir_op_fmin
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- src/gallium/drivers/lima/ir/pp/node.c
+++ src/gallium/drivers/lima/ir/pp/node.c
@@ -106,6 +106,14 @@ const ppir_op_info ppir_op_infos[] = {
          PPIR_INSTR_SLOT_END
       },
    },
+   [ppir_op_min] = {
+      .name = "min",
+      .slots = (int []) {
+         PPIR_INSTR_SLOT_ALU_SCL_ADD, PPIR_INSTR_SLOT_ALU_SCL_MUL,
+         PPIR_INSTR_SLOT_ALU_VEC_ADD, PPIR_INSTR_SLOT_ALU_VEC_MUL,
+         PPIR_INSTR_SLOT_END
+      },
+   },
    [ppir_op_rcp] = {
       .name = "rcp",
       .slots = (int []) {
